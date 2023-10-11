262009e2598a9247b67ca34d537a25a926501d56
Jacob Meixner <jjm3333@gmail.com>
2023-10-10 23:14:27 -0700

    Add a Menu div

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,8 @@ export default function Home() {
             fill={fill}
             alt="Landing Page which has the title of 'Meixner's Amazing Race Fantasy League' and the subtitle 'A web tool to help run an Amazing Race fantasy legue'"
             />
+          <div>  
+          </div>  
       </div>
     )
 }
