9bf77ca66637d07ca2856e3acdf819d7e489428e
Jacob Meixner <jjm3333@gmail.com>
2023-10-10 23:42:29 -0700

    Add some text and styling to render a basic team list

--- app/contestants/page.tsx
+++ app/contestants/page.tsx
@@ -1,9 +1,16 @@
 export default function Contestants() {
 
+    const final = [ "Derek & Claire", "Emily & Molly", "Luis & Michelle", "Aubrey & David", "Marcus & Michael", "Quinton & Mattie", "Glenda & Lumumba", "Abby & Will", "Linton & Sharik", "Rich & Dom", "Tim & Rex", "Aashta & Nina" ]
 
     return (
         <div>
-          <h1 className="text-2xl">Contestants This Season</h1>
+          <h1 className="text-2xl text-center">Contestants This Season</h1>
+          <br/>
+          <p className="text-lg text-center">{final.length} teams</p>
+          <br/>
+          <div className="text-center">
+              {final.map(t => <p>{t}</p>)}
+          </div>
         </div>
     )
 }
