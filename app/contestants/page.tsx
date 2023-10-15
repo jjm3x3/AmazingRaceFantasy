0f16d7e90f37c31ed951dad5e0eac74dcb7b52b2
jjm3x3 <jjm3x3@users.noreply.github.com>
2023-10-15 16:13:18 -0700

    Feature/add menu (#2)
    * Remove unnecessary CSS
    
    * Add a Menu div
    
    * Add styling for the menu box
    
    * Add a contestants page
    
    * Add a link to the contestants page
    
    * Add some styling to make contestant link visible

---
+++ app/contestants/page.tsx
@@ -0,0 +1,9 @@
+export default function Contestants() {
+
+
+    return (
+        <div>
+          <h1 className="text-2xl">Contestants This Season</h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
 
@@ -13,6 +14,9 @@ export default function Home() {
             fill={fill}
             alt="Landing Page which has the title of 'Meixner's Amazing Race Fantasy League' and the subtitle 'A web tool to help run an Amazing Race fantasy legue'"
             />
+          <div className="menu-box">
+            <Link className="menu-item" href="/contestants">Contestant List</Link>
+          </div>  
       </div>
     )
 }
