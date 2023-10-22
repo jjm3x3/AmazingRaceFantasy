ba5d5f87e75e91a878a9eb75cf6a3aa3612c7df0
Jacob Meixner <jjm3333@gmail.com>
2023-10-22 16:58:21 -0700

    Add a getData func to get server props

--- app/contestants/page.tsx
+++ app/contestants/page.tsx
@@ -1,15 +1,21 @@
-export default function Contestants() {
+export async function getData() {
 
-    const final = [ "Derek & Claire", "Emily & Molly", "Luis & Michelle", "Aubrey & David", "Marcus & Michael", "Quinton & Mattie", "Glenda & Lumumba", "Abby & Will", "Linton & Sharik", "Rich & Dom", "Tim & Rex", "Aashta & Nina" ]
+    const aList = [ "Derek & Claire", "Emily & Molly", "Luis & Michelle", "Aubrey & David", "Marcus & Michael", "Quinton & Mattie", "Glenda & Lumumba", "Abby & Will", "Linton & Sharik", "Rich & Dom", "Tim & Rex", "Aashta & Nina" ]
+    return { props: { runners: aList } }
+}
+
+export default async function Contestants() {
+
+    const final = await getData()
 
     return (
         <div>
           <h1 className="text-2xl text-center">Contestants This Season</h1>
           <br/>
-          <p className="text-lg text-center">{final.length} teams</p>
+          <p className="text-lg text-center">{final.props.runners.length} teams</p>
           <br/>
           <div className="text-center">
-              {final.map(t => <p>{t}</p>)}
+              {final.props.runners.map(t => <p>{t}</p>)}
           </div>
         </div>
     )
