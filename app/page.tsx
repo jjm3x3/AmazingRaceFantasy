359d26ec1c5f35eff6433e31d1c71ca6e8677f61
Jacob Meixner <jjm3333@gmail.com>
2023-10-22 19:53:38 -0700

    Remove the unnecessary fill variable

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,11 @@ import Link from 'next/link'
 
 export default function Home() {
 
-
-
-    const fill = true
-
     return (
       <div>
           <Image 
             src="/MeixnerAmzingRaceFantacyLeageHome.svg" 
-            fill={fill}
+            fill
             alt="Landing Page which has the title of 'Meixner's Amazing Race Fantasy League' and the subtitle 'A web tool to help run an Amazing Race fantasy legue'"
             />
           <div className="menu-box">
