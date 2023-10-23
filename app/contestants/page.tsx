4590b4fc4b73291b0fdc930eae46f033c739785b
Jacob Meixner <jjm3333@gmail.com>
2023-10-22 17:22:09 -0700

    Enable domParsing

--- app/contestants/page.tsx
+++ app/contestants/page.tsx
@@ -1,10 +1,13 @@
+import { JSDOM } from 'jsdom'
+
 async function getData() {
 
     const aList = [ "Derek & Claire", "Emily & Molly", "Luis & Michelle", "Aubrey & David", "Marcus & Michael", "Quinton & Mattie", "Glenda & Lumumba", "Abby & Will", "Linton & Sharik", "Rich & Dom", "Tim & Rex", "Aashta & Nina" ]
     const response = await fetch("https://en.wikipedia.org/wiki/The_Amazing_Race_35")
-    console.log("Reponse got...")
     const responseText = await response.text()
-    console.log("Text of web query" + responseText)
+    const doc = new JSDOM(responseText)
+    var domQuery = doc.window.document.querySelectorAll(".vcard .fn")
+    console.log("Here is a domQuery: " + domQuery)
 
     return { props: { runners: aList } }
 }
