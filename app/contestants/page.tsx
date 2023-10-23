f1639061cd9315c51fccf6e19616a42057849834
Jacob Meixner <jjm3333@gmail.com>
2023-10-22 18:40:01 -0700

    Add standard link styling

--- app/contestants/page.tsx
+++ app/contestants/page.tsx
@@ -48,7 +48,7 @@ export default async function Contestants() {
           </div>
           <div>
             <p>
-              Data provided by <a href={wikiUrl} >Wikipedia</a> for this season of the race
+              Data provided by <a className="standard-link" href={wikiUrl} >Wikipedia</a> for this season of the race
             </p>
           </div>
         </div>
