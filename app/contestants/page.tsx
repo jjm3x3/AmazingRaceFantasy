99b9e8da4b3f92a4ce49da5ada9be5f21a84ecbe
jjm3x3 <jjm3x3@users.noreply.github.com>
2023-10-31 22:11:38 -0700

    Feature/contestants page (#4)
    * Add some text and styling to render a basic team list
    
    * Add jsdom to parse wiki page
    
    * Add a getData func to get server props
    
    * Add a key prop
    
    * Remove export for get data func
    
    * Add wiki scraping to getData
    
    * Enable domParsing
    
    * Add types/jsdom as dev dep
    
    * Finish compiling and building page data
    
    * Add console.warn and null check for contestant names
    
    * Get rid of hard coded team list
    
    * Remove extraneous console log
    
    * Extract wiki url
    
    * Add wiki link for attribution
    
    * Add standard link styling

--- app/contestants/page.tsx
+++ app/contestants/page.tsx
@@ -1,9 +1,56 @@
-export default function Contestants() {
+import { JSDOM } from 'jsdom'
 
+const wikiUrl = "https://en.wikipedia.org/wiki/The_Amazing_Race_35"
+
+async function getData() {
+
+    const response = await fetch(wikiUrl)
+    const responseText = await response.text()
+    const doc = new JSDOM(responseText)
+    var domQuery = doc.window.document.querySelectorAll(".vcard .fn")
+
+    // Build data model
+    const final = []
+    var team = ""
+    for (var i = 0; i < domQuery.length; i++) {
+        var contestantFullName = domQuery[i].textContent
+        var contestantNames = null
+        if (contestantFullName == null) {
+            console.warn("Found a null contestant Name...")
+            continue
+        } else {
+            contestantNames = contestantFullName.split(" ")
+        }
+        team += contestantNames[0]
+        if (i % 2 == 0) {
+            team += " & "
+        } else {
+            final.push(team)
+            team = ""
+        }
+    }
+
+    return { props: { runners: final } }
+}
+
+export default async function Contestants() {
+
+    const final = await getData()
 
     return (
         <div>
-          <h1 className="text-2xl">Contestants This Season</h1>
+          <h1 className="text-2xl text-center">Contestants This Season</h1>
+          <br/>
+          <p className="text-lg text-center">{final.props.runners.length} teams</p>
+          <br/>
+          <div className="text-center">
+              {final.props.runners.map(t => <p key={t}>{t}</p>)}
+          </div>
+          <div>
+            <p>
+              Data provided by <a className="standard-link" href={wikiUrl} >Wikipedia</a> for this season of the race
+            </p>
+          </div>
         </div>
     )
 }
