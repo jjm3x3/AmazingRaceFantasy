5aada0305faf3044c8e58f387000a7cfd19d2420
jjm3x3 <jjm3x3@users.noreply.github.com>
2023-11-09 19:32:01 -0800

    Feature/responsive design (#6)
    * Remove the unnecessary fill variable
    
    * Add Image Source map per gpt's suggestion
    
    * Add the smaller version of the image
    
    * Try using a html picture tag on gpt's rec
    
    * Fix formatting
    
    * Remove the fixed position and add tailwind responsive classes
    
    * Move position css classes into the css apply
    
    * Update the md:left setting
    
    * Make hight responsive
    
    * Fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+const imageSources = [
+    {
+        src: 'MeixnerAmzingRaceFantacyLeageHome-360w.svg',
+        media: '(max-width: 768px)'
+    },
+    {
+        src: '/MeixnerAmzingRaceFantacyLeageHome.svg'
+    }
+]
+
 export default function Home() {
-
-
-
-    const fill = true
 
     return (
       <div>
-          <Image 
-            src="/MeixnerAmzingRaceFantacyLeageHome.svg" 
-            fill={fill}
-            alt="Landing Page which has the title of 'Meixner's Amazing Race Fantasy League' and the subtitle 'A web tool to help run an Amazing Race fantasy legue'"
-            />
+          <picture>
+            {imageSources.map((source,index) => ( <source key={index} srcSet={source.src} media={source.media} />
+            ))}
+            <Image
+              src="/MeixnerAmzingRaceFantacyLeageHome.svg"
+              fill
+              alt="Landing Page which has the title of 'Meixner's Amazing Race Fantasy Legue' and the subtitle 'A web tool to help run an Amazing Race fantasy legue'"
+              >
+            </Image>
+          </picture>
           <div className="menu-box">
             <Link className="menu-item" href="/contestants">Contestant List</Link>
           </div>  
