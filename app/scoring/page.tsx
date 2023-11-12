b79be48acf16e4badac6562b04d53a6581f766a5
Jacob Meixner <jjm3333@gmail.com>
2023-11-11 16:17:25 -0800

    Add code to generate weekly scores

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -6,6 +6,20 @@ export default async function Scoring() {
 
     const currentSelectedContestant = "Jacob"
 
+    const numberOfRounds = wikiData.props.runners.reduce(
+        (acc, x) => {
+            return x.eliminationOrder > acc ? x.eliminationOrder : acc
+        }, 0)
+
+    const roundScores = []
+    for(let i = 1; i <= numberOfRounds; i++) {
+        const roundScore = wikiData.props.runners.reduce(
+            (acc, x) => {
+                return x.eliminationOrder === 0 || x.eliminationOrder > i ? acc + 10 : acc
+            }, 0)
+        roundScores.push(roundScore)
+    }
+
     const weeklyScore = wikiData.props.runners.reduce(
         (acc, x) => {
             return x.isParticipating ? acc + 10 : acc
