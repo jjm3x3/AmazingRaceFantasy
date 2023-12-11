b619eaa7741f905aa582e152027c577dadaf0b10
Jacob Meixner <jjm3333@gmail.com>
2023-12-10 20:56:09 -0800

    Add another test to assert a thing I found during testing

--- __tests__/wikiQuery.js
+++ __tests__/wikiQuery.js
@@ -22,4 +22,23 @@ describe('getData', () => {
         // Assert
         expect(result).not.toBeNull()
     })
+
+    it('should throw an error for a contestantData w/ a missing status for second on a team', () => {
+        // Arrange
+        const firstContestantsFirstName = "Some"
+        const secondContestantsFirstName = "SomeGuys"
+
+        const listOfContestants = [
+            {
+                name: firstContestantsFirstName + " Guy",
+                status: "Participating"
+            },
+            {team: secondContestantsFirstName + " Brother"}]
+
+        // Act
+        var act = () => getTeamList(listOfContestants)
+
+        // Assert
+        expect(act).toThrow(new ReferenceError("Status is either null or undefined and it shouldn not be"))
+    })
 })
