372a232aab61b4295f812d797ee276ea8558b671
Jacob Meixner <jjm3333@gmail.com>
2023-12-10 19:21:20 -0800

    Move the apiUrl to global scope

--- app/utils/wikiFetch.tsx
+++ app/utils/wikiFetch.tsx
@@ -1,6 +1,7 @@
 import * as cheerio from 'cheerio'
 
 export const wikiUrl = "https://en.wikipedia.org/wiki/The_Amazing_Race_35"
+const apiUrl = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_35&section=7&formatversion=2"
 
 interface IWikipediaData {
     parse: {
@@ -17,7 +18,6 @@ interface IWikipediaContestantData {
 }
 
 async function fetchWikipediaData(): Promise<IWikipediaData> {
-    const apiUrl = 'https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_35&section=7&formatversion=2'
     const response = await fetch(apiUrl)
     const data = await response.json()
     return data
