0dd5bf5f90581dde5d97dbe25056ee7bde621fd6
jjm3x3 <jjm3x3@users.noreply.github.com>
2023-12-12 22:54:19 -0800

    Bugfix/elimination is number (#23)
    * Add eliminationOrder test to show issue
    
    * Make sure that eliminationOrder is a number
    
    * Update package-lock.json based on running locally

--- __tests__/wikiQuery.js
+++ __tests__/wikiQuery.js
@@ -42,4 +42,26 @@ describe('getData', () => {
         // Assert
         expect(act).toThrow(new ReferenceError("Status is either null or undefined and it shouldn not be"))
     })
+
+    it('Should parse out elimination order a populate it when the team is not participating', () => {
+        const firstContestantsFirstName = "Some"
+        const secondContestantsFirstName = "SomeGuys"
+        const expectedEliminationOrder = 2
+
+        const listOfContestants = [
+            {},
+            {
+                name: firstContestantsFirstName + " Guy",
+                status: "Eliminated " + expectedEliminationOrder + "nd"
+            },
+            {
+                name: secondContestantsFirstName + " Brother",
+                status: "Eliminated " + expectedEliminationOrder + "nd"
+            }
+        ]
+
+        var result = getTeamList(listOfContestants)
+
+        expect(result.props.runners[0].eliminationOrder).toEqual(2)
+    })
 })
