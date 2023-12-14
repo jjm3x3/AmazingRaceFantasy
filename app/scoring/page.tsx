5c228c966a51e5d9ea710d3e9e7d440531cba963
jjm3x3 <jjm3x3@users.noreply.github.com>
2023-12-13 17:22:53 -0800

    Feature/add perfect scoring (#12)
    * Add a display similar to the contestants page with no scoring
    
    * Get team data and generate this weeks score
    
    * Add code to generate weekly scores
    
    * Output weekly scores
    
    * Generate team list every time
    
    * Add some extra breaks
    
    * Reverse the list so eliminated teams are on the bottom
    
    * Only Cross of teams based on the week it is
    
    * Add a week header
    
    * Add in grand total
    
    * Update the name of the util to import
    
    * Bit of fixup using new code
    
    * Fix typing
    
    * Add a roundNumber (AKA index) to use for keys
    
    * Update the fragment and all other top level tags child tags
    
    * Add key to fragment and all children tags
    
    * Update score variable to use full name

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -1,9 +1,62 @@
-export default function Scoring() {
-    var currentSelectedContestant = "Jacob"
+import { Fragment } from 'react'
+import { getTeamList, ITeam } from "../utils/wikiQuery"
+import { wikiUrl, getWikipediaContestantData } from "../utils/wikiFetch"
 
+export default async function Scoring() {
+
+    const wikiContestants = await getWikipediaContestantData()
+    const pageData = getTeamList(wikiContestants)
+
+    const currentSelectedContestant = "Jacob"
+
+    const numberOfRounds = pageData.props.runners.reduce(
+        (acc: number, x: ITeam) => {
+            return x.eliminationOrder > acc ? x.eliminationOrder : acc
+        }, 0)
+
+    const roundScores = []
+    for(let i = 1; i <= numberOfRounds; i++) {
+        const roundScore = pageData.props.runners.reduce(
+            (acc: number, x: ITeam) => {
+                return x.eliminationOrder === 0 || x.eliminationOrder > i ? acc + 10 : acc
+            }, 0)
+        roundScores.push(roundScore)
+    }
+
+    const weeklyScore = pageData.props.runners.reduce(
+        (acc: number, x: ITeam) => {
+            return x.isParticipating ? acc + 10 : acc
+        }, 0)
+
+    const reverseTeamsList = [...pageData.props.runners].reverse()
+    let grandTotal = 0
+    let currentWeek = 0
     return (
         <div>
             <h1 className="text-2xl text-center">Current Scoring for {currentSelectedContestant}</h1>
+            <br/>
+            <div className="text-center">
+                {roundScores.map((score, roundNumber) => {
+                    grandTotal += score
+                    currentWeek++
+
+                    return (<Fragment key={"round details"+roundNumber}>
+                        <h2 key={"weekHeader"+roundNumber}className="text-xl">Week {currentWeek}</h2>
+                        {reverseTeamsList.map(t => {
+                            return (<Fragment key={"teamStanding"+t.teamName+roundNumber}>
+                                <p key={t.teamName+roundNumber}>
+                                    {t.eliminationOrder === 0 || currentWeek < t.eliminationOrder ? t.teamName : <s>{t.teamName}</s>}
+                                </p>
+                            </Fragment>)
+                        })}
+                        <br/>
+                        <p key={"weekTotal"+roundNumber}className="text-center">Weekly Total: {score}</p>
+                        <p key={"grandTotal"+roundNumber}className="text-center">Grand Total: {grandTotal}</p>
+                        <br/>
+                        <br/>
+                    </Fragment>)
+                })}
+            </div>
         </div>
     )
 }
