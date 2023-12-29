1e27c445b3322c0d0259fc4db54f15ce553fdd72
Jacob Meixner <jjm3333@gmail.com>
2023-12-28 22:03:55 -0800

    Add a new test file just for fetch/filter util

---
+++ __tests__/fetchQuery.js
@@ -0,0 +1,7 @@
+import { filterEmptyContestants } from '../app/utils/wikiFetch'
+
+describe('filterEmptyContestants', () => {
+    it('should make sure that empty contestants are not returned', () => {
+
+    })
+})
