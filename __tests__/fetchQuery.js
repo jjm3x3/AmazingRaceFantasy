05557acfc34e6e27c2d7ef04d1615773194509bf
Jacob Meixner <jjm3333@gmail.com>
2023-12-30 15:40:59 -0800

    Fix up the one test to assert something

--- __tests__/fetchQuery.js
+++ __tests__/fetchQuery.js
@@ -1,7 +1,13 @@
 import { filterEmptyContestants } from '../app/utils/wikiFetch'
 
 describe('filterEmptyContestants', () => {
-    it('should make sure that empty contestants are not returned', () => {
+    it('should make sure that contestants without names are not returned', () => {
+        const inputContestants = [
+            {},
+            {name: "some name"}
+        ]
+        const result = filterEmptyContestants(inputContestants)
 
+        expect(result).not.toHaveLength(2)
     })
 })
