27e9bb3599f84cb1ea34fabdda7b32babf515c88
Jacob Meixner <jjm3333@gmail.com>
2023-12-30 18:13:05 -0800

    Fix typing on a reduce call

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -36,7 +36,7 @@ export default async function Scoring() {
     const wikiContestants = await getWikipediaContestantData()
     const pageData = getTeamList(wikiContestants)
 
-    const teamDictionary = pageData.props.runners.reduce((acc: Dictionary<any>, t) => {
+    const teamDictionary = pageData.props.runners.reduce((acc: Dictionary<any>, t: ITeam) => {
             acc[getKey(t.teamName)] = t
 
             return acc
