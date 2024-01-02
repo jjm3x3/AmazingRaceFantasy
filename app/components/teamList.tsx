732bce5d0b4092bfc4928529c2399905b050bef2
Jacob Meixner <jjm3333@gmail.com>
2024-01-01 17:07:49 -0800

    Cross off team based on position in list

--- app/components/teamList.tsx
+++ app/components/teamList.tsx
@@ -1,15 +1,17 @@
 import { Fragment } from 'react'
 import Team from '../models/Team'
-
+import { shouldBeScored } from '../utils/teamListUtils'
 
 export default function TeamList({ teamList, roundNumber }: { teamList: Team[], roundNumber: number }) {
 
     
     return <div>
         {teamList.map(t => {
+            const teamPosition = teamList.length - teamList.indexOf(t)
+
             return (<Fragment key={"teamStanding"+t.teamName+roundNumber}>
                 <p key={t.teamName+roundNumber}>
-                    {t.isInPlay(roundNumber) ? t.teamName : <s>{t.teamName}</s>}
+                    {shouldBeScored(t, teamPosition, roundNumber) ? t.teamName : <s>{t.teamName}</s> }
                 </p>
             </Fragment>)
         })}
