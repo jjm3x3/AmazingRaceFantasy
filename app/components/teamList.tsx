f6a985c468aba5b5ee0bde2a51d6fbd41b41b4f3
Jacob Meixner <jjm3333@gmail.com>
2024-01-01 16:42:55 -0800

    Rework component to be more readable

--- app/components/teamList.tsx
+++ app/components/teamList.tsx
@@ -1,13 +1,21 @@
 import { Fragment } from 'react'
 import { ITeam } from '../utils/wikiQuery'
 
+function shouldBeCrossedOff(teamInfo: ITeam, currentWeek: number): boolean {
+
+    const teamIsNotParticipating = !teamInfo.isParticipating
+    const teamWasEliminatedThisWeekOrBefore = teamInfo.eliminationOrder <= currentWeek
+
+    return teamIsNotParticipating && teamWasEliminatedThisWeekOrBefore
+}
+
 export default function TeamList({ teamList, roundNumber }: { teamList: ITeam[], roundNumber: number }) {
     
     return <div>
         {teamList.map(t => {
             return (<Fragment key={"teamStanding"+t.teamName+roundNumber}>
                 <p key={t.teamName+roundNumber}>
-                    {t.eliminationOrder === 0 || roundNumber+1 < t.eliminationOrder ? t.teamName : <s>{t.teamName}</s>}
+                    {shouldBeCrossedOff(t, roundNumber+1) ? <s>{t.teamName}</s> : t.teamName }
                 </p>
             </Fragment>)
         })}
