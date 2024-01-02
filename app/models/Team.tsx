42cd0341b210d6dceafed9bd547cbd36c85aba52
Jacob Meixner <jjm3333@gmail.com>
2024-01-01 17:37:58 -0800

    Create a class to represent a team

---
+++ app/models/Team.tsx
@@ -0,0 +1,19 @@
+import { ITeam } from '../utils/wikiQuery'
+
+export default class Team {
+    constructor(inTeam: ITeam) {
+
+        this.teamName = inTeam.teamName
+        this.relationship = inTeam.relationship
+        this.isParticipating = inTeam.isParticipating
+        this.eliminationOrder = inTeam.eliminationOrder
+    }
+
+    isInPlay(roundNumber: number): boolean {
+        
+        //return x.eliminationOrder === 0 || x.eliminationOrder > i ? acc + 10 : acc
+        return this.eliminationOrder === 0 || roundNumber+1 < this.eliminationOrder
+    }
+         
+}
+
