0a9de4e46379955ab1b59240410b92c218973c0d
Jacob Meixner <jjm3333@gmail.com>
2024-01-01 19:11:56 -0800

    Add a new describe for isInPlay method

--- __tests__/models/Team.js
+++ __tests__/models/Team.js
@@ -9,3 +9,22 @@ describe('Team construct', () => {
         expect(aTeam).not.toBeNull()
     })
 })
+
+describe('Team isInPlay', () => {
+    it('should be true when elimination order is 0 reguardless of roundNumber (sparse)', () => {
+        const aTeam = new Team({
+            eliminationOrder: 0
+        })
+
+        // Assert, just check sections because otherwise it takes too long
+        for (let i = Number.MAX_VALUE - 100; i < Number.MAX_VALUE; i++) {
+            expect(aTeam.isInPlay(i)).toBeTruthy()
+        }
+        for (let i = Number.MIN_VALUE + 100; i > Number.MIN_VALUE; i--) {
+            expect(aTeam.isInPlay(i)).toBeTruthy()
+        }
+        for (let i = 0 - 100; i < 100; i++) {
+            expect(aTeam.isInPlay(i)).toBeTruthy()
+        }
+    })
+})
