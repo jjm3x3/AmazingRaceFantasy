1412dd26b7a3de5cf68a07e83e5d20ba5def089b
Jacob Meixner <jjm3333@gmail.com>
2024-01-01 19:20:25 -0800

    Add anothe bounds test

--- __tests__/models/Team.js
+++ __tests__/models/Team.js
@@ -43,4 +43,20 @@ describe('Team isInPlay', () => {
             expect(aTeam.isInPlay(i)).toBeFalsy()
         }
     })
+
+    it('should be false if roundOrder is one less than eliminationOrder', () => {
+
+        for (let i = 0 - 100; i < 100; i++) {
+            if (i === 0) { continue } // This was already coverd in the last test
+
+            // Arrange
+            const aTeam = new Team({
+                eliminationOrder: i
+            })
+
+            // Act, Assert
+            expect(aTeam.eliminationOrder).toBe(i)
+            expect(aTeam.isInPlay(i-1)).toBeFalsy()
+        }
+    })
 })
