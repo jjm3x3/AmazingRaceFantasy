4ed5255687eb3ecd77857d4bd7cc54b433556359
Jacob Meixner <jjm3333@gmail.com>
2024-01-11 21:48:41 -0800

    Add a util function so it can be tested

---
+++ app/utils/teamListUtils.tsx
@@ -0,0 +1,9 @@
+
+export function shouldBeScored(team: Team, teamPosition: number, roundNumber: number): bool {
+    
+   const currentWeek = roundNumber+1
+   const listHasTeamBeingEliminated = teamPosition < currentWeek
+
+   return team.isInPlay(roundNumber) && !listHasTeamBeingEliminated
+}
+
