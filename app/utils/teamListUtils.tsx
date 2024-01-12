b25b6bf0021218afe0efc6ada78d174b2894653d
Jacob Meixner <jjm3333@gmail.com>
2024-01-11 22:42:36 -0800

    Fix off by one introduced for scoring

--- app/utils/teamListUtils.tsx
+++ app/utils/teamListUtils.tsx
@@ -1,8 +1,9 @@
+import Team from '../models/Team'
 
 export function shouldBeScored(team: Team, teamPosition: number, roundNumber: number): bool {
     
    const currentWeek = roundNumber+1
-   const listHasTeamBeingEliminated = teamPosition < currentWeek
+   const listHasTeamBeingEliminated = teamPosition <= currentWeek
 
    return team.isInPlay(roundNumber) && !listHasTeamBeingEliminated
 }
