8b97a045a41bdcd14a56bdcccd125830c7497388
Jacob Meixner <jjm3333@gmail.com>
2024-01-12 22:54:09 -0800

    Update test accordingly

--- __tests__/utils/teamListUtils.js
+++ __tests__/utils/teamListUtils.js
@@ -1,40 +1,57 @@
 import { shouldBeScored } from '../../app/utils/teamListUtils'
 
 describe('teamListUtils shouldBeScored', () => {
-    it('should be false when position and round are equal', () => {
+    it('should be false when there is exactly one team and we are on the first round', () => {
         // Arrange
         const aTeam = {
             isInPlay: (round) => true
         }
+        const teamList = [aTeam]
 
         // Act
-        const result = shouldBeScored(aTeam, 0, 0)
+        const result = shouldBeScored(teamList, aTeam, 0)
 
         // Assert
         expect(result).toBeFalsy()
     })
 
-    it('should be false when position is 1 more than the round', () => {
+    it('should be false when the target team is in the first position or end of array and its the first round', () => {
         // Arrange
         const aTeam = {
             isInPlay: (round) => true
         }
+        const teamList = [{}, aTeam]
 
         // Act
-        const result = shouldBeScored(aTeam, 1, 0)
+        const result = shouldBeScored(teamList, aTeam, 0)
 
         // Assert
         expect(result).toBeFalsy()
     })
 
-    it('should be false when position is 2 more than the round', () => {
+    it('should be false when the target team is in the first position or end of array and its the second round round', () => {
         // Arrange
         const aTeam = {
             isInPlay: (round) => true
         }
+        const teamList = [{}, aTeam]
 
         // Act
-        const result = shouldBeScored(aTeam, 2, 0)
+        const result = shouldBeScored(teamList, aTeam, 1)
+
+        // Assert
+        expect(result).toBeFalsy()
+    })
+
+    it('should be true when the target team is in the 2nd position or second from the end of array and its the first round', () => {
+        // Arrange
+        const aTeam = {
+            isInPlay: (round) => true
+        }
+        const teamList = [aTeam, {}]
+
+        // Act
+        const result = shouldBeScored(teamList, aTeam, 0)
 
         // Assert
         expect(result).toBeTruthy()
