b64586b562653c3e2f3ae1bd530de8a91b4cd74a
Jacob Meixner <jjm3333@gmail.com>
2024-01-13 18:58:45 -0800

    Add the teamList for contestantTeamList to component

--- app/components/round.tsx
+++ app/components/round.tsx
@@ -1,7 +1,15 @@
 import { Fragment } from 'react'
 import TeamList from '../components/teamList'
 
-export default function Round({ roundNumber, perfectTeamList }: { roundNumber: number, perfectTeamList: Team[] }) {
+export default function Round({
+        roundNumber,
+        perfectTeamList,
+        contestantTeamList
+    }: {
+        roundNumber: number,
+        perfectTeamList: Team[],
+        contestantTeamList: Team[]
+    }) {
 
     return (<Fragment key={"round details"+roundNumber}>
         <h2 key={"weekHeader"+roundNumber}className="text-xl">Week {roundNumber+1}</h2>
@@ -9,7 +17,11 @@ export default function Round({ roundNumber, perfectTeamList }: { roundNumber: n
             <div className="basis-1/2">
                 <TeamList teamList={perfectTeamList} roundNumber={roundNumber} />
             </div>
+            <div className="basis-1/2">
+                <TeamList teamList={contestantTeamList} roundNumber={roundNumber} />
+            </div>
         </div>
+        <br/>
         <br/>
     </Fragment>)
 }
