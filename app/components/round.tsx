3e863ada9b19e1695977bad5e8b8d819ab33288f
Jacob Meixner <jjm3333@gmail.com>
2024-01-13 18:56:23 -0800

    Add a teamList for the perfect listing

--- app/components/round.tsx
+++ app/components/round.tsx
@@ -1,9 +1,15 @@
 import { Fragment } from 'react'
+import TeamList from '../components/teamList'
 
-export default function Round({ roundNumber }: { roundNumber: number }) {
+export default function Round({ roundNumber, perfectTeamList }: { roundNumber: number, perfectTeamList: Team[] }) {
 
     return (<Fragment key={"round details"+roundNumber}>
         <h2 key={"weekHeader"+roundNumber}className="text-xl">Week {roundNumber+1}</h2>
+        <div className="text-center flex">
+            <div className="basis-1/2">
+                <TeamList teamList={perfectTeamList} roundNumber={roundNumber} />
+            </div>
+        </div>
         <br/>
     </Fragment>)
 }
