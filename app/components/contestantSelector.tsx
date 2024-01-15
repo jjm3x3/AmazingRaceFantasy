8d8acbf1a0675fb73f424a3d9ef6e1591124f1c3
Jacob Meixner <jjm3333@gmail.com>
2024-01-15 14:15:43 -0800

    Create a contestantSelector

---
+++ app/components/contestantSelector.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { Fragment, useState } from 'react'
+
+
+export default function ContestantSelector() {
+
+    const [selectedContestant, setSelectedContestant] = useState("Andrew")
+
+    return (
+        <div className="flex justify-center">
+            <select value={selectedContestant} onChange={e => setSelectedContestant(e.target.value)}>
+                <option>Andrew</option>
+                <option>Cindy</option>
+                <option>Jacob</option>
+                <option>Jim</option>
+                <option>Rachel</option>
+            </select>
+        </div>
+    )
+}
