fb32df0b5f016a66d99b9fbb4e26c7e4a22e2ac6
Jacob Meixner <jjm3333@gmail.com>
2024-01-18 23:07:17 -0800

    Add grandTotal variables

--- app/components/contestantRoundList.tsx
+++ app/components/contestantRoundList.tsx
@@ -1,9 +1,14 @@
 
 export default function ContestantRoundList({ perfectRoundScores }) {
 
+    let grandTotal = 0
+    let contestantGrandTotal = 0
+
     return (
         <div className="text-center">
             {perfectRoundScores.map((score, roundNumber) => {
+                grandTotal += score
+                contestantGrandTotal += contestantRoundScore
 
                 return (<><p>I Will Be A Round</p></>)
             })}
