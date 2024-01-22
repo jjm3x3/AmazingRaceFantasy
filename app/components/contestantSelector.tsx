98c10d38e416879ea01b2a94a444b88ff70c005a
Jacob Meixner <jjm3333@gmail.com>
2024-01-21 21:41:06 -0800

    Do some reformatting for better visuals

--- app/components/contestantSelector.tsx
+++ app/components/contestantSelector.tsx
@@ -18,14 +18,18 @@ export default function ContestantSelector({ listOfContestantRoundLists }: Conte
 
     return (
         <div className="flex justify-center">
-            <select value={selectedContestant} onChange={e => setSelectedContestant(e.target.value)}>
-                <option>Andrew</option>
-                <option>Cindy</option>
-                <option>Jacob</option>
-                <option>Jim</option>
-                <option>Rachel</option>
-            </select>
-            {filteredItems}
+            <div>
+                <select value={selectedContestant} onChange={e => setSelectedContestant(e.target.value)}>
+                    <option>Andrew</option>
+                    <option>Cindy</option>
+                    <option>Jacob</option>
+                    <option>Jim</option>
+                    <option>Rachel</option>
+                </select>
+            </div>
+            <div>
+                {filteredItems}
+            </div>
         </div>
     )
 }
