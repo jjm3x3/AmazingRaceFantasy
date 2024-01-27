8e1db34f74f9f7ec9395bd9aa02cc9c2e96ad12b
Jacob Meixner <jjm3333@gmail.com>
2024-01-26 21:30:09 -0800

    Remove all contestants who do not have rankings

--- app/components/contestantSelector.tsx
+++ app/components/contestantSelector.tsx
@@ -20,11 +20,7 @@ export default function ContestantSelector({ listOfContestantRoundLists }: Conte
         <div className="justify-center">
             <div className="flex justify-center">
                 <select value={selectedContestant} onChange={e => setSelectedContestant(e.target.value)}>
-                    <option>Andrew</option>
-                    <option>Cindy</option>
                     <option>Jacob</option>
-                    <option>Jim</option>
-                    <option>Rachel</option>
                 </select>
             </div>
             <br />
