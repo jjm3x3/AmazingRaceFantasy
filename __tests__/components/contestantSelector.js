f91d184a57ff2b3f41c5165f0f254109671d57d9
Jacob Meixner <jjm3333@gmail.com>
2024-01-30 22:37:16 -0800

    Add enough data to get test running without breaking

--- __tests__/components/contestantSelector.js
+++ __tests__/components/contestantSelector.js
@@ -2,16 +2,25 @@ import React from "react";
 import { render } from "@testing-library/react";
 import ContestantSelector from "../../app/components/contestantSelector";
 import ContestantRoundList from "../../app/components/contestantRoundList";
+import Team from "../../app/models/Team"
 
 const listOfContestantRoundListsMockData = [
   {
     key: "Jacob",
     content: (
       <ContestantRoundList
-        perfectRoundScores={[]}
-        contestantRoundScores={[]}
-        perfectTeamList={[]}
-        contestantTeamList={[]}
+        perfectRoundScores={[1,2,3]}
+        contestantRoundScores={[1,2,3]}
+        perfectTeamList={[
+            new Team({teamName: "name1_1 & name1_2"}),
+            new Team({teamName: "name2_1 & name2_2"}),
+            new Team({teamName: "name3_1 & name3_2"})
+        ]}
+        contestantTeamList={[
+            new Team({teamName: "name1_1 & name1_2"}),
+            new Team({teamName: "name2_1 & name2_2"}),
+            new Team({teamName: "name3_1 & name3_2"})
+        ]}
       />
     ),
   },
