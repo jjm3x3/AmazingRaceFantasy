a692a8786db6c6d320c38396eec93d28d20fc43d
Antoinette <acj.gfx@gmail.com>
2024-01-31 21:54:06 -0800

    Trying different comfig

--- jest.config.mjs
+++ jest.config.mjs
@@ -11,7 +11,8 @@ const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
-  testEnvironment: "jest-environment-jsdom",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
