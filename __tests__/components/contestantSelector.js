b03e46a6b2cb4e392ce214926fd9941e5272541d
Antoinette <acj.gfx@gmail.com>
2024-01-31 21:51:48 -0800

    Added missing dependency

--- __tests__/components/contestantSelector.js
+++ __tests__/components/contestantSelector.js
@@ -1,3 +1,4 @@
+import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import ContestantSelector from "../../app/components/contestantSelector";
 import ContestantRoundList from "../../app/components/contestantRoundList";
