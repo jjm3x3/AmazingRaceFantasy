56408d1652ecbeb1875bc2f4167bd88b53c46677
Jacob Meixner <jjm3333@gmail.com>
2024-02-02 17:35:02 -0800

    Remove currentContetant from page

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -38,8 +38,6 @@ export default async function Scoring() {
             return acc
         }, {})
 
-    const currentSelectedContestant = "Jacob"
-
     const currentSelectedContestantRanking = [ "Corey McArthur & Rob McArthur", "Jocelyn Chao & Victor Limary", "Lena Franklin & Morgan Franklin", "Greg Franklin & John Franklin", "Chelsea Day & Robbin Tomich", "Anna Leigh Wilson & Steve Cargile", "Ashlie Martin & Todd Martin", "Garrett Smith & Joel Strasser", "Ian Todd & Joe Moskowitz", "Andrea Simpson & Malaina Hatcher", "Liam Hykel & Yeremi Hykel", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
 
     const numberOfRounds = pageData.props.runners.reduce(
