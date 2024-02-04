fa51943a941157e5c97329a4e0820722f69e9bfd
Jacob Meixner <jjm3333@gmail.com>
2024-02-03 19:42:58 -0800

    Remove commented out code

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,5 @@
 import nextJest from "next/jest.js";
 
-/** @type {import('jest').Config} */
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
