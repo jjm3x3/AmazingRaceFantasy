7b794d2e2b4abfc5213c85d018a6d947f66a99b0
Jacob Meixner <jjm3333@gmail.com>
2024-02-07 21:55:31 -0800

    Add a league data file for s35 with JACOBS ranking

---
+++ app/leagueData/AmazingRace_35.tsx
@@ -0,0 +1,3 @@
+
+// Contestant Ranking
+export const JACOBS_RANKING = [ "Corey McArthur & Rob McArthur", "Jocelyn Chao & Victor Limary", "Lena Franklin & Morgan Franklin", "Greg Franklin & John Franklin", "Chelsea Day & Robbin Tomich", "Anna Leigh Wilson & Steve Cargile", "Ashlie Martin & Todd Martin", "Garrett Smith & Joel Strasser", "Ian Todd & Joe Moskowitz", "Andrea Simpson & Malaina Hatcher", "Liam Hykel & Yeremi Hykel", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
