b796489b43c447d9bb238a6767a9132b2a55c61e
Jacob Meixner <jjm3333@gmail.com>
2024-02-17 18:14:23 -0800

    Rework scoring page based on new generator work

--- app/newLeague/scoring/page.tsx
+++ app/newLeague/scoring/page.tsx
@@ -1,23 +1,11 @@
 import { WIKI_API_URL } from '../../leagueConfiguration/AmazingRace_36'
 import { CONTESTANT_LEAGUE_DATA } from '../../leagueData/AmazingRace_36'
 import ContestantSelector from '../../components/contestantSelector'
-import GenerateContestantRoundList from '../../generators/contestantRoundListGenerator'
+import generateListOfContestantRoundLists from '../../generators/contestantRoundListGenerator'
 
 export default async function Scoring() {
 
-    const listOfContestantRoundLists = await Promise.all(CONTESTANT_LEAGUE_DATA.map(async x => {
-
-        try {
-            const contestantRoundList = await GenerateContestantRoundList(WIKI_API_URL, x.ranking)
-
-            return {
-                key: x.name,
-                content: contestantRoundList
-            }
-        } catch (error) {
-            console.error("An error occrured generating a contestantRoundList: " + error)
-        }
-    }))
+    const listOfContestantRoundLists = await generateListOfContestantRoundLists(WIKI_API_URL, CONTESTANT_LEAGUE_DATA)
 
     return (
         <div>
