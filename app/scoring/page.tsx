e6516f77ec85bc6a961e1d88a2283f6f4068f52b
Jacob Meixner <jjm3333@gmail.com>
2024-02-17 18:09:02 -0800

    Remove extraneous code

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -8,28 +8,6 @@ import { CONTESTANT_LEAGUE_DATA } from '../leagueData/AmazingRace_35'
 import ContestantSelector from '../components/contestantSelector'
 import generateListOfContestantRoundLists from '../generators/contestantRoundListGenerator'
 
-interface Dictionary<T> {
-    [Key: string]: T;
-}
-
-
-function generateContestantRoundScores(contestantTeamsList: Team[], numberOfRounds: number) {
-
-    const contestantRoundScores: number[] = []
-
-    for(let i = 0; i < numberOfRounds; i++) {
-        const roundScore = contestantTeamsList.reduce(
-            (acc: number, x: Team) => {
-                const teamShouldBeScored = shouldBeScored(contestantTeamsList, x, i)
-
-                return teamShouldBeScored ? acc + 10 : acc
-            }, 0)
-        contestantRoundScores.push(roundScore)
-    }
-
-    return contestantRoundScores
-}
-
 export default async function Scoring() {
 
     const listOfContestantRoundLists = await generateListOfContestantRoundLists(WIKI_API_URL, CONTESTANT_LEAGUE_DATA)
