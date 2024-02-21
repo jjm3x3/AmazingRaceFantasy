dfb6949f3864e7196775bc3f6916a4b106f57c95
jjm3x3 <jjm3x3@users.noreply.github.com>
2024-02-21 15:24:15 -0800

    Feature/generate contestant data (#61)
    * Create generator file to generate ContestantRoundList
    
    * Import generator function
    
    * Replace generation code w/ generator call
    
    * Remove extraneous code
    
    * Remove unused imports
    
    * Removed poorly copied import
    
    * Add missing type to high order function

---
+++ app/generators/contestantRoundListGenerator.tsx
@@ -0,0 +1,69 @@
+import { ReactNode } from 'react'
+import { getTeamList, ITeam } from "../utils/wikiQuery"
+import { getWikipediaContestantData } from "../utils/wikiFetch"
+import Team from '../models/Team'
+import { shouldBeScored } from '../utils/teamListUtils'
+import ContestantRoundList from '../components/contestantRoundList'
+
+interface Dictionary<T> {
+    [Key: string]: T;
+}
+
+function generateContestantRoundScores(contestantTeamsList: Team[], numberOfRounds: number) {
+
+    const contestantRoundScores: number[] = []
+
+    for(let i = 0; i < numberOfRounds; i++) {
+        const roundScore = contestantTeamsList.reduce(
+            (acc: number, x: Team) => {
+                const teamShouldBeScored = shouldBeScored(contestantTeamsList, x, i)
+
+                return teamShouldBeScored ? acc + 10 : acc
+            }, 0)
+        contestantRoundScores.push(roundScore)
+    }
+
+    return contestantRoundScores
+}
+
+export default async function generateListOfContestantRoundLists(wikiApiUrl: string, listOfContestantLeagueData: any[]) {
+
+    const wikiContestants = await getWikipediaContestantData(wikiApiUrl)
+    const pageData = getTeamList(wikiContestants)
+
+    const teamDictionary = pageData.props.runners.reduce((acc: Dictionary<ITeam>, t: ITeam) => {
+            acc[Team.getKey(t.teamName)] = t
+
+            return acc
+        }, {})
+
+    const numberOfRounds = pageData.props.runners.reduce(
+        (acc: number, x: ITeam) => {
+            return x.eliminationOrder > acc ? x.eliminationOrder : acc
+        }, 0)
+
+    const reverseTeamsList = [...pageData.props.runners].reverse()
+
+    const roundScores = generateContestantRoundScores(reverseTeamsList, numberOfRounds)
+
+
+    return listOfContestantLeagueData.map(contestant => {
+
+        const currentSelectedContestantTeamsList = contestant.ranking.map((x: string) => {
+            const foundTeam = teamDictionary[Team.getKey(x)]
+            return foundTeam
+        })
+
+        const contestantRoundScores: number[] = generateContestantRoundScores(currentSelectedContestantTeamsList, numberOfRounds)
+
+        return {
+            key: contestant.name,
+            content: <ContestantRoundList
+                perfectRoundScores={roundScores}
+                contestantRoundScores={contestantRoundScores}
+                perfectTeamList={reverseTeamsList}
+                contestantTeamList={currentSelectedContestantTeamsList}
+            />
+        }
+    })
+}
--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -1,70 +1,11 @@
-import { getTeamList, ITeam } from "../utils/wikiQuery"
-import { getWikipediaContestantData } from "../utils/wikiFetch"
 import { WIKI_API_URL } from '../leagueConfiguration/AmazingRace_35'
-import Team from '../models/Team'
-import { shouldBeScored } from '../utils/teamListUtils'
-import ContestantRoundList from '../components/contestantRoundList'
 import { CONTESTANT_LEAGUE_DATA } from '../leagueData/AmazingRace_35'
 import ContestantSelector from '../components/contestantSelector'
-
-interface Dictionary<T> {
-    [Key: string]: T;
-}
-
-
-function generateContestantRoundScores(contestantTeamsList: Team[], numberOfRounds: number) {
-
-    const contestantRoundScores: number[] = []
-
-    for(let i = 0; i < numberOfRounds; i++) {
-        const roundScore = contestantTeamsList.reduce(
-            (acc: number, x: Team) => {
-                const teamShouldBeScored = shouldBeScored(contestantTeamsList, x, i)
-
-                return teamShouldBeScored ? acc + 10 : acc
-            }, 0)
-        contestantRoundScores.push(roundScore)
-    }
-
-    return contestantRoundScores
-}
+import generateListOfContestantRoundLists from '../generators/contestantRoundListGenerator'
 
 export default async function Scoring() {
 
-    const wikiContestants = await getWikipediaContestantData(WIKI_API_URL)
-    const pageData = getTeamList(wikiContestants)
-
-    const teamDictionary = pageData.props.runners.reduce((acc: Dictionary<ITeam>, t: ITeam) => {
-            acc[Team.getKey(t.teamName)] = t
-
-            return acc
-        }, {})
-
-
-    const numberOfRounds = pageData.props.runners.reduce(
-        (acc: number, x: ITeam) => {
-            return x.eliminationOrder > acc ? x.eliminationOrder : acc
-        }, 0)
-
-    const reverseTeamsList = [...pageData.props.runners].reverse()
-
-    const roundScores = generateContestantRoundScores(reverseTeamsList, numberOfRounds)
-
-    // New New Way
-    const listOfContestantRoundLists = CONTESTANT_LEAGUE_DATA.map(contestant => {
-
-        const contestantsTeamList = contestant.ranking.map(x => {
-            const foundTeam = teamDictionary[Team.getKey(x)]
-            return foundTeam
-        })
-
-        const contestantsRoundScores: number[] = generateContestantRoundScores(contestantsTeamList, numberOfRounds)
-
-        return {
-            key: contestant.name,
-            content: <ContestantRoundList perfectRoundScores={roundScores} contestantRoundScores={contestantsRoundScores} perfectTeamList={reverseTeamsList} contestantTeamList={contestantsTeamList}/>
-        }
-    })
+    const listOfContestantRoundLists = await generateListOfContestantRoundLists(WIKI_API_URL, CONTESTANT_LEAGUE_DATA)
 
     return (
         <div>
