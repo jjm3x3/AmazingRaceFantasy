245043300da4d93abce748893b711e10d4e7bd36
Jacob Meixner <jjm3333@gmail.com>
2024-02-20 23:12:36 -0800

    Fix new league import paths

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -1,7 +1,7 @@
-import { WIKI_API_URL } from '../../leagueConfiguration/AmazingRace_36'
-import { CONTESTANT_LEAGUE_DATA } from '../../leagueData/AmazingRace_36'
-import ContestantSelector from '../../components/contestantSelector'
-import generateListOfContestantRoundLists from '../../generators/contestantRoundListGenerator'
+import { WIKI_API_URL } from '../leagueConfiguration/AmazingRace_36'
+import { CONTESTANT_LEAGUE_DATA } from '../leagueData/AmazingRace_36'
+import ContestantSelector from '../components/contestantSelector'
+import generateListOfContestantRoundLists from '../generators/contestantRoundListGenerator'
 
 export default async function Scoring() {
 
