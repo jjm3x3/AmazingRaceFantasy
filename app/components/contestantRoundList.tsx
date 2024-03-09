be4d10648c8bb593224f155a9e11e9edb7832569
Jacob Meixner <jjm3333@gmail.com>
2024-03-09 15:41:19 -0800

    Add IContestantRoundData typing

--- app/components/contestantRoundList.tsx
+++ app/components/contestantRoundList.tsx
@@ -1,6 +1,7 @@
 import Round from './round'
 import Team from '../models/Team'
 import IRound from '../models/IRound'
+import IContestantRoundData from '../models/IContestantRoundData'
 
 export default function ContestantRoundList({
         perfectRoundScores,
@@ -23,12 +24,12 @@ export default function ContestantRoundList({
                     console.warn("Something is wrong that the rounds are out of order")
                 }
 
-                const perfectRound = round.contestantRoundData.filter((x: any) => x.name === "*perfect*")[0]
+                const perfectRound = round.contestantRoundData.filter((x: IContestantRoundData) => x.name === "*perfect*")[0]
                 const perfectScore = perfectRound.roundScore
                 const grandTotal = perfectRound.totalScore
 
                 const contestantRound = contestantRoundScores[roundNumber] // check round number
-                const filteredContestantRound = contestantRound.contestantRoundData.filter((x: any) => x.name === contestantName)[0]
+                const filteredContestantRound = contestantRound.contestantRoundData.filter((x: IContestantRoundData) => x.name === contestantName)[0]
                 const contestantRoundScore = filteredContestantRound.roundScore
                 const contestantGrandTotal = filteredContestantRound.totalScore
 
