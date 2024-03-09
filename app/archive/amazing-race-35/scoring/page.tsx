2f725dc8bf8bbb802e9eaf364f19bb273e2e05aa
Jacob Meixner <jjm3333@gmail.com>
2024-03-08 23:35:32 -0800

    Add the injection to the last season

--- app/archive/amazing-race-35/scoring/page.tsx
+++ app/archive/amazing-race-35/scoring/page.tsx
@@ -2,11 +2,13 @@
 import { WIKI_API_URL } from '../../../leagueConfiguration/AmazingRace_35'
 import { CONTESTANT_LEAGUE_DATA } from '../../../leagueData/AmazingRace_35'
 import ContestantSelector from '../../../components/contestantSelector'
+import { getWikipediaContestantDataFetcher } from '../../../utils/wikiFetch'
 import generateListOfContestantRoundLists from '../../../generators/contestantRoundListGenerator'
 
 export default async function Scoring() {
 
-    const listOfContestantRoundLists = await generateListOfContestantRoundLists(WIKI_API_URL, CONTESTANT_LEAGUE_DATA)
+    const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL)
+    const listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, CONTESTANT_LEAGUE_DATA)
 
     return (
         <div>
