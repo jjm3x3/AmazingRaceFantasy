efe4e1fc918c7d6a24670f7ebc547e3a4ef4f62e
Jacob Meixner <jjm3333@gmail.com>
2024-03-09 16:00:36 -0800

    Update contestantRoundListGenerator to use contestantUtils

--- app/generators/contestantRoundListGenerator.tsx
+++ app/generators/contestantRoundListGenerator.tsx
@@ -5,35 +5,10 @@ import Team from '../models/Team'
 import { shouldBeScored } from '../utils/teamListUtils'
 import ContestantRoundList from '../components/contestantRoundList'
 import IRound from '../models/IRound'
+import { generateContestantRoundScores } from '../utils/contestantUtils'
 
 interface Dictionary<T> {
     [Key: string]: T;
-}
-
-function generateContestantRoundScores(contestantTeamsList: Team[], numberOfRounds: number, contestantName: string) {
-
-    const contestantRoundScores: IRound[] = []
-
-    let grandTotal = 0
-    for(let i = 0; i < numberOfRounds; i++) {
-        const roundScore = contestantTeamsList.reduce(
-            (acc: number, x: Team) => {
-                const teamShouldBeScored = shouldBeScored(contestantTeamsList, x, i)
-
-                return teamShouldBeScored ? acc + 10 : acc
-            }, 0)
-        grandTotal += roundScore
-        contestantRoundScores.push({
-            round: i,
-            contestantRoundData:[{
-                name: contestantName,
-                roundScore: roundScore,
-                totalScore: grandTotal
-            }]
-        })
-    }
-
-    return contestantRoundScores
 }
 
 export default async function generateListOfContestantRoundLists(dataFetcher: () => Promise<IWikipediaContestantData[]>, listOfContestantLeagueData: any[]) {
