3e2f68d4aa17d8b60349b25ea5afe02890154e7a
Jacob Meixner <jjm3333@gmail.com>
2024-03-09 17:28:20 -0800

    Add a test for a single round

--- __tests__/utils/contestantUtils.js
+++ __tests__/utils/contestantUtils.js
@@ -3,12 +3,25 @@ import { generateContestantRoundScores } from '../../app/utils/contestantUtils'
 describe("generateContestantRoundScores", () => {
     it("Should work with simple defaults", () => {
         // Arrange
+        const rounds = 0
 
         // Act
-        const result = generateContestantRoundScores([], 0, "")
+        const result = generateContestantRoundScores([], rounds, "")
 
         // Assert
         expect(result).not.toBeNull()
         expect(result.length).toBe(0)
+    })
+
+    it("Should work with one round", () => {
+        // Arrange
+        const rounds = 1
+
+        // Act
+        const result = generateContestantRoundScores([], rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(1)
     })
 })
