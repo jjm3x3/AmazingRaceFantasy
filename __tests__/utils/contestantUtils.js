badc13c86dd8125f7951aeb52992ba46e200fbf9
Jacob Meixner <jjm3333@gmail.com>
2024-03-09 17:29:58 -0800

    Add a teamList test

--- __tests__/utils/contestantUtils.js
+++ __tests__/utils/contestantUtils.js
@@ -1,12 +1,14 @@
 import { generateContestantRoundScores } from '../../app/utils/contestantUtils'
+import Team from '../../app/models/Team'
 
 describe("generateContestantRoundScores", () => {
     it("Should work with simple defaults", () => {
         // Arrange
+        const teamList  = []
         const rounds = 0
 
         // Act
-        const result = generateContestantRoundScores([], rounds, "")
+        const result = generateContestantRoundScores(teamList, rounds, "")
 
         // Assert
         expect(result).not.toBeNull()
@@ -15,10 +17,24 @@ describe("generateContestantRoundScores", () => {
 
     it("Should work with one round", () => {
         // Arrange
+        const teamList  = []
         const rounds = 1
 
         // Act
-        const result = generateContestantRoundScores([], rounds, "")
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(1)
+    })
+
+    it("Should work with one round and one team in the ranking", () => {
+        // Arrange
+        const teamList = [new Team({teamName: "name1_1 & name1_2"})]
+        const rounds = 1
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
 
         // Assert
         expect(result).not.toBeNull()
