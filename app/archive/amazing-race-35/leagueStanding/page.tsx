25921c8c46dd8434deb538abfc1a8fdbba7998e2
Jacob Meixner <jjm3333@gmail.com>
2024-03-10 19:13:20 -0700

    Add code to generate scores

--- app/archive/amazing-race-35/leagueStanding/page.tsx
+++ app/archive/amazing-race-35/leagueStanding/page.tsx
@@ -3,10 +3,11 @@ import { CONTESTANT_LEAGUE_DATA } from '../../../leagueData/AmazingRace_35'
 import { getWikipediaContestantDataFetcher } from '../../../utils/wikiFetch'
 import generateContestantRoundScores from '../../../generators/contestantRoundScoreGenerator'
 
-export default function LeagueStanding() {
+export default async function LeagueStanding() {
 
     const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL)
-    
+    const contestantScores = await generateContestantRoundScores(dataFetcher, CONTESTANT_LEAGUE_DATA)
+
     return (
         <h1>Here is the league standing: placeholder...</h1>
     )
