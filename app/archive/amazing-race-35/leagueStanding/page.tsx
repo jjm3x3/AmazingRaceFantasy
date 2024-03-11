6f24d89a6b35d3e535159f489dc3839a207b4838
Jacob Meixner <jjm3333@gmail.com>
2024-03-10 19:12:17 -0700

    Add new imports to page

--- app/archive/amazing-race-35/leagueStanding/page.tsx
+++ app/archive/amazing-race-35/leagueStanding/page.tsx
@@ -1,5 +1,7 @@
 import { WIKI_API_URL } from '../../../leagueConfiguration/AmazingRace_35'
+import { CONTESTANT_LEAGUE_DATA } from '../../../leagueData/AmazingRace_35'
 import { getWikipediaContestantDataFetcher } from '../../../utils/wikiFetch'
+import generateContestantRoundScores from '../../../generators/contestantRoundScoreGenerator'
 
 export default function LeagueStanding() {
 
