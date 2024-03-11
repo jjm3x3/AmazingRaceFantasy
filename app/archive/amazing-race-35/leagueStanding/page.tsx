f70c7986d7cb9c4d09e9258aef044b6b193e4057
Jacob Meixner <jjm3333@gmail.com>
2024-03-10 19:03:42 -0700

    Import config and data fetcher to start pulling data

--- app/archive/amazing-race-35/leagueStanding/page.tsx
+++ app/archive/amazing-race-35/leagueStanding/page.tsx
@@ -1,4 +1,9 @@
+import { WIKI_API_URL } from '../../../leagueConfiguration/AmazingRace_35'
+import { getWikipediaContestantDataFetcher } from '../../../utils/wikiFetch'
+
 export default function LeagueStanding() {
+
+    const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL)
     
     return (
         <h1>Here is the league standing: placeholder...</h1>
