b337e8887286fcf7b70d99c36bea870d86355473
Jacob Meixner <jjm3333@gmail.com>
2024-03-16 15:06:42 -0700

    Update the current sesons leagueStanding page

--- app/leagueStanding/page.tsx
+++ app/leagueStanding/page.tsx
@@ -1,6 +1,18 @@
-export default function LeagueStanding() {
-    
+import { WIKI_API_URL } from '../leagueConfiguration/AmazingRace_36'
+import { CONTESTANT_LEAGUE_DATA } from '../leagueData/AmazingRace_36'
+import { getWikipediaContestantDataFetcher } from '../utils/wikiFetch'
+import generateContestantRoundScoreComponent from '../generators/contestantRoundScoreGenerator'
+
+export default async function LeagueStanding() {
+
+    const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL)
+    const contestantScoresComponent = await generateContestantRoundScoreComponent(dataFetcher, CONTESTANT_LEAGUE_DATA)
+
     return (
-        <h1>Here is the league standing: placeholder...</h1>
+        <div>
+            <h1 className="text-3xl text-center">Here is the league standing</h1>
+            {contestantScoresComponent}
+        </div>
     )
 }
+
