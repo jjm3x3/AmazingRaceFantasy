5210f0282c56e6963c63e84c1a2834cd4269ca8a
Jacob Meixner <jjm3333@gmail.com>
2024-03-16 16:30:07 -0700

    Remove image

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,6 @@ export default function Home() {
 
     return (
       <div>
-          <picture>
-            {imageSources.map((source,index) => ( <source key={index} srcSet={source.src} media={source.media} />
-            ))}
-            <Image
-              src="/MeixnerAmzingRaceFantacyLeageHome.svg"
-              fill
-              alt="Landing Page which has the title of 'Meixner's Amazing Race Fantasy Legue' and the subtitle 'A web tool to help run an Amazing Race fantasy legue'"
-              >
-            </Image>
-          </picture>
           <div className="menu-tray">
             <div className="menu-box">
               <Link className="menu-item" href="/contestants">Contestant List</Link>
