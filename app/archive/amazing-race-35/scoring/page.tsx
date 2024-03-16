4c57d41d150aabb6dfe3220a5776e65725e2f468
Jacob Meixner <jjm3333@gmail.com>
2024-03-16 15:32:45 -0700

    Add a notice to the bottom of old league w/ link to google sheet

--- app/archive/amazing-race-35/scoring/page.tsx
+++ app/archive/amazing-race-35/scoring/page.tsx
@@ -1,5 +1,4 @@
-
-import { WIKI_API_URL } from '../../../leagueConfiguration/AmazingRace_35'
+import { WIKI_API_URL, GOOGLE_SHEET_URL } from '../../../leagueConfiguration/AmazingRace_35'
 import { CONTESTANT_LEAGUE_DATA } from '../../../leagueData/AmazingRace_35'
 import ContestantSelector from '../../../components/contestantSelector'
 import { getWikipediaContestantDataFetcher } from '../../../utils/wikiFetch'
@@ -15,6 +14,7 @@ export default async function Scoring() {
             <h1 className="text-2xl text-center">Current Scoring for</h1>
             <br/>
             <ContestantSelector listOfContestantRoundLists={listOfContestantRoundLists}/>
+            <p>This seaon was largely managed by Andrew Jaicks using <a className="standard-link" href={GOOGLE_SHEET_URL}>this google sheet</a> which was populdated by him based on SMSs sent by the league contestants.</p>
         </div>
     )
 }
