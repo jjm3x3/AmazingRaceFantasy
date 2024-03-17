045da89aae2c889cf480f4f4dcb70b375d673978
Jacob Meixner <jjm3333@gmail.com>
2024-03-17 11:20:36 -0700

    Add Antoinette's ranking

--- app/leagueData/AmazingRace_36.tsx
+++ app/leagueData/AmazingRace_36.tsx
@@ -2,7 +2,13 @@
 // Contestant Ranking
 export const CONTESTANT_LEAGUE_DATA = [
     {
+        name: "Antoinette",
+        ranking: [ "Rod Gardner & Leticia Gardner", "Ricky Rotandi & Cesar Aldrete", "Derek Williams & Shelisa Williams", "Kishori Turner & Karishma Cordero", "Anthony Smith & Bailey Smith", "Yvonne Chavez & Melissa Main", "Sunny Pulver & Bizzy Smith", "Michelle Clark & Sean Clark", "Juan Villa & Shane Bilek", "Angie Butler & Danny Butler", "Amber Craven & Vinny Cagungun", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
+    },
+    {
         name: "Jacob",
         ranking: [ "Rod Gardner & Leticia Gardner", "Derek Williams & Shelisa Williams", "Ricky Rotandi & Cesar Aldrete", "Anthony Smith & Bailey Smith", "Juan Villa & Shane Bilek", "Kishori Turner & Karishma Cordero", "Angie Butler & Danny Butler", "Michelle Clark & Sean Clark", "Yvonne Chavez & Melissa Main", "Amber Craven & Vinny Cagungun", "Sunny Pulver & Bizzy Smith", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
     }
+
+
 ]
