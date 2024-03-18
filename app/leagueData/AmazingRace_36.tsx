b8a55d08cf2540fce7b3ffc52383e6495c4f3d8e
Jacob Meixner <jjm3333@gmail.com>
2024-03-17 19:13:06 -0700

    Add Cindy's ranking

--- app/leagueData/AmazingRace_36.tsx
+++ app/leagueData/AmazingRace_36.tsx
@@ -6,6 +6,10 @@ export const CONTESTANT_LEAGUE_DATA = [
         ranking: [ "Rod Gardner & Leticia Gardner", "Ricky Rotandi & Cesar Aldrete", "Derek Williams & Shelisa Williams", "Kishori Turner & Karishma Cordero", "Anthony Smith & Bailey Smith", "Yvonne Chavez & Melissa Main", "Sunny Pulver & Bizzy Smith", "Michelle Clark & Sean Clark", "Juan Villa & Shane Bilek", "Angie Butler & Danny Butler", "Amber Craven & Vinny Cagungun", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
     },
     {
+        name: "Cindy",
+        ranking: [ "Juan Villa & Shane Bilek", "Ricky Rotandi & Cesar Aldrete", "Rod Gardner & Leticia Gardner", "Derek Williams & Shelisa Williams", "Kishori Turner & Karishma Cordero", "Yvonne Chavez & Melissa Main", "Michelle Clark & Sean Clark", "Sunny Pulver & Bizzy Smith", "Amber Craven & Vinny Cagungun", "Anthony Smith & Bailey Smith", "Angie Butler & Danny Butler", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
+    },
+    {
         name: "Jacob",
         ranking: [ "Rod Gardner & Leticia Gardner", "Derek Williams & Shelisa Williams", "Ricky Rotandi & Cesar Aldrete", "Anthony Smith & Bailey Smith", "Juan Villa & Shane Bilek", "Kishori Turner & Karishma Cordero", "Angie Butler & Danny Butler", "Michelle Clark & Sean Clark", "Yvonne Chavez & Melissa Main", "Amber Craven & Vinny Cagungun", "Sunny Pulver & Bizzy Smith", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
     }
