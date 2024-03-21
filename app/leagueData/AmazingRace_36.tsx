fb9ab3fd7488c49e95163183b99ea447878df278
jjm3x3 <jjm3x3@users.noreply.github.com>
2024-03-20 22:24:44 -0700

    Feature/add contestant ranking (#76)
    * Add Jacob's ranking
    
    * Add Antoinette's ranking
    
    * Add Cindy's ranking
    
    * Add Jim's ranking
    
    * Add Anita's scoring
    
    * Add Rachel's ranking

--- app/leagueData/AmazingRace_36.tsx
+++ app/leagueData/AmazingRace_36.tsx
@@ -2,7 +2,27 @@
 // Contestant Ranking
 export const CONTESTANT_LEAGUE_DATA = [
     {
-        name: "Nobody",
-        ranking: []
+        name: "Anita",
+        ranking: [ "Rod Gardner & Leticia Gardner", "Ricky Rotandi & Cesar Aldrete", "Juan Villa & Shane Bilek", "Sunny Pulver & Bizzy Smith", "Derek Williams & Shelisa Williams", "Michelle Clark & Sean Clark", "Yvonne Chavez & Melissa Main", "Kishori Turner & Karishma Cordero", "Anthony Smith & Bailey Smith", "Angie Butler & Danny Butler", "Amber Craven & Vinny Cagungun", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
+    },
+    {
+        name: "Antoinette",
+        ranking: [ "Rod Gardner & Leticia Gardner", "Ricky Rotandi & Cesar Aldrete", "Derek Williams & Shelisa Williams", "Kishori Turner & Karishma Cordero", "Anthony Smith & Bailey Smith", "Yvonne Chavez & Melissa Main", "Sunny Pulver & Bizzy Smith", "Michelle Clark & Sean Clark", "Juan Villa & Shane Bilek", "Angie Butler & Danny Butler", "Amber Craven & Vinny Cagungun", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
+    },
+    {
+        name: "Cindy",
+        ranking: [ "Juan Villa & Shane Bilek", "Ricky Rotandi & Cesar Aldrete", "Rod Gardner & Leticia Gardner", "Derek Williams & Shelisa Williams", "Kishori Turner & Karishma Cordero", "Yvonne Chavez & Melissa Main", "Michelle Clark & Sean Clark", "Sunny Pulver & Bizzy Smith", "Amber Craven & Vinny Cagungun", "Anthony Smith & Bailey Smith", "Angie Butler & Danny Butler", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
+    },
+    {
+        name: "Jacob",
+        ranking: [ "Rod Gardner & Leticia Gardner", "Derek Williams & Shelisa Williams", "Ricky Rotandi & Cesar Aldrete", "Anthony Smith & Bailey Smith", "Juan Villa & Shane Bilek", "Kishori Turner & Karishma Cordero", "Angie Butler & Danny Butler", "Michelle Clark & Sean Clark", "Yvonne Chavez & Melissa Main", "Amber Craven & Vinny Cagungun", "Sunny Pulver & Bizzy Smith", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
+    },
+    {
+        name: "Jim",
+        ranking: [ "Ricky Rotandi & Cesar Aldrete", "Derek Williams & Shelisa Williams", "Rod Gardner & Leticia Gardner", "Michelle Clark & Sean Clark", "Juan Villa & Shane Bilek", "Chris Foster & Mary Cardona-Foster", "Sunny Pulver & Bizzy Smith", "Kishori Turner & Karishma Cordero", "Anthony Smith & Bailey Smith", "Yvonne Chavez & Melissa Main", "Amber Craven & Vinny Cagungun", "Angie Butler & Danny Butler", "Maya Mody & Rohan Mody" ]
+    },
+    {
+        name: "Rachel",
+        ranking: ["Yvonne Chavez & Melissa Main", "Amber Craven & Vinny Cagungun", "Derek Williams & Shelisa Williams", "Ricky Rotandi & Cesar Aldrete", "Michelle Clark & Sean Clark", "Juan Villa & Shane Bilek", "Kishori Turner & Karishma Cordero", "Rod Gardner & Leticia Gardner", "Anthony Smith & Bailey Smith", "Sunny Pulver & Bizzy Smith", "Angie Butler & Danny Butler", "Chris Foster & Mary Cardona-Foster", "Maya Mody & Rohan Mody"]
     }
 ]
