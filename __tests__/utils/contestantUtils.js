783ec5d571e8ff131efaf7f32637de61fff12902
jjm3x3 <jjm3x3@users.noreply.github.com>
2024-03-20 22:29:08 -0700

    Feature/extract contestant score generation (#72)
    * Add a contestantUtils for league data generation
    
    * Update contestantRoundListGenerator to use contestantUtils
    
    * Add an extra typing
    
    * Add a real simple dummy test
    
    * Add a test for a single round
    
    * Add a teamList test
    
    * Add some more sensible assertions
    
    * Add a test to demonstrate some scoring taking place
    
    * Add a test to show when multiple teams should contribute to a single roundScore
    
    * Add the final test to demonstrate how this scoring works

---
+++ __tests__/utils/contestantUtils.js
@@ -0,0 +1,120 @@
+import { generateContestantRoundScores } from '../../app/utils/contestantUtils'
+import Team from '../../app/models/Team'
+
+describe("generateContestantRoundScores", () => {
+    it("Should work with simple defaults", () => {
+        // Arrange
+        const teamList  = []
+        const rounds = 0
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(0)
+    })
+
+    it("Should work with one round", () => {
+        // Arrange
+        const teamList  = []
+        const rounds = 1
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(1)
+        expect(result[0].contestantRoundData).not.toBeNull()
+        expect(result[0].contestantRoundData.length).toBe(1)
+    })
+
+    it("Should work with one round and one team in the ranking", () => {
+        // Arrange
+        const teamList = [new Team({teamName: "name1_1 & name1_2"})]
+        const rounds = 1
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(1)
+        expect(result[0].contestantRoundData).not.toBeNull()
+        expect(result[0].contestantRoundData.length).toBe(1)
+    })
+
+    it("Should output some score when there is one", () => {
+        // Arrange
+        let exampleTeam = new Team({teamName: "name1_1 & name1_2", isParticipating: true, eliminationOrder: 0})
+        let exampleTeam2 = new Team({teamName: "name2_1 & name2_2", isParticipating: true, eliminationOrder: 0})
+
+        const teamList = [exampleTeam, exampleTeam2]
+        const rounds = 1
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(1)
+        expect(result[0]).not.toBeNull()
+        expect(result[0].contestantRoundData).not.toBeNull()
+        expect(result[0].contestantRoundData.length).toBe(1)
+        expect(result[0].contestantRoundData[0].roundScore).toBe(10)
+        expect(result[0].contestantRoundData[0].totalScore).toBe(10)
+    })
+
+    it("Should add multiple teams to one round score when the shouldBeScored", () => {
+        // Arrange
+        let exampleTeam = new Team({teamName: "name1_1 & name1_2", isParticipating: true, eliminationOrder: 0})
+        let exampleTeam2 = new Team({teamName: "name2_1 & name2_2", isParticipating: true, eliminationOrder: 0})
+        let exampleTeam3 = new Team({teamName: "name2_1 & name2_2", isParticipating: true, eliminationOrder: 0})
+
+        const teamList = [exampleTeam, exampleTeam2, exampleTeam3]
+        const rounds = 1
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        expect(result.length).toBe(1)
+        expect(result[0]).not.toBeNull()
+        expect(result[0].contestantRoundData).not.toBeNull()
+        expect(result[0].contestantRoundData.length).toBe(1)
+        expect(result[0].contestantRoundData[0].roundScore).toBe(20)
+        expect(result[0].contestantRoundData[0].totalScore).toBe(20)
+    })
+
+    it("Should accumulate total score over multiple rounds and should remove at least one team per round", () => {
+        // Arrange
+        let exampleTeam = new Team({teamName: "name1_1 & name1_2", isParticipating: true, eliminationOrder: 0})
+        let exampleTeam2 = new Team({teamName: "name2_1 & name2_2", isParticipating: true, eliminationOrder: 0})
+        let exampleTeam3 = new Team({teamName: "name2_1 & name2_2", isParticipating: true, eliminationOrder: 0})
+
+        const teamList = [exampleTeam, exampleTeam2, exampleTeam3]
+        const rounds = 2
+
+        // Act
+        const result = generateContestantRoundScores(teamList, rounds, "")
+
+        // Assert
+        expect(result).not.toBeNull()
+        // assert 2 rounds
+        expect(result.length).toBe(2)
+        // round1 assertions
+        expect(result[0]).not.toBeNull()
+        expect(result[0].contestantRoundData).not.toBeNull()
+        expect(result[0].contestantRoundData.length).toBe(1)
+        expect(result[0].contestantRoundData[0].roundScore).toBe(20)
+        expect(result[0].contestantRoundData[0].totalScore).toBe(20)
+        // round2 assertions
+        expect(result[1]).not.toBeNull()
+        expect(result[1].contestantRoundData).not.toBeNull()
+        expect(result[1].contestantRoundData.length).toBe(1)
+        expect(result[1].contestantRoundData[0].roundScore).toBe(10)
+        expect(result[1].contestantRoundData[0].totalScore).toBe(30)
+    })
+})
