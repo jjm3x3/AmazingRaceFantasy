8d63e367a9895f4d938db71c38918d3305e2369c
Jacob Meixner <jjm3333@gmail.com>
2024-04-05 21:59:17 -0700

    Set the value to 1hr

--- app/utils/wikiFetch.tsx
+++ app/utils/wikiFetch.tsx
@@ -15,7 +15,7 @@ export interface IWikipediaContestantData {
 }
 
 async function fetchWikipediaData(wikiUrl: string): Promise<IWikipediaData> {
-    const response = await fetch(wikiUrl, { next: { revalidate: 10 } })
+    const response = await fetch(wikiUrl, { next: { revalidate: 3600 } })
     const data = await response.json()
     return data
 }
