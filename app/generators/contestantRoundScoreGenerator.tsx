92cfa581c6df819cea44936e5189062314e1b116
jjm3x3 <jjm3x3@users.noreply.github.com>
2024-04-10 22:49:51 -0700

    Feature/add league standing page (#80)
    * Add a new placeholder page for league standing
    
    * Add a link to new page from the home page
    
    * Add text to make it clear it's a placeholder
    
    * Copy page into the archive season to work on it from there
    
    * Import config and data fetcher to start pulling data
    
    * Copy roundList generator as is into roundScore generator
    
    * Make modifications to build up LeagueStanding
    
    * Rename function accordingly
    
    * Clean up imports
    
    * Add new imports to page
    
    * Add code to generate scores
    
    * Start to build a page like the rest
    
    * Add a week heading for each round
    
    * Add in displaying each contestants total score in a row
    
    * Fix week number to be human friendly
    
    * Add a new funcion to generate the "component"
    
    * Switch to letting the generator function generate the component
    
    * Update the current sesons leagueStanding page
    
    * Move current league page
    
    * Update linke on homepage
    
    * Move archive page to correct path

---
+++ app/generators/contestantRoundScoreGenerator.tsx
@@ -0,0 +1,62 @@
+import { IWikipediaContestantData } from "../utils/wikiFetch"
+import { getTeamList, ITeam } from "../utils/wikiQuery"
+import Team from '../models/Team'
+import LeagueStanding from '../models/LeagueStanding'
+
+interface Dictionary<T> {
+    [Key: string]: T;
+}
+
+async function generateContestantRoundScores(dataFetcher: () => Promise<IWikipediaContestantData[]>, listOfContestantLeagueData: any[]) {
+
+    const wikiContestants = await dataFetcher()
+    const pageData = getTeamList(wikiContestants)
+
+    const teamDictionary = pageData.props.runners.reduce((acc: Dictionary<ITeam>, t: ITeam) => {
+            acc[Team.getKey(t.teamName)] = t
+
+            return acc
+        }, {})
+
+    const numberOfRounds = pageData.props.runners.reduce(
+        (acc: number, x: ITeam) => {
+            return x.eliminationOrder > acc ? x.eliminationOrder : acc
+        }, 0)
+
+    const result: LeagueStanding = new LeagueStanding()
+
+    listOfContestantLeagueData.map(contestant => {
+
+        const currentSelectedContestantTeamsList = contestant.ranking.map((x: string) => {
+            const foundTeam = teamDictionary[Team.getKey(x)]
+            return foundTeam
+        })
+
+        result.addContestantRoundScores(currentSelectedContestantTeamsList, numberOfRounds, contestant.name)
+
+    })
+
+    return result
+}
+
+export default async function generateContestantRoundScoreComponent(dataFetcher: () => Promise<IWikipediaContestantData[]>, listOfContestantLeagueData: any[]) {
+
+    const contestantScores = await generateContestantRoundScores(dataFetcher, listOfContestantLeagueData)
+
+    return contestantScores.rounds.map(roundData => {
+        return <>
+            <h1 className="text-2xl text-center">Week {roundData.round+1}</h1>
+            <br/>
+            <div className="flex flex-row">
+                {roundData.contestantRoundData.map(contestantRound => {
+                    return <div className="basis-1/6">
+                        <h1 className="text-1xl text-center">{contestantRound.name}</h1>
+                        <p className="text-center">{contestantRound.totalScore}</p>
+                    </div>
+                })}
+            </div>
+            <br/>
+            <br/>
+        </>
+    })
+}
