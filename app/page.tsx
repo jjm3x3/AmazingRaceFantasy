411efd203ec19e8f8c5169b5dc0a717d80348226
Jacob Meixner <jjm3333@gmail.com>
2024-04-10 21:15:28 -0700

    Add links to current league pages

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,17 @@ export default function Home() {
             <div className="title-bar">
                 <p className="page-title">X Factor Fantasy</p>
             </div>
-            <p className="site-notice">
-                Welcome to X Factor Fantasy! A new season of the Amazing Race has just begun!
-            </p>
-            <p className="current-league-heading" >Links For The Current League</p>
+            <div className="main-content">
+                <p className="site-notice">
+                    Welcome to X Factor Fantasy! A new season of the Amazing Race has just begun!
+                    <br/>
+                    <a className="standard-link" href="/scoring"> Jump Into The Action</a>
+                </p>
+                <p className="current-league-heading" >Links For The Current League</p>
+                <Link className="standard-link league-page-link" href="/contestants">Contestants</Link>
+                <Link className="standard-link league-page-link" href="/scoring">Scoring</Link>
+                <Link className="standard-link league-page-link" href="/league-standing">League Standing</Link>
+            </div>
         </div>
     )
 }
