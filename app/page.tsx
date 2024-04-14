13c1d89fb82d7670a2478813d349688094bf2db9
Jacob Meixner <jjm3333@gmail.com>
2024-04-14 15:02:33 -0700

    Put the links in a flex row

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,11 @@ export default function Home() {
                     <a className="standard-link" href="/scoring"> Jump Into The Action</a>
                 </p>
                 <p className="current-league-heading" >Links For The Current League</p>
-                <Link className="standard-link league-page-link" href="/contestants">Contestants</Link>
-                <Link className="standard-link league-page-link" href="/scoring">Scoring</Link>
-                <Link className="standard-link league-page-link" href="/league-standing">League Standing</Link>
+                <div className="flex flex-row">
+                    <Link className="standard-link league-page-link" href="/contestants">Contestants</Link>
+                    <Link className="standard-link league-page-link" href="/scoring">Scoring</Link>
+                    <Link className="standard-link league-page-link" href="/league-standing">League Standing</Link>
+                </div>
             </main>
         </div>
     )
