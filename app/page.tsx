0942c116133bf2580a13feba2b7874e58e3cd44b
Jacob Meixner <jjm3333@gmail.com>
2024-04-17 21:26:47 -0700

    Swap out the div for header

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
 
     return (
         <div>
-            <div className="title-bar">
+            <header className="title-bar">
                 <p className="page-title">X Factor Fantasy</p>
-            </div>
+            </header>
             <div className="main-content">
                 <p className="site-notice">
                     Welcome to X Factor Fantasy! A new season of the Amazing Race has just begun!
