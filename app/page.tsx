f9c0bdae5665074487f76c8b90c8b45458659416
jjm3x3 <jjm3x3@users.noreply.github.com>
2024-04-21 18:41:20 -0700

    Feature/add archived leagues (#87)
    * Try adding no-wrap
    
    * Put the links in a flex row
    
    * take down the padding by 5px
    
    * Remove images used for the first version of the site
    
    * The rest of the owl
    
    * Fix the name of one css class
    
    * Add type as suggested where one can't be inferred

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Link from 'next/link'
+import fs from 'fs'
 
 export default function Home() {
+
+    const currentDirFilesList = fs.readdirSync(__dirname)
+    let archiveDirFilesList: string[] = []
+
+    if (currentDirFilesList.includes("archive")) {
+        console.log("Yay there is an 'archive' folder and it's contents are:")
+        archiveDirFilesList = fs.readdirSync(__dirname+"/archive")
+        console.log(archiveDirFilesList)
+    }
 
     return (
         <div>
@@ -13,10 +23,28 @@ export default function Home() {
                     <br/>
                     <a className="standard-link" href="/scoring"> Jump Into The Action</a>
                 </p>
-                <p className="current-league-heading" >Links For The Current League</p>
-                <Link className="standard-link league-page-link" href="/contestants">Contestants</Link>
-                <Link className="standard-link league-page-link" href="/scoring">Scoring</Link>
-                <Link className="standard-link league-page-link" href="/league-standing">League Standing</Link>
+                <p className="league-link-heading" >Links For The Current League</p>
+                <div className="flex flex-row">
+                    <Link className="standard-link league-page-link" href="/contestants">Contestants</Link>
+                    <Link className="standard-link league-page-link" href="/scoring">Scoring</Link>
+                    <Link className="standard-link league-page-link" href="/league-standing">League Standing</Link>
+                </div>
+                {archiveDirFilesList.map(s => {
+                    const friendlyName = s.replaceAll("-", " ")
+                    // TODO capitalize show name
+                    const contestantsPath = "/archive/" + s + "/contestants"
+                    const scoringPath = "/archive/" + s + "/scoring"
+                    const leagueStandingPath = "/archive/" + s +"/league-standing"
+
+                    return <>
+                        <p className="league-link-heading" >Links For {friendlyName} League</p>
+                        <div className="flex flex-row">
+                            <Link className="standard-link league-page-link" href={contestantsPath}>Contestants</Link>
+                            <Link className="standard-link league-page-link" href={scoringPath}>Scoring</Link>
+                            <Link className="standard-link league-page-link" href={leagueStandingPath}>League Standing</Link>
+                        </div>
+                    </>
+                })}
             </main>
         </div>
     )
