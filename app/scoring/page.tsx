73d15c3d90fbcce542914f7336dfd3eda50a7914
Jacob Meixner <jjm3333@gmail.com>
2024-04-28 21:49:56 -0700

    Update the title for the scoring page

--- app/scoring/page.tsx
+++ app/scoring/page.tsx
@@ -12,7 +12,7 @@ export default async function Scoring() {
 
     return (
         <div>
-            <h1 className="text-2xl text-center">Current Scoring for</h1>
+            <h1 className="text-2xl text-center">Scoring For</h1>
             <br/>
             <ContestantSelector listOfContestantRoundLists={listOfContestantRoundLists}/>
             <p>This season's contestant data has been sourced from <a className="standard-link" href={GOOGLE_SHEET_URL}>this google sheet</a> which was populated using a google form.</p>
