494d5b384a7c55ee014a4af6fb2e6b88898a9cba
Jacob Meixner <jjm3333@gmail.com>
2024-04-28 22:10:41 -0700

    Add a link back to home on the page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navigation from './components/navigation/navigation'
 import { getPages } from '@/app/utils/pages';
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <header>
-            <p className="page-title">X Factor Fantasy</p>
+            <Link href="/"><p className="page-title">X Factor Fantasy</p></Link>
             {pages.length > 0 && <Navigation pages={pages} />}
         </header>
         <br/>
