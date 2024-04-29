76cddee49d3214b7427a675ecc1f6957911cf844
Jacob Meixner <jjm3333@gmail.com>
2024-04-28 21:39:20 -0700

    Move Header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navigation from './components/navigation/navigation'
+import { getPages } from '@/app/utils/pages';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +16,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const pages = getPages() || [];
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+            <p className="page-title">X Factor Fantasy</p>
+            {pages.length > 0 && <Navigation pages={pages} />}
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 import Link from 'next/link'
-import Navigation from './components/navigation/navigation'
 import IPage from './models/IPage';
 import ISubpage from './models/ISubpage';
 import { getPages } from '@/app/utils/pages';
 
 export default function Home() {
     const pages = getPages() || [];
+
     return (
         <div>
-            <header>
-                <p className="page-title">X Factor Fantasy</p>
-                {pages.length > 0 && <Navigation pages={pages} />}
-            </header>
             <main>
                 <p className="site-notice">
                     Welcome to X Factor Fantasy! A new season of the Amazing Race has just begun!
