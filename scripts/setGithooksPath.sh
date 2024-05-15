a51e2a86957f48e20afb2b2edf47af1e1d5e85a0
Antoinette Janus <antoinettejanus@lululemon.com>
2024-05-15 11:02:05 -0700

    Changed the path to be github recommended

--- scripts/setGithooksPath.sh
+++ scripts/setGithooksPath.sh
@@ -12,13 +12,12 @@
 GIT_NUMBER_STRING=($(git -v | tr -dc '[. [:digit:]]'))
 GIT_VERSION_STRING="${GIT_NUMBER_STRING[0]}"
 GIT_MIN_VERSION="2.9"
-GITHOOKS_PATH=".githooks"
+GITHOOKS_PATH="./.github/hooks"
 if (( $(echo "$GIT_NUMBER_STRING $GIT_MIN_VERSION" | awk '{print ($1 >= $2)}') )); then
     ($(git config --local core.hooksPath $GITHOOKS_PATH))
-    echo "Git hooks path changed from .git/hooks to $GITHOOKS_PATH"
     exit 1
 else
     ($(find .git/hooks -type l -exec rm {} \;))
-    ($(find ${GITHOOKS_PATH} -type f -exec ln -sf ../../{} .git/hooks/ \;))
-    echo "Git hooks path changed from .git/hooks to $GITHOOKS_PATH"
+    ($(find ${GITHOOKS_PATH} -type f -exec ln -sf ../../{} ${GITHOOKS_PATH} \;))
 fi
+echo "Git hooks path changed from .git/hooks to $GITHOOKS_PATH"
