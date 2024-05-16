7d3034cfdcc6a78984f64d0e14f4fec062ee1d7a
Antoinette Janus <antoinettejanus@lululemon.com>
2024-05-16 14:20:03 -0700

    Updated script to check for pre-existing path

--- scripts/setGithooksPath.sh
+++ scripts/setGithooksPath.sh
@@ -12,11 +12,16 @@
 GIT_NUMBER_STRING=($(git -v | tr -dc '[. [:digit:]]'))
 GIT_VERSION_STRING="${GIT_NUMBER_STRING[0]}"
 GIT_MIN_VERSION="2.9"
+CURRENT_GITHOOKS_PATH=($(git rev-parse --git-path hooks))
 GITHOOKS_PATH="./.github/hooks"
-if (( $(echo "$GIT_NUMBER_STRING $GIT_MIN_VERSION" | awk '{print ($1 >= $2)}') )); then
-    ($(git config --local core.hooksPath $GITHOOKS_PATH))
+if ["$CURRENT_GITHOOKS_PATH" != "$GITHOOKS_PATH"]; then
+    if (( $(echo "$GIT_NUMBER_STRING $GIT_MIN_VERSION" | awk '{print ($1 >= $2)}') )); then
+        ($(git config --local core.hooksPath $GITHOOKS_PATH))
+    else
+        ($(find .git/hooks -type l -exec rm {} \;))
+        ($(find ${GITHOOKS_PATH} -type f -exec ln -sf ../../{} ${GITHOOKS_PATH} \;))
+    fi
+    echo "Git hooks path changed from .git/hooks to $GITHOOKS_PATH"
 else
-    ($(find .git/hooks -type l -exec rm {} \;))
-    ($(find ${GITHOOKS_PATH} -type f -exec ln -sf ../../{} ${GITHOOKS_PATH} \;))
+    echo "Git hooks path is already $GITHOOKS_PATH"
 fi
-echo "Git hooks path changed from .git/hooks to $GITHOOKS_PATH"
