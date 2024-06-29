ce7b9fa01fbc4a124fb9ae8eaad0901c72a5b4ac
Jacob Meixner <jjm3333@gmail.com>
2024-06-29 15:01:29 -0700

    Remove collating behavior to join contestants into teams

--- app/utils/wikiQuery.tsx
+++ app/utils/wikiQuery.tsx
@@ -78,48 +78,44 @@ export function isPartialContestantData(contestantRowData: ITableRowData): boole
 
 export function getCompetingEntityList(contestantData :ITableRowData[]): any {
 
-    const contestants: ITeam[] = []
+    const contestants: CompetingEntity[] = []
 
     contestantData.forEach((element, index) => {
 
+        console.log(element)
         const status = element.col4
-        let teamName = element.name
+        console.log("status is: " + status)
+        let teamName = element.name || element.name2
 
         if (status === null || status === undefined) {
             throw new ReferenceError("Status is either null or undefined and it should not be")
         }
 
-        if (index % 2 == 0) {
-            let isParticipating = true
-            let eliminationOrder = 0
+        let isParticipating = true
+        let eliminationOrder = 0
 
-            if (status.toLowerCase().includes('eliminated')) {
-                isParticipating = false
-                eliminationOrder = Number(status.match(/Eliminated (\d+)/i)![1])
-            } else if (status.toLowerCase().includes("third")) {
-                isParticipating = false
-                eliminationOrder = (contestantData.length/2) - 2
-            } else if (status.toLowerCase().includes("runners-up")) {
-                isParticipating = false
-                eliminationOrder = (contestantData.length/2) - 1
-            }
+        if (status.toLowerCase().includes('eliminated')) {
+            isParticipating = false
+            eliminationOrder = Number(status.match(/Eliminated (\d+)/i)![1])
+        } else if (status.toLowerCase().includes("third")) {
+            isParticipating = false
+            eliminationOrder = (contestantData.length/2) - 2
+        } else if (status.toLowerCase().includes("runners-up")) {
+            isParticipating = false
+            eliminationOrder = (contestantData.length/2) - 1
+        }
 
-            const contestant: Team = new Team({
-                teamName: teamName,
-                relationship: element.col2,
-                isParticipating,
-                eliminationOrder
-            })
+        const contestant: Team = new Team({
+            teamName: teamName,
+            relationship: element.col2,
+            isParticipating,
+            eliminationOrder
+        })
 
-            if (contestant.teamName) {
-                contestants.push(contestant)
-            } else {
-                console.warn("Found a null contestant Name...")
-            }
+        if (contestant.teamName) {
+            contestants.push(contestant)
         } else {
-            if (index > 0) {
-                contestants[contestants.length-1].teamName += " & " + teamName
-            }
+            console.warn("Found a null contestant Name...")
         }
     })
 
