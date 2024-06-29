f8807e8eff76e8c2c06e1c771b95a51f5fe8550a
Jacob Meixner <jjm3333@gmail.com>
2024-06-29 14:58:23 -0700

    Add a full copy of getTeamList to start from

--- app/utils/wikiQuery.tsx
+++ app/utils/wikiQuery.tsx
@@ -76,3 +76,53 @@ export function isPartialContestantData(contestantRowData: ITableRowData): boole
     return (contestantRowData.name == null || contestantRowData.name === "") && (!contestantRowData.name2 || !contestantRowData.col2)
 }
 
+export function getCompetingEntityList(contestantData :ITableRowData[]): any {
+
+    const contestants: ITeam[] = []
+
+    contestantData.forEach((element, index) => {
+
+        const status = element.col4
+        let teamName = element.name
+
+        if (status === null || status === undefined) {
+            throw new ReferenceError("Status is either null or undefined and it should not be")
+        }
+
+        if (index % 2 == 0) {
+            let isParticipating = true
+            let eliminationOrder = 0
+
+            if (status.toLowerCase().includes('eliminated')) {
+                isParticipating = false
+                eliminationOrder = Number(status.match(/Eliminated (\d+)/i)![1])
+            } else if (status.toLowerCase().includes("third")) {
+                isParticipating = false
+                eliminationOrder = (contestantData.length/2) - 2
+            } else if (status.toLowerCase().includes("runners-up")) {
+                isParticipating = false
+                eliminationOrder = (contestantData.length/2) - 1
+            }
+
+            const contestant: Team = new Team({
+                teamName: teamName,
+                relationship: element.col2,
+                isParticipating,
+                eliminationOrder
+            })
+
+            if (contestant.teamName) {
+                contestants.push(contestant)
+            } else {
+                console.warn("Found a null contestant Name...")
+            }
+        } else {
+            if (index > 0) {
+                contestants[contestants.length-1].teamName += " & " + teamName
+            }
+        }
+    })
+
+    return { props: { runners: contestants }}
+}
+
