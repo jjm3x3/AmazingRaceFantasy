25ab68c767dd30a0ea276527b7b075340358db42
Jacob Meixner <jjm3333@gmail.com>
2024-07-14 09:10:36 -0700

    Add a random ranking to test with

--- app/leagueData/BigBrother_25.tsx
+++ app/leagueData/BigBrother_25.tsx
@@ -1,7 +1,8 @@
 
 // Contestant Ranking
 export const CONTESTANT_LEAGUE_DATA = [
-{
-        name: "Nobody",
-        ranking: []
-}]
+    {
+        name: "Random",
+        ranking: [ "Reilly Smedley", "Cory Wurtenberger", "Hisam Goueli", "Michael \"Red\" Utley", "Cirie Fields", "Jared Fields", "Bowie Jane", "Isabel \"Izzy\" Gleicher", "Matt Klotz", "Jagateshwar \"Jag\" Bains", "Blue Kim", "Cameron Hardin", "Mecole Hayes", "Kirsten Elwin", "America Lopez", "Felicia Cannon", "Luke Valentine" ]
+    }
+]
