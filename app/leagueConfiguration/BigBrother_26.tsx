d1d758061930d9b65156d7c2280d815cb6638097
Jacob Meixner <jjm3333@gmail.com>
2024-07-17 19:29:03 -0700

    Remove unnecessary google link

--- app/leagueConfiguration/BigBrother_26.tsx
+++ app/leagueConfiguration/BigBrother_26.tsx
@@ -2,5 +2,3 @@
 export const WIKI_API_URL = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Big_Brother_26_(American_season)"
 
 export const WIKI_PAGE_URL = "https://en.wikipedia.org/wiki/Big_Brother_26_(American_season)"
-
-export const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/TBD"
