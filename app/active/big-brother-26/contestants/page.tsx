8599cc6bb859ccd33a50b093df212e369b0e69c4
Jacob Meixner <jjm3333@gmail.com>
2024-07-17 19:29:24 -0700

    Update import

--- app/active/big-brother-26/contestants/page.tsx
+++ app/active/big-brother-26/contestants/page.tsx
@@ -1,6 +1,6 @@
 import { getCompetingEntityList, ITeam } from "../../../utils/wikiQuery"
 import { getWikipediaContestantData } from "../../../utils/wikiFetch"
-import { WIKI_API_URL, WIKI_PAGE_URL } from '../../../leagueConfiguration/BigBrother_25'
+import { WIKI_API_URL, WIKI_PAGE_URL } from '../../../leagueConfiguration/BigBrother_26'
 
 export default async function Contestants() {
 
