7197e1fbf35d7db51f76c128e8783cd7e4445855
Jacob Meixner <jjm3333@gmail.com>
2024-07-23 23:57:37 -0700

    Antoinette added

--- app/leagueData/BigBrother_26.tsx
+++ app/leagueData/BigBrother_26.tsx
@@ -2,6 +2,10 @@
 // Contestant Ranking
 export const CONTESTANT_LEAGUE_DATA = [
     {
+        name: "Antoinette",
+        ranking: [ "Makensy Manbeck", "Brooklyn Rivera", "Quinn Martin", "Rubina Bernabe", "Tucker Des Lauriers", "Cam Sullivan-Brown", "Leah Peters", "T'kor Clottey", "Cedric Hodges", "Chelsie Baham", "Kimo Apaka", "Angela Murray", "Joseph Rodriguez", "Matt Hardeman", "Lisa Weintraub", "Kenney Kelly" ]
+    },
+    {
         name: "Duncan",
         ranking: [ "Leah Peters", "Quinn Martin", "Brooklyn Rivera", "Cam Sullivan-Brown", "Makensy Manbeck", "Chelsie Baham", "T'kor Clottey", "Joseph Rodriguez", "Tucker Des Lauriers", "Rubina Bernabe", "Lisa Weintraub", "Angela Murray", "Kimo Apaka", "Matt Hardeman", "Cedric Hodges", "Kenney Kelly" ]
     }
