dbd64c7c9e1c2d4540f8b0ae8af3e888fcf4a9f1
Jacob Meixner <jjm3333@gmail.com>
2024-07-23 17:53:34 -0700

    Inject getCompetingEntityList aggregator

--- app/active/big-brother-26/scoring/page.tsx
+++ app/active/big-brother-26/scoring/page.tsx
@@ -2,13 +2,14 @@ import { WIKI_API_URL, GOOGLE_SHEET_URL } from '../../../leagueConfiguration/Big
 import { CONTESTANT_LEAGUE_DATA } from '../../../leagueData/AmazingRace_36'
 import ContestantSelector from '../../../components/contestantSelector'
 import { getWikipediaContestantDataFetcher } from '../../../utils/wikiFetch'
+import { getCompetingEntityList } from "../../../utils/wikiQuery"
 import generateListOfContestantRoundLists from '../../../generators/contestantRoundListGenerator'
 
 export default async function Scoring() {
 
     const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL, "HouseGuests")
 
-    const listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, CONTESTANT_LEAGUE_DATA)
+    const listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, CONTESTANT_LEAGUE_DATA, getCompetingEntityList)
 
     return (
         <div>
