4bb58b0e4dcc9ce3fee1499c8e651097c08f6511
Jacob Meixner <jjm3333@gmail.com>
2024-07-23 17:59:39 -0700

    Add Duncun's ranking

--- app/leagueData/BigBrother_26.tsx
+++ app/leagueData/BigBrother_26.tsx
@@ -2,7 +2,7 @@
 // Contestant Ranking
 export const CONTESTANT_LEAGUE_DATA = [
     {
-        name: "Nobody",
-        ranking: []
+        name: "Duncan",
+        ranking: [ "Leah Peters", "Quinn Martin", "Brooklyn Rivera", "Cam Sullivan-Brown", "Makensy Manbeck", "Chelsie Baham", "T'kor Clottey", "Joseph Rodriguez", "Tucker Des Lauriers", "Rubina Bernabe", "Lisa Weintraub", "Angela Murray", "Kimo Apaka", "Matt Hardeman", "Cedric Hodges", "Kenney Kelly" ]
     }
 ]
