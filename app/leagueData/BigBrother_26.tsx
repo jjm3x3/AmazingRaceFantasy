944f3364759b3a3185305fbf7f448c7d9a7b07d0
Jacob Meixner <jjm3333@gmail.com>
2024-07-23 23:59:07 -0700

    Add Jacob's ranking

--- app/leagueData/BigBrother_26.tsx
+++ app/leagueData/BigBrother_26.tsx
@@ -8,5 +8,9 @@ export const CONTESTANT_LEAGUE_DATA = [
     {
         name: "Duncan",
         ranking: [ "Leah Peters", "Quinn Martin", "Brooklyn Rivera", "Cam Sullivan-Brown", "Makensy Manbeck", "Chelsie Baham", "T'kor Clottey", "Joseph Rodriguez", "Tucker Des Lauriers", "Rubina Bernabe", "Lisa Weintraub", "Angela Murray", "Kimo Apaka", "Matt Hardeman", "Cedric Hodges", "Kenney Kelly" ]
+    },
+    {
+        name: "Jacob",
+        ranking: [ "Rubina Bernabe", "Quinn Martin", "Makensy Manbeck", "Cam Sullivan-Brown", "Brooklyn Rivera", "T'kor Clottey", "Matt Hardeman", "Tucker Des Lauriers", "Leah Peters", "Cedric Hodges", "Joseph Rodriguez", "Kenney Kelly", "Kimo Apaka", "Lisa Weintraub", "Angela Murray", "Chelsie Baham" ]
     }
 ]
