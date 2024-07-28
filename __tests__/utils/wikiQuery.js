eb09918953565303349cb772129759850495f153
Jacob Meixner <jjm3333@gmail.com>
2024-07-27 19:13:27 -0700

    Remove unnecessary test

--- __tests__/utils/wikiQuery.js
+++ __tests__/utils/wikiQuery.js
@@ -558,35 +558,4 @@ describe('getCompetingEntityList', () => {
         expect(result.props.runners.length).toEqual(0)
         expect(result.props.runners.map(x => x.eliminationOrder)).not.toContain(0)
     })
-
-    // it('should create as many teams as it can alternating after finding the first and even adding a partial team when there is not a match at the end', () => {
-    //     const firstContestantsFirstName = "Some"
-    //     const secondContestantsFirstName = "SomeGuys"
-
-    //     const listOfContestants = [
-    //         {
-    //             col4: "Participating"
-    //         },
-    //         {
-    //             name: "hi my name is",
-    //             col4: "Participating"
-    //         },
-    //         {
-    //             name: null,
-    //             col4: "Participating"
-    //         },
-    //         {
-    //             name: "new person",
-    //             col4: "Participating"
-    //         },
-    //         {
-    //             name: "new person",
-    //             col4: "Participating"
-    //         },
-    //     ]
-
-    //     var result = getTeamList(listOfContestants)
-
-    //     expect(result.props.runners.map(x => x.eliminationOrder)).not.toContain(2)
-    // })
 })
