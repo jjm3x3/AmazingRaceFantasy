f2193fe199ffe83ee6361aa43db06a770931a985
Antoinette Janus <acj.gfx@gmail.com>
2024-08-19 10:27:29 -0700

    Removed commented out code

--- app/active/big-brother-26/league-standing/page.tsx
+++ app/active/big-brother-26/league-standing/page.tsx
@@ -7,30 +7,8 @@ import LeagueStandingTable from '../../../components/leagueStandingTable/leagueS
 
 export default async function LeagueStanding() {
     const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL, "HouseGuests")
-
     const listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, CONTESTANT_LEAGUE_DATA, getCompetingEntityList)
-    // type rowState = any[];
-    // const tableColNames: rowState = ["Rank", "Name", "Score"];
-    // const tableRows: rowState = [];
-    // const tableData = {
-    //     colsNames: tableColNames,
-    //     rows: tableRows
-    // }
-    // listOfContestantRoundLists.map(contestantData =>{
-    //     const tableContestantData = [contestantData.content.props.contestantName, contestantData.content.props.contestantRoundScores[contestantData.content.props.contestantRoundScores.length - 1].contestantRoundData[0].totalScore]
-    //     tableData.rows.push(tableContestantData);
-    // });
-
-    // tableData.rows.sort((a, b) => {
-    //     const aScore = a.filter((param:number|string) => typeof param === "number")[0];
-    //     const bScore = b.filter((param:number|string)=> typeof param === "number")[0];
-    //     return aScore > bScore ? aScore : bScore
-    // });
-
-    // tableData.rows.map((tableRow, index) => {
-    //     return tableRow.unshift(index+1);
-    // })
-
+    
     return <>
         <h1 className="text-3xl text-center">League Standing</h1>
         <br/>
