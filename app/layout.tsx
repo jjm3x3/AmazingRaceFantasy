96c6734ca79454c1b5ee0b9415e92ddb53bf4997
Antoinette Janus <acj.gfx@gmail.com>
2024-08-20 15:32:53 -0700

    Added navigation to every page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,32 @@
 import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { getPages } from '@/app/utils/pages';
+import Navigation from './components/navigation/navigation'
 
 export const metadata: Metadata = {
   title: 'X Factor League',
   description: 'A website for all of your fantasy league needs',
 }
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const pages = getPages() || [];
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <p className="page-title">X Factor Fantasy</p>
+          {pages.length > 0 && <Navigation pages={pages} />}
+        </header>
+        <main>
+          {children}
+1        </main>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Navigation from './components/navigation/navigation'
 import IPage from './models/IPage';
 import ISubpage from './models/ISubpage';
 import { getPages } from '@/app/utils/pages';
@@ -8,29 +7,23 @@ import "./styles/homepage.scss";
 export default function Home() {
     const pages = getPages() || [];
     return (
-        <div>
-            <header>
-                <p className="page-title">X Factor Fantasy</p>
-                {pages.length > 0 && <Navigation pages={pages} />}
-            </header>
-            <main>
-                <p className="site-notice">
-                    A new season of the Big Brother has just begun. If you are already expecting an invite, they should be sent out by 12AM 07-25-24. Then, you will have until the first eviction to submit your ranking.
-                </p>
-                {pages.map((p: IPage) => { 
-                    const keyName = p.name.toLowerCase().replaceAll(' ', '-');
-                    return (<div key={`links-section-${keyName}`}>
-                    <p className="league-link-heading" >Links For { p.name } League</p>
-                    <div className="md:flex md:flex-row">{
-                        p.subpages.map((pSub: ISubpage) => (
-                            <p className="standard-link-container md:basis-1/3">
-                                <Link className="standard-link" key={`links-section-${keyName}-link-${pSub.name.toLowerCase().replaceAll(' ', '-')}`} href={pSub.path}>{ pSub.name}</Link>
-                            </p>
-                        ))
-                    }</div>
-                </div>)
-                })}
-            </main>
-        </div>
+        <>
+            <p className="site-notice">
+                A new season of the Big Brother has just begun. If you are already expecting an invite, they should be sent out by 12AM 07-25-24. Then, you will have until the first eviction to submit your ranking.
+            </p>
+            {pages.map((p: IPage) => { 
+                const keyName = p.name.toLowerCase().replaceAll(' ', '-');
+                return (<div key={`links-section-${keyName}`}>
+                <p className="league-link-heading" >Links For { p.name } League</p>
+                <div className="md:flex md:flex-row">{
+                    p.subpages.map((pSub: ISubpage) => (
+                        <p className="standard-link-container md:basis-1/3">
+                            <Link className="standard-link" key={`links-section-${keyName}-link-${pSub.name.toLowerCase().replaceAll(' ', '-')}`} href={pSub.path}>{ pSub.name}</Link>
+                        </p>
+                    ))
+                }</div>
+            </div>)
+            })}
+        </>
     )
 }
