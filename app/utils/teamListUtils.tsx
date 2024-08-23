8a2ac2ac8653f81223dc9d0be6b47c320b3b7708
Antoinette Janus <acj.gfx@gmail.com>
2024-08-23 07:29:04 -0700

    Removed missed new line only file

--- app/utils/teamListUtils.tsx
+++ app/utils/teamListUtils.tsx
@@ -1,6 +1,7 @@
 import Team from '../models/Team'
 
 export function shouldBeScored(teamList: Team[], team: Team, roundNumber: number): boolean {
+
     const teamPosition = teamList.length - teamList.indexOf(team)
 
     const currentWeek = roundNumber+1
