51bc83c6f8bb87684557d63fd072a40a21b9c454
Antoinette Janus <acj.gfx@gmail.com>
2024-09-22 20:51:13 -0700

    Fixed

--- app/active/big-brother-26/league-standing/page.tsx
+++ app/active/big-brother-26/league-standing/page.tsx
@@ -2,12 +2,11 @@ import { WIKI_API_URL } from '../../../leagueConfiguration/BigBrother_26'
 import { CONTESTANT_LEAGUE_DATA } from '../../../leagueData/BigBrother_26'
 import { getWikipediaContestantDataFetcher } from '@/app/utils/wikiFetch';
 import generateListOfContestantRoundLists from '@/app/generators/contestantRoundListGenerator';
-import generateContestantRoundScores from '@/app/generators/contestantRoundScoreGenerator';
 import LeagueStandingTable from '../../../components/leagueStandingTable/leagueStandingTable';
 
 export default async function LeagueStanding() {
     const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL, "Cast");
-    const contestantsScores = await generateListOfContestantRoundLists(dataFetcher,  CONTESTANT_LEAGUE_DATA, getCompetingEntityList);
+    const contestantsScores = await generateListOfContestantRoundLists(dataFetcher,  CONTESTANT_LEAGUE_DATA);
     return <>
         <h1 className="text-3xl text-center">League Standing</h1>
         <br/>
