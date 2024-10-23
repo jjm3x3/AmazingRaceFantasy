23b5efd455e2d61ad88161607c871df994d76a6a
Antoinette Janus <acj.gfx@gmail.com>
2024-10-22 22:27:57 -0700

    Updated rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,37 +2,44 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
   {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
     languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
         browser: true,
-        module: true
-      }
+        module: true,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
     },
     rules: {
-      indent: [
-          "error",
-          4
-        ],
-        "linebreak-style": [
-          "error",
-          "unix"
-        ],
-        quotes: [
-          "error",
-          "double"
-        ],
-        semi: [
-          "error",
-          "always"
-        ]
-    }
-  }
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          indent: 4,
+          quotes: "double",
+          "linebreak-style": "unix",
+        },
+      ],
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
