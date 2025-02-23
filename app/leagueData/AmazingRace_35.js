fe729181da05f467016ecdcebd46dfe1d7760d90
jjm3x3 <jjm3x3@users.noreply.github.com>
2025-02-23 11:59:13 -0800

    Feature/add amazing 35 league contestants to db (#163)
    * Start looping through league data
    
    * Give Andrew the first guid
    
    * Add filter condition with warning for users w/o and id
    
    * Add json set operation
    
    * Add userId for cindy
    
    * Add userIds for the rest of amazing race 35 league data
    
    * Renmae userCursor at the end of the seed script
    
    * Add process, assuming one scan operation, to delete all keys

--- app/leagueData/AmazingRace_35.js
+++ app/leagueData/AmazingRace_35.js
@@ -3,22 +3,27 @@
 const CONTESTANT_LEAGUE_DATA = [
     {
         name: "Andrew",
+        userId: "6252275B-C6AF-427B-82A6-1F4B4A2267C1",
         ranking: [ "Corey McArthur & Rob McArthur", "Jocelyn Chao & Victor Limary", "Liam Hykel & Yeremi Hykel", "Greg Franklin & John Franklin", "Ashlie Martin & Todd Martin", "Chelsea Day & Robbin Tomich", "Lena Franklin & Morgan Franklin", "Anna Leigh Wilson & Steve Cargile", "Garrett Smith & Joel Strasser", "Ian Todd & Joe Moskowitz", "Andrea Simpson & Malaina Hatcher", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
     },
     {
         name: "Cindy",
+        userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
         ranking: [ "Jocelyn Chao & Victor Limary", "Corey McArthur & Rob McArthur", "Ian Todd & Joe Moskowitz", "Liam Hykel & Yeremi Hykel", "Ashlie Martin & Todd Martin", "Garrett Smith & Joel Strasser", "Anna Leigh Wilson & Steve Cargile", "Lena Franklin & Morgan Franklin", "Greg Franklin & John Franklin", "Andrea Simpson & Malaina Hatcher", "Chelsea Day & Robbin Tomich", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
     },
     {
         name: "Jacob",
+        userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
         ranking: [ "Corey McArthur & Rob McArthur", "Jocelyn Chao & Victor Limary", "Lena Franklin & Morgan Franklin", "Greg Franklin & John Franklin", "Chelsea Day & Robbin Tomich", "Anna Leigh Wilson & Steve Cargile", "Ashlie Martin & Todd Martin", "Garrett Smith & Joel Strasser", "Ian Todd & Joe Moskowitz", "Andrea Simpson & Malaina Hatcher", "Liam Hykel & Yeremi Hykel", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
     },
     {
         name: "Jim",
+        userId: "CAFA7731-EC3C-4B9D-8BC6-A199B6FB86A4",
         ranking: [ "Jocelyn Chao & Victor Limary", "Lena Franklin & Morgan Franklin", "Anna Leigh Wilson & Steve Cargile", "Corey McArthur & Rob McArthur", "Liam Hykel & Yeremi Hykel", "Greg Franklin & John Franklin", "Ashlie Martin & Todd Martin", "Ian Todd & Joe Moskowitz", "Andrea Simpson & Malaina Hatcher", "Chelsea Day & Robbin Tomich", "Elizabeth Rivera & Iliana Rivera", "Garrett Smith & Joel Strasser", "Alexandra Lichtor & Sheridan Lichtor" ]
     },
     {
         name: "Rachel",
+        userId: "E3BA8CF1-0F66-4911-88D8-A9ECFEEB37A7",
         ranking: [ "Ashlie Martin & Todd Martin", "Jocelyn Chao & Victor Limary", "Garrett Smith & Joel Strasser", "Lena Franklin & Morgan Franklin", "Ian Todd & Joe Moskowitz", "Corey McArthur & Rob McArthur", "Greg Franklin & John Franklin", "Liam Hykel & Yeremi Hykel", "Anna Leigh Wilson & Steve Cargile", "Andrea Simpson & Malaina Hatcher", "Chelsea Day & Robbin Tomich", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
     }
 ]
--- scripts/seedDb.mjs
+++ scripts/seedDb.mjs
@@ -11,4 +11,23 @@ const redis = new Redis({
 })
 
 const userCursor = await redis.scan("0", {match: "amazing_race:35:*"})
-console.log(userCursor)
+
+for (const aKey of userCursor[1]) { //list of keys
+    redis.del(aKey)
+}
+
+for(const user of CONTESTANT_LEAGUE_DATA) {
+    if (user.userId == null) {
+        console.warn(`The user named: '${user.name}'`)
+        continue
+    }
+
+    console.log("Setting user '" + user.name + "'")
+
+    const userString = JSON.stringify(user)
+
+    await redis.json.set("amazing_race:35:"+user.userId, "$", userString)
+}
+
+const fullCursor = await redis.scan("0", {match: "*"})
+console.log(fullCursor)
