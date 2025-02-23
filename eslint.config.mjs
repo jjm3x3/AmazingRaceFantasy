e3f80bb9bf9ab762f6cd1af8331bfb9bb1b50160
Antoinette Janus <acj.gfx@gmail.com>
2025-02-23 11:17:50 -0800

    Fixed broken ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,10 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-
 export default [
+    {
+        ignores: ['.next', 'node_modules']
+    },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReactConfig,
