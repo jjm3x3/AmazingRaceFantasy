b3b029aa97ec75f59efd46aee00d284d379a700b
Jacob Meixner <jjm3333@gmail.com>
2025-02-22 20:43:45 -0800

    Fix the import and console write the results

--- scripts/seedDb.mjs
+++ scripts/seedDb.mjs
@@ -1,6 +1,8 @@
 import fs from "node:fs"
 import { kv } from "@vercel/kv"
-import { CONTESTANT_LEAGUE_DATA } from "../app/leagueData"
+import { CONTESTANT_LEAGUE_DATA } from "../app/leagueData/AmazingRace_35.js"
 
 console.log("Seeding the db")
+
+console.log(CONTESTANT_LEAGUE_DATA)
 
