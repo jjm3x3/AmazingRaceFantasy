5ab148efe6363cb0ca80ba3b986c3ec8fe6b9682
Antoinette Janus <acj.gfx@gmail.com>
2025-02-26 12:36:11 -0800

    Updated no-unused-vars to focus on args

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,8 +23,12 @@ export default [
         rules: {
             "react/react-in-jsx-scope": "off",
             "react/no-unescaped-entities": "off",
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "off",
+            "no-unused-vars": ["error", {
+                args: "none"
+            }],
+            "@typescript-eslint/no-unused-vars": ["error", {
+                args: "none"
+            }],
             indent: [
                 "error",
                 4
