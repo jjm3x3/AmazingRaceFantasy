d67dfdc598ecf51233c2a466384a7ebf0da8323b
Antoinette Janus <acj.gfx@gmail.com>
2025-02-27 00:04:42 -0800

    Migrated leagueConfig from tsx to js (#169)
    * Migrated leagueConfig from tsx to js
    
    * Removed the additional tsconfig files

---
+++ app/leagueConfiguration/AmazingRace_36.js
@@ -0,0 +1,8 @@
+
+const WIKI_API_URL = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_36"
+
+const WIKI_PAGE_URL = "https://en.wikipedia.org/wiki/The_Amazing_Race_36"
+
+const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1AhDphP_QPb8fRYJarcgl_0o4r6yabrX_WW76XCqvvXg/edit?usp=sharing"
+
+module.exports = { WIKI_PAGE_URL, WIKI_API_URL, GOOGLE_SHEET_URL }
