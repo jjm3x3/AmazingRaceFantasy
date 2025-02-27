9151595a04cd3f1fe8aa8cf8be9dbf20fcf6774c
Antoinette Janus <acj.gfx@gmail.com>
2025-02-27 01:35:49 -0800

    Created Amazing Race 37 League Configuration page

---
+++ app/leagueConfiguration/AmazingRace_37.js
@@ -0,0 +1,17 @@
+const WIKI_PAGE_URL = "https://en.wikipedia.org/wiki/The_Amazing_Race_37"
+
+const WIKI_API_URL = "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_37"
+
+const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1QqgkpKDEj89jt-DQHJdltJNJAVFKV4MhQo_BjWF_zRY/edit?resourcekey=&gid=831430647#gid=831430647"
+
+const LEAGUE_STATUS = "active"
+
+const CAST_PHRASE = "Cast"
+
+const COMPETING_ENTITY_NAME = "team";
+
+const PRE_GOOGLE_SHEETS_LINK_TEXT = "This season's contestant data has been sourced from";
+
+const POST_GOOGLE_SHEETS_LINK_TEXT = "which was populated using a google form.";
+
+module.exports = { WIKI_PAGE_URL, WIKI_API_URL, GOOGLE_SHEET_URL, LEAGUE_STATUS, CAST_PHRASE, PRE_GOOGLE_SHEETS_LINK_TEXT, POST_GOOGLE_SHEETS_LINK_TEXT, COMPETING_ENTITY_NAME }
