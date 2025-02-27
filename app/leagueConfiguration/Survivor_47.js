29dd1903edf44003ba951e82cde211054d6be3b8
Antoinette Janus <acj.gfx@gmail.com>
2025-02-27 01:07:15 -0800

    Migrated League Contestants page

--- app/leagueConfiguration/Survivor_47.js
+++ app/leagueConfiguration/Survivor_47.js
@@ -7,8 +7,10 @@ const LEAGUE_STATUS = "active"
 
 const CAST_PHRASE = "Contestants"
 
+const COMPETING_ENTITY_NAME = "contestants"
+
 const PRE_GOOGLE_SHEETS_LINK_TEXT = "This season's contestant data has been sourced from";
 
 const POST_GOOGLE_SHEETS_LINK_TEXT = "which was populated using a google form.";
 
-module.exports = { WIKI_PAGE_URL, WIKI_API_URL, LEAGUE_STATUS, CAST_PHRASE, PRE_GOOGLE_SHEETS_LINK_TEXT, POST_GOOGLE_SHEETS_LINK_TEXT }
+module.exports = { WIKI_PAGE_URL, WIKI_API_URL, LEAGUE_STATUS, CAST_PHRASE, PRE_GOOGLE_SHEETS_LINK_TEXT, POST_GOOGLE_SHEETS_LINK_TEXT, COMPETING_ENTITY_NAME }
