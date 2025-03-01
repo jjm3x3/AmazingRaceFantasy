5d745b0117a3e71fa62ccac550fe65ac9e0608d3
Jacob Meixner <jjm3333@gmail.com>
2025-03-01 12:48:24 -0800

    Replace code in pages utils with import from leagueUtils

--- app/utils/pages.tsx
+++ app/utils/pages.tsx
@@ -1,3 +1,5 @@
+import {transformFilenameToSesonNameRepo } from "./leagueUtils"
+
 interface ILeagueLink {
     name: string
     subpages: IPage[]
@@ -54,21 +56,5 @@ export function getPages(): ILeagueLink[] {
     });
     const paths:Array<ILeagueLink> = activeLeaguePaths.concat(archiveLeaguePaths);
     return paths;
-}
-
-interface SeasonNameRepo {
-    urlSlug: string
-    friendlyName: string
-}
-
-function transformFilenameToSesonNameRepo(fileName: string): SeasonNameRepo {
-    const showAndSeason = fileName.split('_');
-    const showNameArray = showAndSeason[0].split(/(?<![A-Z])(?=[A-Z])/);
-    const showNameFormatted = showNameArray.join('-').toLowerCase();
-    const showSeason = showAndSeason[1].replace('.js', '');
-    const showNameAndSeason = `${showNameFormatted}-${showSeason}`;
-    let friendlyName = `${showNameArray.join(' ')} ${showSeason}`;
-
-    return { urlSlug: showNameAndSeason, friendlyName: friendlyName }
 }
 
