3faf33ece9d558b3a0036cb053bd59752890fbd7
Jacob Meixner <jjm3333@gmail.com>
2025-03-01 14:17:24 -0800

    Get the KeyPrefix from leagueConfiguration

--- app/[showStatus]/[showNameAndSeason]/league-standing/page.tsx
+++ app/[showStatus]/[showNameAndSeason]/league-standing/page.tsx
@@ -50,7 +50,7 @@ export default async function LeagueStanding({ params }: {
     const fileName = `${showName}_${showSeason}`;
     // "Dynamically" (still static site generated) retrieving modules
     const { CONTESTANT_LEAGUE_DATA } = await require(`../../../leagueData/${fileName}.js`);
-    const { WIKI_API_URL, CAST_PHRASE } = await require(`../../../leagueConfiguration/${fileName}.js`);
+    const { WIKI_API_URL, CAST_PHRASE, CONTESTANT_LEAGUE_DATA_KEY_PREFIX } = await require(`../../../leagueConfiguration/${fileName}.js`);
     const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL, CAST_PHRASE);
 
     const getEntityFn = showName.match('AmazingRace') ? getTeamList : getCompetingEntityList;
