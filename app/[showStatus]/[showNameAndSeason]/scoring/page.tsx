d23bfb6b14b61a6170d925c24c6387d455094e2a
jjm3x3 <jjm3x3@users.noreply.github.com>
2025-03-01 17:16:20 -0800

    193 update scoring page to read from db (#197)
    * Import Redis lib in scoring page
    
    * Add GET function which reads the KV Database
    
    * Use the get function to populate contestant data
    
    * Remove export and change the name of get data method
    
    * Try out adding config to deterimine which keyspace to pull from
    
    * Add other leagues
    
    * Add a validation to catch some unexpected errors
    
    * Remove direct file data reading
    
    * Remove extra console.log(s)

--- app/[showStatus]/[showNameAndSeason]/scoring/page.tsx
+++ app/[showStatus]/[showNameAndSeason]/scoring/page.tsx
@@ -3,6 +3,7 @@ import ContestantSelector from '../../../components/contestantSelector'
 import { getCompetingEntityList } from "../../../utils/wikiQuery"
 import { getWikipediaContestantDataFetcher } from '../../../dataSources/wikiFetch'
 import generateListOfContestantRoundLists from '../../../generators/contestantRoundListGenerator'
+import { Redis } from "@upstash/redis"
 
 // This forces Next to only generate routes that exist in generateStaticParams, otherwise return a 404
 export const dynamicParams = false
@@ -39,6 +40,28 @@ export function generateStaticParams() {
     return shows;
 }
 
+async function getContestantData(keyPrefix: string): Promise<any[]> {
+
+    if (keyPrefix === undefined) {
+        throw new Error(`Unable to getContestantData. Provided param 'keySpace' is undefined but must have a value"`);
+    }
+
+    const redis = new Redis({
+        url: process.env.KV_REST_API_URL,
+        token: process.env.KV_REST_API_TOKEN
+    })
+
+    const userCursor = await redis.scan("0", {match: keyPrefix})
+    const userLeagueKeys = userCursor[1] // get's the list of keys in the cursor
+    const userList = []
+    for (let i = 0; i < userLeagueKeys.length; i++) {
+        const userData = await redis.json.get(userLeagueKeys[i])
+        userList.push(userData)
+    }
+
+    return userList
+}
+
 export default async function Scoring({ params }: {
     params: Promise<{ showStatus: string; showNameAndSeason: string }>
   }) {
@@ -51,17 +74,18 @@ export default async function Scoring({ params }: {
     const showName = showAndSeasonArr.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join('');
     const fileName = `${showName}_${showSeason}`;
     // "Dynamically" (still static site generated) retrieving modules
-    const { CONTESTANT_LEAGUE_DATA } = await require(`../../../leagueData/${fileName}.js`);
-    const { WIKI_API_URL, GOOGLE_SHEET_URL, CAST_PHRASE, PRE_GOOGLE_SHEETS_LINK_TEXT, POST_GOOGLE_SHEETS_LINK_TEXT } = await require(`../../../leagueConfiguration/${fileName}.js`);
+    const { WIKI_API_URL, GOOGLE_SHEET_URL, CAST_PHRASE, PRE_GOOGLE_SHEETS_LINK_TEXT, POST_GOOGLE_SHEETS_LINK_TEXT, CONTESTANT_LEAGUE_DATA_KEY_PREFIX } = await require(`../../../leagueConfiguration/${fileName}.js`);
 
     const dataFetcher = getWikipediaContestantDataFetcher(WIKI_API_URL, CAST_PHRASE);
     let listOfContestantRoundLists;
 
+    const contestantRoundData = await getContestantData(CONTESTANT_LEAGUE_DATA_KEY_PREFIX);
+
     // Check for Amazing Race due to additional param
     if(showName.match('AmazingRace')){
-      listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, CONTESTANT_LEAGUE_DATA)
+      listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, contestantRoundData)
     } else {
-      listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, CONTESTANT_LEAGUE_DATA, getCompetingEntityList)
+      listOfContestantRoundLists = await generateListOfContestantRoundLists(dataFetcher, contestantRoundData, getCompetingEntityList)
     }
 
     return (
