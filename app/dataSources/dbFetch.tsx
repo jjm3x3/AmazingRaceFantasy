047786575bbfbb12b271f107fc88c6e18a621074
jjm3x3 <jjm3x3@users.noreply.github.com>
2025-03-01 17:41:39 -0800

    Feature/extract get contestent data to db fetch (#200)
    * Add existing srouce to new module
    
    * Replace use of getContestantData with module def/decl

---
+++ app/dataSources/dbFetch.tsx
@@ -0,0 +1,23 @@
+import { Redis } from "@upstash/redis"
+
+export async function getContestantData(keyPrefix: string): Promise<any[]> {
+
+    if (keyPrefix === undefined) {
+        throw new Error(`Unable to getContestantData. Provided param 'keySpace' is undefined but must have a value"`);
+    }
+
+    const redis = new Redis({
+        url: process.env.KV_REST_API_URL,
+        token: process.env.KV_REST_API_TOKEN
+    })
+
+    const userCursor = await redis.scan("0", {match: keyPrefix})
+    const userLeagueKeys = userCursor[1] // get's the list of keys in the cursor
+    const userList = []
+    for (let i = 0; i < userLeagueKeys.length; i++) {
+        const userData = await redis.json.get(userLeagueKeys[i])
+        userList.push(userData)
+    }
+
+    return userList
+}
