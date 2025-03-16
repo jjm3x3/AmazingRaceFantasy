4921f9bfe4445e85e9203fb823e5a6eb38a1d15a
Jacob Meixner <jjm3333@gmail.com>
2025-03-15 18:08:50 -0700

    Add Andrews ranking to test it out

--- app/leagueData/AmazingRace_37.js
+++ app/leagueData/AmazingRace_37.js
@@ -1,6 +1,11 @@
 
 // Contestant Ranking
 const CONTESTANT_LEAGUE_DATA = [
+    {
+        name: "Andrew J.",
+        userId: "6252275B-C6AF-427B-82A6-1F4B4A2267C1",
+        ranking: [ "Brett Hamby & Mark Romain", "Nick Fiorito & Mike Fiorito", "Courtney Ramsey & Jasmin Carey", "Alyssa Borden & Josiah Borden", "Jonathan Towns & Ana Towns", "Bernie Gutierrez & Carrigain Scadden", "Jeff \"Pops\" Bailey & Jeff Bailey", "Carson McCalley & Jack Dodge", "Scott Thompson & Lori Thompson", "Han Nguyen & Holden Nguyen", "Ernest Cato & Bridget Cato", "Melinda Papadeas & Erika Papadeas", "Mark Crawford & Larry Graham", "Jackye Clayton & Lauren McKinney" ]
+    }
 ];
 
 module.exports = { CONTESTANT_LEAGUE_DATA };
