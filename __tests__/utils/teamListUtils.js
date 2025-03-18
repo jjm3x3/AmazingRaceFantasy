1cd4e9725d3ab2cf64fc7d800a5cf24b234882c5
Jacob Meixner <jjm3333@gmail.com>
2025-03-17 19:00:52 -0700

    Add a test to filter out inf

--- __tests__/utils/teamListUtils.js
+++ __tests__/utils/teamListUtils.js
@@ -90,4 +90,19 @@ describe("getNumberOfRounds", () => {
         // Assert
         expect(result).toBe(2);
     });
+
+    it("Should not countin Number.MAX_VALUE as a unique number", () => {
+        //Arrange
+        const teamList = [
+            {eliminationOrder: 1},
+            {eliminationOrder: 3},
+            {eliminationOrder: Number.MAX_VALUE}
+        ];
+
+        // Act
+        const result = getNumberOfRounds(teamList);
+
+        // Assert
+        expect(result).toBe(2);
+    });
 });
