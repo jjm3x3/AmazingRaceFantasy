28e5b576701f53178d1c98dd4917aae0251eb65e
Antoinette Janus <acj.gfx@gmail.com>
2025-03-19 22:43:39 -0700

    Testing active league

--- __tests__/utils/pages.js
+++ __tests__/utils/pages.js
@@ -19,7 +19,7 @@ const BigBrotherConfigData = () => {
         wikiPageUrl: "https://en.wikipedia.org/wiki/Big_Brother_26_(American_season)", 
         wikiApiUrl: "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Big_Brother_26_(American_season)",
         googleSheetUrl: "https://docs.google.com/spreadsheets/d/1i-81N_JnqRl7jAHUANS5fr2tuY7CH9KpUvgTNzSIqNw/edit?usp=sharing",
-        leagueStatus: "archive",
+        leagueStatus: "active",
         castPhrase: "Houseguests",
         preGoogleSheetsLinkText: "This season's contestant data has been sourced from",
         postGoogleSheetsLinkText: "which was populated using a google form.",
