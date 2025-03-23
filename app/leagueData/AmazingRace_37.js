9ad406baac00adbafdd70a3f2b10ef822fcccf6d
Jacob Meixner <jjm3333@gmail.com>
2025-03-23 16:55:08 -0700

    Add Antoinette to amazing race 37

--- app/leagueData/AmazingRace_37.js
+++ app/leagueData/AmazingRace_37.js
@@ -7,6 +7,11 @@ const CONTESTANT_LEAGUE_DATA = [
         ranking: [ "Brett Hamby & Mark Romain", "Nick Fiorito & Mike Fiorito", "Courtney Ramsey & Jasmin Carey", "Alyssa Borden & Josiah Borden", "Jonathan Towns & Ana Towns", "Bernie Gutierrez & Carrigain Scadden", "Jeff \"Pops\" Bailey & Jeff Bailey", "Carson McCalley & Jack Dodge", "Scott Thompson & Lori Thompson", "Han Nguyen & Holden Nguyen", "Ernest Cato & Bridget Cato", "Melinda Papadeas & Erika Papadeas", "Mark Crawford & Larry Graham", "Jackye Clayton & Lauren McKinney" ]
     },
     {
+        name: "Antoinette",
+        userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+        ranking: [ "Alyssa Borden & Josiah Borden", "Carson McCalley & Jack Dodge", "Bernie Gutierrez & Carrigain Scadden", "Jeff \"Pops\" Bailey & Jeff Bailey", "Jackye Clayton & Lauren McKinney", "Han Nguyen & Holden Nguyen", "Brett Hamby & Mark Romain", "Jonathan Towns & Ana Towns", "Ernest Cato & Bridget Cato", "Courtney Ramsey & Jasmin Carey", "Melinda Papadeas & Erika Papadeas", "Nick Fiorito & Mike Fiorito", "Scott Thompson & Lori Thompson", "Mark Crawford & Larry Graham" ]
+    },
+    {
         name: "Cindy",
         userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
         ranking: [ "Brett Hamby & Mark Romain", "Nick Fiorito & Mike Fiorito", "Jonathan Towns & Ana Towns", "Carson McCalley & Jack Dodge", "Alyssa Borden & Josiah Borden", "Courtney Ramsey & Jasmin Carey", "Han Nguyen & Holden Nguyen", "Melinda Papadeas & Erika Papadeas", "Scott Thompson & Lori Thompson", "Jeff \"Pops\" Bailey & Jeff Bailey", "Ernest Cato & Bridget Cato", "Bernie Gutierrez & Carrigain Scadden", "Mark Crawford & Larry Graham", "Jackye Clayton & Lauren McKinney" ]
