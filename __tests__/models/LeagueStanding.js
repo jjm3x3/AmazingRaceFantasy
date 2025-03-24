79af4d0043e28557d48e6008e76e51dadaa2f317
Jacob Meixner <jjm3333@gmail.com>
2025-03-23 18:17:00 -0700

    Setup basics for a test

--- __tests__/models/LeagueStanding.js
+++ __tests__/models/LeagueStanding.js
@@ -195,6 +195,16 @@ describe("addContestantRoundScores", () => {
 
 describe("Regression Tests Checking Scoring of Archived Leagues", () => {
 
-    it("");
+    it("Should Score Rachel correctly for Amazing Race 35", () => {
 
+        // Arrange
+        const rachelsRawTeamList = [{"teamName":"Todd Martin & Ashlie Martin","relationship":"Married High School Sweethearts","isParticipating":false,"eliminationOrder":9},{"teamName":"Jocelyn Chao & Victor Limary","relationship":"Married Entrepreneurs","isParticipating":false,"eliminationOrder":3},{"teamName":"Joel Strasser & Garrett Smith","relationship":"Best Friends","isParticipating":false,"eliminationOrder":12.5},{"teamName":"Morgan Franklin & Lena Franklin","relationship":"Sisters","isParticipating":false,"eliminationOrder":7},{"teamName":"Joe Moskowitz & Ian Todd","relationship":"Engaged","isParticipating":false,"eliminationOrder":4},{"teamName":"Rob McArthur & Corey McArthur","relationship":"Father & Son","isParticipating":false,"eliminationOrder":11.5},{"teamName":"Greg Franklin & John Franklin","relationship":"Brothers & Computer Scientists","isParticipating":true,"eliminationOrder":0},{"teamName":"Liam Hykel & Yeremi Hykel","relationship":"Brothers","isParticipating":false,"eliminationOrder":5},{"teamName":"Steve Cargile & Anna Leigh Wilson","relationship":"Father & Daughter","isParticipating":false,"eliminationOrder":10},{"teamName":"Andrea Simpson & Malaina Hatcher","relationship":"College Friends","isParticipating":false,"eliminationOrder":6},{"teamName":"Robbin Tomich & Chelsea Day","relationship":"Childhood Friends","isParticipating":false,"eliminationOrder":8},{"teamName":"Elizabeth Rivera & Iliana Rivera","relationship":"Mother & Daughter","isParticipating":false,"eliminationOrder":2},{"teamName":"Alexandra Lichtor & Sheridan Lichtor","relationship":"Siblings & Roommates","isParticipating":false,"eliminationOrder":1}]
+
+        const numberOfRounds = 13;
+        const handicap = 0;
+
+        // Act
+        const rachelsRoundScores = LeagueStanding.generateContestantRoundScores(rachelsRawTeamList, numberOfRounds, "testingRach", handicap);
+
+    });
 });
