04d22e715a422f67552b5a071be948aa39f7bdb6
Antoinette Janus <acj.gfx@gmail.com>
2025-04-07 15:04:35 -0700

    Changed the section title

--- app/leagueConfiguration/AmazingRace_37.js
+++ app/leagueConfiguration/AmazingRace_37.js
@@ -3,7 +3,7 @@ module.exports = {
     wikiApiUrl: "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_37",
     googleSheetUrl: "https://docs.google.com/spreadsheets/d/1QqgkpKDEj89jt-DQHJdltJNJAVFKV4MhQo_BjWF_zRY/edit?resourcekey=&gid=831430647#gid=831430647",
     leagueStatus: "active",
-    castPhrase: "Cast",
+    castPhrase: "Contestants",
     preGoogleSheetsLinkText: "This season's contestant data has been sourced from",
     postGoogleSheetsLinkText: "which was populated using a google form.",
     competitingEntityName: "team",
