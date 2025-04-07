3758919760ebad6b5bd091843ccc95875537aaa1
Antoinette Janus <acj.gfx@gmail.com>
2025-04-07 16:59:43 -0700

    Fix broken section titles (#250)
    * Corrected show name in footer
    
    * Changed the section title

--- app/leagueConfiguration/AmazingRace_35.js
+++ app/leagueConfiguration/AmazingRace_35.js
@@ -3,7 +3,7 @@ module.exports = {
     wikiApiUrl: "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_35",
     googleSheetUrl: "https://docs.google.com/spreadsheets/d/1ei7I7ER9uHwAODnLGyNRhUA7ppYQLuwox8cYRgypLm8/edit?usp=sharing",
     leagueStatus: "archive",
-    castPhrase: "Cast",
+    castPhrase: "Contestants",
     preGoogleSheetsLinkText: "This season was largely managed by Andrew Jaicks using",
     postGoogleSheetsLinkText: "which was populated by him based on SMSs sent by the league contestants.",
     competitingEntityName: "teams",
--- app/leagueConfiguration/AmazingRace_36.js
+++ app/leagueConfiguration/AmazingRace_36.js
@@ -3,7 +3,7 @@ module.exports = {
     wikiApiUrl: "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_36",
     googleSheetUrl: "https://docs.google.com/spreadsheets/d/1AhDphP_QPb8fRYJarcgl_0o4r6yabrX_WW76XCqvvXg/edit?usp=sharing",
     leagueStatus: "archive",
-    castPhrase: "Cast",
+    castPhrase: "Contestants",
     preGoogleSheetsLinkText: "This season's contestant data has been sourced from",
     postGoogleSheetsLinkText: "which was populated using a google form.",
     competitingEntityName: "teams",
