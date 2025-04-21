22a1edc749fe95d0cef61cca25d000276f281c3f
Antoinette Janus <acj.gfx@gmail.com>
2025-04-21 13:37:59 -0700

    Added semicolon

--- app/components/navigation/google-login-btn.tsx
+++ app/components/navigation/google-login-btn.tsx
@@ -23,33 +23,33 @@ export default function GoogleLoginButton(){
                 use_fedcm_for_prompt: true,
                 use_fedcm_for_button: true
             });
-            const parent = document.getElementById('google_login_btn');
+            const parent = document.getElementById("google_login_btn");
             google.accounts.id.renderButton(parent, {
                 theme: "outline",
                 size: "large"
             });
         }
-    }, [isWindowLoaded])
+    }, [isWindowLoaded]);
     
     function handleCredentialResponse(response:GoogleLogin) {
         const responsePayload = decodeJwtResponse(response.credential);
    
         console.log("ID: " + responsePayload.sub);
-        console.log('Full Name: ' + responsePayload.name);
-        console.log('Given Name: ' + responsePayload.given_name);
-        console.log('Family Name: ' + responsePayload.family_name);
+        console.log("Full Name: " + responsePayload.name);
+        console.log("Given Name: " + responsePayload.given_name);
+        console.log("Family Name: " + responsePayload.family_name);
         console.log("Image URL: " + responsePayload.picture);
         console.log("Email: " + responsePayload.email);
     }
    
     function decodeJwtResponse(token:string) {
-       let base64Url = token.split('.')[1];
-       let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-       let jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-           return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-       }).join(''));
+        const base64Url = token.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jsonPayload = decodeURIComponent(atob(base64).split("").map(function(c) {
+            return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(""));
    
-       return JSON.parse(jsonPayload);
+        return JSON.parse(jsonPayload);
     }
 
     return (<>
