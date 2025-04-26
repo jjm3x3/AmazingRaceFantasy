34e13650121fa5f7c9471d6f104b4cec788032e5
Jacob Meixner <jjm3333@gmail.com>
2025-04-26 16:48:14 -0700

    Add import for the SUT function

--- __tests__/generators/contestantRoundScoreGenerator.js
+++ __tests__/generators/contestantRoundScoreGenerator.js
@@ -1,3 +1,5 @@
+import { generateContestantRoundScores } from "@/app/generators/contestantRoundScoreGenerator"
+
 describe("Regression Tests Checking generation of Archived Leagues", () => {
 
     it("Should return a league with rachels scoring for AmazingRace_35", () => {
