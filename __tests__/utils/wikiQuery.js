722cfe33a258bf2374f40b24397846101dc88f09
Jacob Meixner <jjm3333@gmail.com>
2025-04-26 15:36:25 -0700

    Add a positive test for a known full populated row

--- __tests__/utils/wikiQuery.js
+++ __tests__/utils/wikiQuery.js
@@ -765,4 +765,26 @@ describe("stripTableHeader", () => {
         expect(result).not.toBeNull();
         expect(result.length).toBe(0);
     });
+
+    it("Should not strip a knwon good row with 'fully' populated contetantData", () => {
+
+        // Arrange
+        const knownGoodRow = {
+            name: "Greg Franklin",
+            name2: "Greg Franklin",
+            col1: "25",
+            col2: "Brothers & Computer Scientists",
+            col3: "New York City, New York",
+            col4: "Winners",
+            col5: ""
+        };
+        const tableRowDataList = [knownGoodRow];
+
+        // Act
+        const result = stripTableHeader(tableRowDataList);
+
+        // Assert
+        expect(result).not.toBeNull();
+        expect(result.length).toBe(1);
+    });
 });
