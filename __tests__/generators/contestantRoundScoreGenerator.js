c0fc599f639c9a219bfe186d519c0f2586a15466
Jacob Meixner <jjm3333@gmail.com>
2025-04-26 16:53:05 -0700

    Reconstrct contestantRoundData to pass in

--- __tests__/generators/contestantRoundScoreGenerator.js
+++ __tests__/generators/contestantRoundScoreGenerator.js
@@ -11,6 +11,15 @@ describe("Regression Tests Checking generation of Archived Leagues", () => {
             );
         });
 
+        const rachelsRawTeamList = [ "Ashlie Martin & Todd Martin", "Jocelyn Chao & Victor Limary", "Garrett Smith & Joel Strasser", "Lena Franklin & Morgan Franklin", "Ian Todd & Joe Moskowitz", "Corey McArthur & Rob McArthur", "Greg Franklin & John Franklin", "Liam Hykel & Yeremi Hykel", "Anna Leigh Wilson & Steve Cargile", "Andrea Simpson & Malaina Hatcher", "Chelsea Day & Robbin Tomich", "Elizabeth Rivera & Iliana Rivera", "Alexandra Lichtor & Sheridan Lichtor" ]
+
+        const rachelsContestantLeagueData = {
+            name: "Rachel",
+            userId: "E3BA8CF1-0F66-4911-88D8-A9ECFEEB37A7",
+            ranking: rachelsRawTeamList,
+        };
+        const listOfContetantLeagueData = [rachelsContestantLeagueData]
+
         // Act
         generateContestantRoundScores(testDataFetcher, getTeamList);
     });
