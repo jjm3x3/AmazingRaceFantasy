735d1ae045bab66c3688c91eae4846383194aa9a
Jacob Meixner <jjm3333@gmail.com>
2025-04-26 16:49:49 -0700

    Create a testDataFetcher effectively mocking wikiFetch

--- __tests__/generators/contestantRoundScoreGenerator.js
+++ __tests__/generators/contestantRoundScoreGenerator.js
@@ -4,5 +4,10 @@ describe("Regression Tests Checking generation of Archived Leagues", () => {
 
     it("Should return a league with rachels scoring for AmazingRace_35", () => {
 
+        const testDataFetcher = () => new Promise((resolve, _reject) => {
+            resolve(
+                [{"name":"someTeamName"}]
+            );
+        });
     });
 });
