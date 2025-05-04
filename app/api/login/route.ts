70e8f31f85fe6030f7bb858dd6aca2aba0162099
Antoinette Janus <acj.gfx@gmail.com>
2025-05-03 18:21:05 -0700

    Rename ticket to authResponse

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -5,11 +5,11 @@ const client = new OAuth2Client();
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const clientId = process.env.GOOGLE_LOGIN_CLIENT_ID;
-    const ticket = await client.verifyIdToken({
+    const authResponse = await client.verifyIdToken({
         idToken: body.token,
         audience: clientId
     });
-    const payload = ticket.getPayload();
+    const payload = authResponse.getPayload();
     const userObj = {
         email: payload?.email,
         name: {
