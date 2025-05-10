db3ed48c670480e7194af26299e40841a75827ce
Jacob Meixner <jjm3333@gmail.com>
2025-05-10 15:29:54 -0700

    Add Antoinette's data

---
+++ app/leagueData/Survivor_47.js
@@ -0,0 +1,11 @@
+
+// Contestant Ranking
+const CONTESTANT_LEAGUE_DATA = [
+    {
+        name: "Antoinette",
+        userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+        ranking: [ "Tiyana Hallums", "Kyle Ostwald", "Sierra Wright", "Gabe Ortis", "Caroline Vidmar", "Anika Dhar", "Aysha Welch", "Sue Smey", "Rachel LaMont", "Sol Yi", "Kishan Patel", "Sam Phalen", "Teeny Chirichillo", "TK Foster", "Genevieve Mushaluk", "Andy Rueda", "Rome Cooney", "Jon Lovett" ]
+    }
+];
+
+module.exports = { CONTESTANT_LEAGUE_DATA };
