4917209c242cd85a4c226e534395f60020375e2b
jjm3x3 <jjm3x3@users.noreply.github.com>
2025-05-10 20:54:22 -0700

    Feature/add contestant data (#294)
    * Add Antoinette's data
    
    * Add Survivor47 league data to seedDb
    
    * Clean up script a bit
    
    * Add url to google sheets
    
    * Add everyon else's data
    
    * Fix capitalization

--- app/leagueConfiguration/Survivor_47.js
+++ app/leagueConfiguration/Survivor_47.js
@@ -1,6 +1,7 @@
 module.exports = { 
     wikiPageUrl: "https://en.wikipedia.org/wiki/Survivor_47", 
     wikiApiUrl: "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=Survivor_47",
+    googleSheetUrl: "https://docs.google.com/spreadsheets/d/10I_BWk_VEdAC4Qf9OrF4ZJaE5aiHyyJi9Ku1Ds8guzo/edit?gid=1419954199#gid=1419954199",
     leagueStatus: "archive",
     castPhrase: "Contestants",
     preGoogleSheetsLinkText: "This season's contestant data has been sourced from",
