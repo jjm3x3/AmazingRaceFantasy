79eb8e356844cdf7b2dc10ee6dfcc3557bcfa7c7
Antoinette Janus <acj.gfx@gmail.com>
2025-05-10 21:05:33 -0700

    Login fe (#288)
    * Cleaned up Navigation
    
    * Added google button
    
    * Added Google Login btn
    
    * Cleaned up styles
    
    * Removed unnecessary logic due to class name changes
    
    * Added google accounts types for typescript
    
    * Added unit tests for mocking the google login
    
    * Added "use client" for ui persistence
    
    * Formatting fixes
    
    * Removing package
    
    * Readding necessary package for build

---
+++ app/components/navigation/google-login-btn.tsx
@@ -0,0 +1,56 @@
+"use client"
+import Script from "next/script";
+import { useEffect, useState, useRef } from "react";
+
+interface GoogleLogin {
+    credential: string,
+    select_by: string
+}
+
+export default function GoogleLoginButton(){
+    const [scriptLoaded, setScriptLoaded] = useState(false);
+    const handleScriptLoad = () => {
+        setScriptLoaded(true);
+    };
+
+    const googleLoginRef = useRef(null);
+
+    useEffect(()=> {
+        if(scriptLoaded){
+            const google = window.google;
+            google.accounts.id.initialize({
+                client_id: "708154320268-432vdrg2g0h1652frag5vbu8r8qi4ers.apps.googleusercontent.com",
+                callback: handleCredentialResponse,
+                use_fedcm_for_prompt: true
+            });
+            const parent = googleLoginRef.current;
+            if(googleLoginRef && parent){
+                google.accounts.id.renderButton(parent, {
+                    text: "signin",
+                    size: "medium",
+                    logo_alignment: "left",
+                    shape: "rectangular",
+                    theme: "outline",
+                    type: "standard"
+                });
+            }
+        }
+    }, [scriptLoaded]);
+    
+    function handleCredentialResponse(response:GoogleLogin) {
+        fetch("/api/login", {
+            method: "POST",
+            body: JSON.stringify({ token: response.credential }),
+        }).then(handleLogin);
+    }
+
+    async function handleLogin(response: Response) {
+        const data = await response.json();
+        console.log(data);
+    }
+
+    return (<>
+        <div ref={googleLoginRef} id="google_login_btn"/>
+        <Script async src="https://accounts.google.com/gsi/client" onLoad={handleScriptLoad}/>
+    </>);
+}
