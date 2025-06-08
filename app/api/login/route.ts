be9c80c56bc963a710c44472c49f0c44bf4e92e1
Antoinette Janus <acj.gfx@gmail.com>
2025-06-07 20:39:52 -0700

    Setup db insertion

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,6 @@
 import { OAuth2Client } from "google-auth-library";
 import { NextRequest, NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
 
 export async function POST(request: NextRequest) {
     const client = new OAuth2Client();
@@ -10,6 +11,21 @@ export async function POST(request: NextRequest) {
         audience: clientId
     });
     const payload = authResponse.getPayload();
+    const googleUserId = payload["sub"];
+
+    // Setup Redis
+    const redisOptions = {
+        url: process.env.KV_REST_API_URL,
+        token: process.env.KV_REST_API_TOKEN
+    };
+    const redis = new Redis(redisOptions);
+
+    // Post to DB
+    const userDbObj = { googleUserId }
+    const leagueConfigString = JSON.stringify(userDbObj)
+    await redis.json.set(`user:${googleUserId}`, "$", leagueConfigString)
+
+    // Data to send to the front end
     const userObj = {
         email: payload?.email,
         name: {
