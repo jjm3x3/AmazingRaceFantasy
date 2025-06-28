e49ec7804ed1af1cb662647c42538d2781247517
Jacob Meixner <jjm3333@gmail.com>
2025-06-27 17:40:02 -0700

    Update test to have a json func prop

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -7,9 +7,12 @@ import { POST } from "@/app/api/league/route.ts";
 describe("POST", () => {
     it("should return a 401 when auth token is missing", async () => {
         // Aarrange
+        const request = {
+            json: async () => { return {} }
+        };
 
         // Act
-        await POST({});
+        await POST(request);
 
         // Assert
     });
