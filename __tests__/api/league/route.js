1385df4e5dce4102242b8b26dfae52425385c77f
Jacob Meixner <jjm3333@gmail.com>
2025-06-29 18:21:39 -0700

    Add new test to assert missing leagueStatus

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -103,4 +103,22 @@ describe("POST (unit tests)", () => {
         expect(response).not.toBeNull();
         expect(response.status).toEqual(400);
     });
+
+    it("should return a 400 when missing leagueStatus", async () => {
+        // Aarrange
+        const request = {
+            json: async () => { return {
+                token: "testToken",
+                wikiPageName: "someName",
+                googleSheetUrl: "http://some.url",
+            } }
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(400);
+    });
 });
