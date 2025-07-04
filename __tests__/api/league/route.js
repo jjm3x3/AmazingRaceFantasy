71c0d8f267288be5e1a1fcf2a2330477a0396405
Jacob Meixner <jjm3333@gmail.com>
2025-07-04 16:45:31 -0700

    Add another test for a malformed token

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -32,4 +32,18 @@ describe("POST", () => {
         expect(response).not.toBeNull();
         expect(response.status).toEqual(401);
     });
+
+    it("should return a 401 when auth token is malformed (parts are not base64 encoded json)", async () => {
+        // Aarrange
+        const request = {
+            json: async () => { return { token: "someTokenHeader.someTokenBody.someTokenSig" } }
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(401);
+    });
 });
