24d365ab86ec8acc722bccdc0d92de711a851eff
Jacob Meixner <jjm3333@gmail.com>
2025-07-04 16:23:01 -0700

    Add simple check to return 401 when no token is passed

--- app/api/league/route.ts
+++ app/api/league/route.ts
@@ -3,6 +3,10 @@ import { OAuth2Client, TokenPayload } from "google-auth-library";
 
 export async function POST(request: NextRequest) {
     // check auth
+    const body = await request.json();
+    if (!body.token) {
+        return NextResponse.json({"error": "you are not authenticated with this service"}, {status: 401})
+    }
 
     // validate/sanitize input
 
