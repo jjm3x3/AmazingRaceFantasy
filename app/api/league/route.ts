f0c7c8ee528804cd30140af5e550505db34e0a16
Jacob Meixner <jjm3333@gmail.com>
2025-07-04 16:41:10 -0700

    Extract message for more secure responses

--- app/api/league/route.ts
+++ app/api/league/route.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { OAuth2Client, TokenPayload } from "google-auth-library";
 
+const unauthenticatedErrorMessage = "you are not authenticated with this service";
+
 export async function POST(request: NextRequest) {
     // check auth
     const body = await request.json();
     if (!body.token) {
-        return NextResponse.json({"error": "you are not authenticated with this service"}, {status: 401})
+        return NextResponse.json({"error": unauthenticatedErrorMessage}, {status: 401})
     }
     const client = new OAuth2Client();
     const clientId = process.env.GOOGLE_LOGIN_CLIENT_ID;
