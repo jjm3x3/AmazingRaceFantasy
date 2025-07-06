45ac1670ffca8544c633e1dd7df242a484f88df2
jjm3x3 <jjm3x3@users.noreply.github.com>
2025-07-05 20:16:14 -0700

    Feature/fixup tests for easier rebasing (#308)
    * Rename files in order to simplify rebase
    
    * Move initialization into beforeEach
    
    * Update default sub claim in testAuthData
    
    * Fix tyos as suggested
    
    Co-authored-by: Antoinette Janus <acj.gfx@gmail.com>
    
    ---------
    
    Co-authored-by: Antoinette Janus <acj.gfx@gmail.com>

---
+++ __tests__/api/league/route_integration.js
@@ -0,0 +1,49 @@
+/**
+ * @jest-environment node
+ */
+
+import { POST } from "@/app/api/league/route.ts";
+
+describe("POST", () => {
+    it("should return a 401 when auth token is missing", async () => {
+        // Arrange
+        const request = {
+            json: async () => { return {} }
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(401);
+    });
+
+    it("should return a 401 when auth token is malformed (missing 3 parts)", async () => {
+        // Arrange
+        const request = {
+            json: async () => { return { token: "someTokenValue" } }
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(401);
+    });
+
+    it("should return a 401 when auth token is malformed (parts are not base64 encoded json)", async () => {
+        // Arrange
+        const request = {
+            json: async () => { return { token: "someTokenHeader.someTokenBody.someTokenSig" } }
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(401);
+    });
+});
