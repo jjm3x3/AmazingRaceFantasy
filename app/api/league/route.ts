3e7b2b8a2762659249ae36b44146dd9a881dcd7f
Jacob Meixner <jjm3333@gmail.com>
2025-07-05 21:04:31 -0700

    Wrap symbol ussage in error response in string cast

--- app/api/league/route.ts
+++ app/api/league/route.ts
@@ -53,7 +53,7 @@ export async function POST(request: NextRequest) {
     catch(error: unknown){
         if (error instanceof z.ZodError) {
             const firstIssue = error.issues[0];
-            return NextResponse.json({"error": `parsing error caught, first one being property: '${firstIssue.path[0]}' having issue: '${firstIssue.message}'`}, {status: 400});
+            return NextResponse.json({"error": `parsing error caught, first one being property: '${String(firstIssue.path[0])}' having issue: '${firstIssue.message}'`}, {status: 400});
         }
     }
 
