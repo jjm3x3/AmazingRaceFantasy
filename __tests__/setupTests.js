2cee9511fd8ee6ed24867bda84f93d330d28e475
Antoinette Janus <acj.gfx@gmail.com>
2025-07-05 21:10:30 -0700

    Adding Jose acceptance to Next

--- __tests__/setupTests.js
+++ __tests__/setupTests.js
@@ -1,3 +1,6 @@
 // react-testing-library renders your components to document.body,
 // this adds jest-dom's custom assertions
 import "@testing-library/jest-dom";
+import { TextEncoder, TextDecoder } from "jose";
+
+Object.assign(global, { TextDecoder, TextEncoder });
