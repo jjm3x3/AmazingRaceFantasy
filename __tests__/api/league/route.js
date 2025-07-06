7b312a387fa7913cd4747d6abc57d103af34cb4b
Jacob Meixner <jjm3333@gmail.com>
2025-07-05 21:44:22 -0700

    Simplify all the tests to use the happyPathRequest

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -66,15 +66,9 @@ describe("POST (unit tests)", () => {
     it("should return 200 when conditions met", async () => {
         // Arrange
         const request = {
-            json: jest.fn().mockImplementation(async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } })
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -88,15 +82,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when missing wikiPageName", async () => {
         // Aarrange
+        happyPathRequest.wikiPageName = null;
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -113,16 +103,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when wikiPageName has whitespace", async () => {
         // Aarrange
+        happyPathRequest.wikiPageName = "page name with whitespace";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "page name with whitespace",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -139,16 +124,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 200 when wikiPageName has name like a known BB season", async () => {
         // Aarrange
+        happyPathRequest.wikiPageName = "Big_Brother_26_(American_season)";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "Big_Brother_26_(American_season)",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -161,15 +141,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when missing googleSheetUrl", async () => {
         // Aarrange
+        happyPathRequest.googleSheetUrl = null;
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -186,16 +162,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when googleSheetUrl is invalid probably has spaces", async () => {
         // Aarrange
+        happyPathRequest.googleSheetUrl = "not a url";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "not a url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -212,16 +183,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when googleSheetUrl is invalid probably doens't have a scheme", async () => {
         // Aarrange
+        happyPathRequest.googleSheetUrl = "sup";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "sup",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -238,16 +204,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when googleSheetUrl has a insecure http scheme", async () => {
         // Aarrange
+        happyPathRequest.googleSheetUrl = "http:sup";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "http:sup",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -264,16 +225,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when googleSheetUrl has a invalid domain", async () => {
         // Aarrange
+        happyPathRequest.googleSheetUrl = "https:sup_blah_{_ga*b;ge_d^ta"; // the '^' is what breaks the domain regex
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https:sup_blah_{_ga*b;ge_d^ta", // the '^' is what breaks the domain regex
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -290,15 +246,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when missing leagueStatus", async () => {
         // Aarrange
+        happyPathRequest.leagueStatus = null;
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -315,16 +267,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 with an invalid leagueStatus", async () => {
         // Aarrange
+        happyPathRequest.leagueStatus = "maybe-active";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "maybe-active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -341,15 +288,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when missing wikiSectionHeader", async () => {
         // Aarrange
+        happyPathRequest.wikiSectionHeader = null;
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -366,15 +309,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when missing contestantType", async () => {
         // Aarrange
+        happyPathRequest.contestantType = null;
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                leagueKey: "some_show_name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -391,15 +330,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when missing leagueKey", async () => {
         // Aarrange
+        happyPathRequest.leagueKey = null;
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -416,16 +351,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when leagueKey is invalid because it has spaces", async () => {
         // Aarrange
+        happyPathRequest.leagueKey = "some_show _name:and_season_1";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show _name:and_season_1"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
@@ -442,16 +372,11 @@ describe("POST (unit tests)", () => {
 
     it("should return a 400 when leagueKey is invalid because it has '*'", async () => {
         // Aarrange
+        happyPathRequest.leagueKey = "some_show_name:and_season_1:*";
         const request = {
-            json: async () => { return {
-                token: "testToken",
-                wikiPageName: "someName",
-                googleSheetUrl: "https://some.url",
-                leagueStatus: "active",
-                wikiSectionHeader: "Show Contestants",
-                contestantType: "team",
-                leagueKey: "some_show_name:and_season_1:*"
-            } }
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
         };
 
         // Act
