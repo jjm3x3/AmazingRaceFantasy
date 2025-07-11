6b28f0cf5bbcefd054c605a281ac7c9ffa652f58
Antoinette Janus <acj.gfx@gmail.com>
2025-07-10 19:03:41 -0700

    Archived previously listed "active" leagues

--- app/leagueConfiguration/AmazingRace_37.js
+++ app/leagueConfiguration/AmazingRace_37.js
@@ -2,7 +2,7 @@ module.exports = {
     wikiPageUrl: "https://en.wikipedia.org/wiki/The_Amazing_Race_37", 
     wikiApiUrl: "https://en.wikipedia.org/w/api.php?action=parse&format=json&page=The_Amazing_Race_37",
     googleSheetUrl: "https://docs.google.com/spreadsheets/d/1QqgkpKDEj89jt-DQHJdltJNJAVFKV4MhQo_BjWF_zRY/edit?resourcekey=&gid=831430647#gid=831430647",
-    leagueStatus: "active",
+    leagueStatus: "archive",
     castPhrase: "Contestants",
     preGoogleSheetsLinkText: "This season's contestant data has been sourced from",
     postGoogleSheetsLinkText: "which was populated using a google form.",
