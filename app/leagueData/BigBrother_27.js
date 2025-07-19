7a00e0efc85b5b987c8b96696b58a674b6a7be6c
Jacob Meixner <jjm3333@gmail.com>
2025-07-19 11:25:27 -0700

    Add antoinettes ranking as is

--- app/leagueData/BigBrother_27.js
+++ app/leagueData/BigBrother_27.js
@@ -4,7 +4,7 @@ const CONTESTANT_LEAGUE_DATA = [
     {
         name: "Antoinette",
         userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
-        ranking: []
+        ranking: [ "Adrian Rocha", "Morgan Pope", "Katherine Woodman", "Keanu Soto", "Lauren Domingue", "Mystery Guest", "Rylie Jeffries", "Kelley Jorgensen", "Jimmy Heagerty", "Mickey Lee", "Zach Cornell", "Ava Pearl", "Vince Panaro", "Isaiah \"Zae\" Frederich", "Clifton Williams", "Amy Bingham", "Ashley Hollis" ]
     },
     {
         name: "Jacob",
