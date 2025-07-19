40b73fd1983d554705a2cef60f0f6b9616245573
Jacob Meixner <jjm3333@gmail.com>
2025-07-19 14:34:23 -0700

    Remove code to get contestant CompetingEntity[] and teamDictionary and Dictionary type to go with it

--- app/generators/contestantRoundScoreGenerator.tsx
+++ app/generators/contestantRoundScoreGenerator.tsx
@@ -4,10 +4,6 @@ import { IContestantData } from "@/app/dataSources/dbFetch";
 import CompetingEntity from "../models/CompetingEntity";
 import League from "../models/League";
 
-interface Dictionary<T> {
-    [Key: string]: T;
-}
-
 export async function generateContestantRoundScores(
     dataFetcher: () => Promise<ITableRowData[]>,
     getCompetitorList: (_: ITableRowData[]) => CompetingEntity[],
@@ -18,20 +14,10 @@ export async function generateContestantRoundScores(
     const wikiContestants = stripTableHeader(wikiTableData);
     // TODO: come up with better names for getCompetitorList and pageData
     const pageData = getCompetitorList(wikiContestants);
-    const teamDictionary = pageData.reduce((acc: Dictionary<CompetingEntity>, t: CompetingEntity) => {
-        acc[CompetingEntity.getKey(t.teamName)] = t;
-
-        return acc;
-    }, {});
 
     const result: League = new League(pageData);
 
     listOfContestantLeagueData.map(contestant => {
-
-        const currentSelectedContestantTeamsList = contestant.ranking.map((x: string) => {
-            const foundTeam = teamDictionary[CompetingEntity.getKey(x)];
-            return foundTeam;
-        });
 
         result.addContestantRoundScores(contestant.ranking, contestant.name, contestant.handicap);
 
