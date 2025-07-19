f95f4b1621913c32f9322a571637d792f9eceadb
Jacob Meixner <jjm3333@gmail.com>
2025-07-19 11:16:54 -0700

    Add a js file with the expected users, but with no rankings yet

---
+++ app/leagueData/BigBrother_27.js
@@ -0,0 +1,21 @@
+
+// Contestant Ranking
+const CONTESTANT_LEAGUE_DATA = [
+    {
+        name: "Antoinette",
+        userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+        ranking: []
+    },
+    {
+        name: "Jacob",
+        userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
+        ranking: []
+    },
+    {
+        name: "Sam",
+        userId: "CD9F9A71-FF9C-416A-8D0D-C268A13B021F",
+        ranking: []
+    }
+];
+
+module.exports = { CONTESTANT_LEAGUE_DATA };
