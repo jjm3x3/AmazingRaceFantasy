f76d24e5e3df574b4704f7aa17bd8c6c29bb706d
Antoinette Janus <acj.gfx@gmail.com>
2025-07-20 21:55:00 -0700

    Updated collapse based on login

--- app/components/navigation/navigation-item.tsx
+++ app/components/navigation/navigation-item.tsx
@@ -1,13 +1,22 @@
-import { useState } from "react";
+
+import { useState, useEffect, useContext } from "react";
+import { SessionContext } from "@/app/contexts/session";
 import INavigationItem from "@/app/models/INavigationItem";
 export default function NavigationItem({ inputAttr, labelAttr, listAttr, childElements }: INavigationItem) {
     const [isHidden, setIsHidden] = useState(true);
+    const { isLoggedIn } = useContext(SessionContext);
+    useEffect(()=> {
+        if(isLoggedIn){
+            setIsHidden(!isHidden);
+        }
+    }, [isLoggedIn]);
     return (<>
         <input 
             id={inputAttr.id}
             className={inputAttr.classes}
             type="checkbox"
-            onChange={(e)=> setIsHidden(!e.target.value)}
+            checked={!isHidden}
+            onChange={()=> setIsHidden(!isHidden)}
         />
         <label 
             htmlFor={inputAttr.id}
