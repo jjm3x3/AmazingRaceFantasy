881a489684b60a8e57085f7f3e76cbbfde9e0c0d
Jacob Meixner <jjm3333@gmail.com>
2025-07-20 17:42:45 -0700

    Add the test to test for wikiUrl

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -420,4 +420,27 @@ describe("POST (unit tests)", () => {
             `league_configuration:${happyPathRequest.leagueStatus}:${happyPathRequest.leagueKey}`,
             expect.anything());
     })
+
+    it("should call writeLeagueConfigurationData with a config with a wikiPageUrl", async () => {
+        // Arrange
+        const request = {
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(200);
+        expect(request.json).toHaveBeenCalledTimes(1);
+        expect(writeLeagueConfigurationData).toHaveBeenCalledTimes(1);
+        expect(writeLeagueConfigurationData).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+                "wikiPageUrl": `https://en.wikipedia.org/wiki/${happyPathRequest.wikiPageName}`
+            }));
+    });
 });
