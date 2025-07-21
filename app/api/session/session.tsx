330848d12ce6a2432519be5a5882cbce3158ffd2
Antoinette Janus <acj.gfx@gmail.com>
2025-07-20 17:53:34 -0700

    Added additional erroring

--- app/api/session/session.tsx
+++ app/api/session/session.tsx
@@ -6,11 +6,15 @@ const sessionSecretKey = process.env.SESSION_SECRET;
 const encodedKey = new TextEncoder().encode(sessionSecretKey);
 
 export async function encrypt({ envelope, exp }:{ envelope: JWTPayload, exp: number }) {
-    return new SignJWT(envelope)
-        .setProtectedHeader({ alg: "HS256" })
-        .setIssuedAt()
-        .setExpirationTime(exp)
-        .sign(encodedKey)
+    if(encodedKey){
+        return new SignJWT(envelope)
+            .setProtectedHeader({ alg: "HS256" })
+            .setIssuedAt()
+            .setExpirationTime(exp)
+            .sign(encodedKey)
+    } else {
+        throw Error("sessionSecretKey is unset. Have you gone through the setup in the README?")
+    }
 }
 
 export async function createSession({ response, envelope, exp }:{ response: NextResponse, envelope: JWTPayload, exp: number }) {
