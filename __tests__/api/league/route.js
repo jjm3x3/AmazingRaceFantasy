252aa7d9456e9342a92317517acb5aaa57d6b39e
Jacob Meixner <jjm3333@gmail.com>
2025-07-20 18:52:09 -0700

    Add a test for googleSheetPostText

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -558,4 +558,27 @@ describe("POST (unit tests)", () => {
                 "preGoogleSheetsLinkText": expect.anything(),
             }));
     });
+
+    it("should call writeLeagueConfigurationData with a config with a postGoogleSheetsLinkText", async () => {
+        // Arrange
+        const request = {
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(200);
+        expect(request.json).toHaveBeenCalledTimes(1);
+        expect(writeLeagueConfigurationData).toHaveBeenCalledTimes(1);
+        expect(writeLeagueConfigurationData).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+                "postGoogleSheetsLinkText": expect.anything(),
+            }));
+    });
 });
