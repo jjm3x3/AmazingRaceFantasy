034f7c5e5f18258241fe56ef5733ab635651312c
Jacob Meixner <jjm3333@gmail.com>
2025-07-20 17:56:26 -0700

    Add a test to assert league status

--- __tests__/api/league/route.js
+++ __tests__/api/league/route.js
@@ -489,4 +489,27 @@ describe("POST (unit tests)", () => {
                 "googleSheetUrl": happyPathRequest.googleSheetUrl,
             }));
     });
+
+    it("should call writeLeagueConfigurationData with a config with a leageStatus", async () => {
+        // Arrange
+        const request = {
+            json: jest.fn().mockImplementation(async () => {
+                return happyPathRequest
+            })
+        };
+
+        // Act
+        const response = await POST(request);
+
+        // Assert
+        expect(response).not.toBeNull();
+        expect(response.status).toEqual(200);
+        expect(request.json).toHaveBeenCalledTimes(1);
+        expect(writeLeagueConfigurationData).toHaveBeenCalledTimes(1);
+        expect(writeLeagueConfigurationData).toHaveBeenCalledWith(
+            expect.anything(),
+            expect.objectContaining({
+                "leagueStatus": happyPathRequest.leagueStatus,
+            }));
+    });
 });
