de661fff9ffe62ba780fe302fdef2e68a03efa0a
Antoinette Janus <acj.gfx@gmail.com>
2025-08-09 10:09:54 -0700

    Added error validation

--- app/components/baseComponents/components/inputs/text/text.tsx
+++ app/components/baseComponents/components/inputs/text/text.tsx
@@ -1,3 +1,4 @@
+import { useState, KeyboardEvent } from "react";
 import styles from "./text.module.scss";
 
 export default function TextInput({
@@ -13,15 +14,28 @@ export default function TextInput({
     validationPattern?: string,
     id: string
 }){
+    const [validity, setValidity] = useState(true);
+    const validationPatternAsRegexp = new RegExp(validationPattern);
+    const inputHandler = (event: KeyboardEvent<HTMLInputElement>)=> {
+        const inputValidity =  validationPatternAsRegexp.test(event.currentTarget.value);
+        if(event.currentTarget.value.length === 0){
+            setValidity(true);
+        } else {
+            setValidity(inputValidity);
+        }
+    }
     return (
         <div className={`flex-auto ${styles.inputContainer}`}>
             <label htmlFor={id} className={styles.label}>{label}</label>
             <input id={id} 
                 type="text"
+                pattern={validationPattern}
                 placeholder={placeholder}
                 required={isRequired}
-                className={styles.input}
-                pattern={validationPattern}/>
+                className={`${ styles.input } ${validity === false && styles.error}`}
+                onKeyUp={inputHandler}
+                />
+            {validity === false && <p className={`${styles.error} ${styles.errorMsg}`}>There is an error</p>}
         </div>
     )
 }
--- app/league/configuration/form/form.tsx
+++ app/league/configuration/form/form.tsx
@@ -29,7 +29,7 @@ export default function LeagueConfigurationForm(){
                 label="Google Sheets Url"
                 placeholder="example: https://docs.google.com/spreadsheets/d/testurl"
                 isRequired={true}
-                validationPattern="/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/"
+                validationPattern="((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)"
                 id="googleSheetsPageUrl"
             />
             <Button text="Create League"
