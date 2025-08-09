1b88d5bd5e952df024f72ea61e44aa693a46c3aa
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 12:40:24 -0700

    Add a test for redirect

--- __tests__/api/logout/route.js
+++ __tests__/api/logout/route.js
@@ -21,4 +21,22 @@ describe("POST", () => {
         expect(LogoutResponse).not.toBeNull();
     });
 
+    it("should return a redirect", async () => {
+
+        // Arrange
+        const requestMock = {
+            url: "http://localhost:3000/api/logout",
+            cookies: {
+                get: () => "a session cookie"
+            }
+        };
+
+        // Act
+        const LogoutResponse = await POST(requestMock);
+
+        // Assert
+        expect(LogoutResponse).not.toBeNull();
+        expect(LogoutResponse.status).not.toBeNull();
+        expect(LogoutResponse.status.toString()).toMatch(/^3/);
+    });
 });
