35e1b551951243ca5c05b7c5748b134f8792223c
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 11:57:57 -0700

    Add a test to cover a smol edgecase

--- __tests__/api/session/session.js
+++ __tests__/api/session/session.js
@@ -1,5 +1,12 @@
 import { removeSession } from "@/app/api/session/session.tsx";
 
+beforeEach(() => {
+    global.conosle = {
+        log: jest.fn(),
+        warn: jest.fn(),
+    };
+});
+
 describe("removeSession", () => {
     it("Should call set on the response on happy path", async () => {
 
@@ -21,5 +28,27 @@ describe("removeSession", () => {
 
         // Assert
         expect(responseMock.cookies.set).toBeCalled();
+    });
+
+    it("Should log a warning when sessionCookie is not found", async () => {
+
+        // Arrange
+        const requestMock = {
+            cookies: {
+                get: () => undefined
+            }
+        };
+
+        const responseMock = {
+            cookies: {
+                set: jest.fn()
+            }
+        };
+
+        // Act
+        await removeSession(requestMock, responseMock);
+
+        // Assert
+        expect(console.warn).toHaveBeenCalled();
     });
 });
