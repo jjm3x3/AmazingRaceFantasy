03646f0689156444b13415caa14cd444794ae7ce
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 12:41:34 -0700

    Return a 302

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const response = NextResponse.json({}, {status: 200});
+    const response = NextResponse.json({}, {status: 302});
 
     return response;
 }
