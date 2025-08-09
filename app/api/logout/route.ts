a2de0b34359a4ee64868c63b7c78974717212a84
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 09:31:13 -0700

    Add super simple logout api endpoint

---
+++ app/api/logout/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const response = NextResponse.json({}, {status: 200});
+
+    return response;
+}
