40184528701fe2fb5e868b6849723bc703f8ec7a
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 12:57:13 -0700

    Add call to removeSession in logout

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import { removeSession } from "../session/session";
 
 export async function POST(request: NextRequest) {
     const newUrl = new URL("/", request.url)
     const response = NextResponse.redirect(newUrl, 302);
+    removeSession(request, response);
 
     return response;
 }
