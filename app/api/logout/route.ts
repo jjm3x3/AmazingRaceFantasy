73aeae354c805cce3573ae0abf1c3eabe3d1578b
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 12:45:50 -0700

    Update to use NextResponse.redirect and set location

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-    const response = NextResponse.json({}, {status: 302});
+    const newUrl = new URL("/", request.url)
+    const response = NextResponse.redirect(newUrl, 302);
 
     return response;
 }
