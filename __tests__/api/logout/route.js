f39ba266ec599c0070463da2c94cd24f5edf8fea
Jacob Meixner <jjm3333@gmail.com>
2025-08-09 09:42:43 -0700

    Add the very basic body of a test

--- __tests__/api/logout/route.js
+++ __tests__/api/logout/route.js
@@ -1,10 +1,24 @@
 /**
  * @jest-environment node
  */
+import { POST } from "@/app/api/logout/route.ts";
 
 describe("POST", () => {
     it("should be able to get a session cookie from header", async () => {
-        
+
+        // Arrange
+        const requestMock = {
+            url: "http://localhost:3000/api/logout",
+            cookies: {
+                get: () => "a session cookie"
+            }
+        };
+
+        // Act
+        const LogoutResponse = await POST(requestMock);
+
+        // Assert
+        expect(LogoutResponse).not.toBeNull();
     });
 
 });
