ea6e03313f488893669adc3aea7e87f67e0263af
Antoinette Janus <acj.gfx@gmail.com>
2025-08-17 20:58:47 -0700

    Added validation and render test

--- app/components/baseComponents/components/inputs/text/text.tsx
+++ app/components/baseComponents/components/inputs/text/text.tsx
@@ -28,19 +28,20 @@ export default function TextInput({
     }
     return (
         <div className={`flex-auto ${styles.textComponentContainer}`}>
-            <label htmlFor={id} className={styles.label}>{label}</label>
+            <label htmlFor={id} data-testid={`test-label-${id}`} className={styles.label}>{label}</label>
             <div className={styles.inputContainer}>
                 <input id={id} 
                     type="text"
+                    data-testid={`test-input-${id}`}
                     pattern={validationPattern}
                     placeholder={placeholder}
                     required={isRequired}
                     className={`${ styles.input } ${validity === false && styles.error}`}
                     onKeyUp={inputHandler}
                 />
-                {validity === false && <span className={`${styles.error} ${styles.errorIcon}`}>!</span>}
+                {validity === false && <span data-testid={`test-label-${id}-icon`} className={`${styles.error} ${styles.errorIcon}`}>!</span>}
             </div>
-            {validity === false && <p className={`${styles.error} ${styles.errorMsg}`}>There is an error</p>}
+            {validity === false && <p data-testid={`test-label-${id}-msg`} className={`${styles.error} ${styles.errorMsg}`}>There is an error</p>}
         </div>
     )
 }
