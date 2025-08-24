1592ae54e776c5cdb9d2e37a0f25b41968c3e1bf
Jacob Meixner <jjm3333@gmail.com>
2025-08-24 11:56:34 -0700

    Add a test id to loggout button tag for testing

--- app/components/navigation/logoutButton.tsx
+++ app/components/navigation/logoutButton.tsx
@@ -18,6 +18,7 @@ export default function LogoutButton(){
     }
 
     return (<>
-        <button onClick={performLogout}>Log Out</button>
+        <button data-testid="logout-button-core" onClick={performLogout}>Log Out</button>
     </>);
 }
+
