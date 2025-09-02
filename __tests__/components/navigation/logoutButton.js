b122e95b7b48ce5348945a539758886ff42d27f6
Jacob Meixner <jjm3333@gmail.com>
2025-09-01 17:46:45 -0700

    Add a test to assert the hook is called

--- __tests__/components/navigation/logoutButton.js
+++ __tests__/components/navigation/logoutButton.js
@@ -33,4 +33,22 @@ describe("LogoutButton", () => {
         fireEvent.click(logoutButton);
         expect(fetchPromise.then).toHaveBeenCalled()
     });
+
+    it("should call set on sessionContext when a 205 is returned", () => {
+        const fetchPromise = { then: jest.fn((resolve) => {
+            resolve({status: 205});
+        })};
+        window.fetch = jest.fn()
+            .mockImplementation(() => fetchPromise);
+        const setIsLoggedInMock = jest.fn();
+        jest.spyOn(React, "useContext").mockReturnValue({ setIsLoggedIn: setIsLoggedInMock });
+
+        const { getByTestId } = render(
+            <LogoutButton/>
+        );
+        const logoutButton = getByTestId("logout-button-core");
+        fireEvent.click(logoutButton);
+
+        expect(setIsLoggedInMock).toHaveBeenCalled();
+    });
 });
