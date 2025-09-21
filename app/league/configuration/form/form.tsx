c1db3f7783950ce6d005d1b46e37c07531527254
Antoinette Janus <acj.gfx@gmail.com>
2025-09-20 19:51:15 -0700

    Added submit

--- app/league/configuration/form/form.tsx
+++ app/league/configuration/form/form.tsx
@@ -1,67 +1,85 @@
 "use client";
-import { MouseEvent } from "react";
+import { useRef, useState, MouseEvent } from "react";
 import validationPattern from "@/app/dataSources/validationPatterns";
 import styles from "./styles.module.scss";
 import TextInput from "@/app/components/baseComponents/components/inputs/text/text";
 import Select from "@/app/components/baseComponents/components/inputs/select/select";
 import Button from "@/app/components/baseComponents/components/button/button";
 export default function LeagueConfigurationForm(){
-    const formSubmit = (e: MouseEvent<HTMLButtonElement>)=> { e.preventDefault(); };
+    // This is needed to allow for query selector below
+    const formRef = useRef(null as HTMLFormElement | null);
+    const [errorMsg, setErrorMsg] = useState("");
+    const formSubmit = async (e: MouseEvent)=> { 
+        e.preventDefault();
+        if(formRef.current){
+            const formData = new FormData(formRef.current);
+            const response = await fetch("/api/league", {
+                method: "POST",
+                body: formData
+            });
+            if (!response.ok) {
+                setErrorMsg("There was a problem. Please check the form and try to resubmit.");
+            }
+        }
+    };
     return (
-        <form className={styles.form}>
-            <TextInput
-                label="Wikipedia Page Name"
-                placeholder="example: big_brother"
-                isRequired={true}
-                validationPattern={validationPattern.wikiPageName.string}
-                id="wikiPageUrl"
-            />
-            <TextInput
-                label="Wiki Section Header"
-                placeholder="example: HouseGuests"
-                isRequired={true}
-                validationPattern={validationPattern.wikiSectionHeader.string}
-                id="castPhrase"
-            />
-            <TextInput
-                label="League Key Prefix"
-                placeholder="example: big_brother:27"
-                isRequired={true}
-                validationPattern={validationPattern.leagueKey.string}
-                id="leagueKeyPrefix"
-            />
-            <TextInput
-                label="Contestant Type"
-                placeholder="example: team"
-                isRequired={true}
-                validationPattern={validationPattern.contestantType.string}
-                id="contestantType"
-            />
-            <Select 
-                labelText="League Status"
-                selectOptions={
-                    [{
-                        value: "active",
-                        text: "Active"
-                    },{
-                        value: "archived",
-                        text: "Archived"
-                    }]
-                }
-                id="league-status"
-            />
-            <TextInput
-                label="Google Sheets Url"
-                placeholder="example: https://docs.google.com/spreadsheets/d/testurl"
-                isRequired={true}
-                validationPattern={validationPattern.googleSheetsUrl.string}
-                id="googleSheetsPageUrl"
-            />
-            <Button text="Create League"
-                kind="primary"
-                type="submit"
-                clickHandler={formSubmit}
-            />
-        </form>
+        <>
+            <p>{errorMsg}</p>
+            <form ref={formRef} className={styles.form}>
+                <TextInput
+                    label="Wikipedia Page Name"
+                    placeholder="example: big_brother"
+                    isRequired={true}
+                    validationPattern={validationPattern.wikiPageUrl.string}
+                    id="wikiPageName"
+                />
+                <TextInput
+                    label="Wiki Section Header"
+                    placeholder="example: HouseGuests"
+                    isRequired={true}
+                    validationPattern={validationPattern.wikiSectionHeader.string}
+                    id="wikiSectionHeader"
+                />
+                <TextInput
+                    label="League Key Prefix"
+                    placeholder="example: big_brother:27"
+                    isRequired={true}
+                    validationPattern={validationPattern.leagueKey.string}
+                    id="leagueKey"
+                />
+                <TextInput
+                    label="Contestant Type"
+                    placeholder="example: team"
+                    isRequired={true}
+                    validationPattern={validationPattern.contestantType.string}
+                    id="contestantType"
+                />
+                <Select 
+                    labelText="League Status"
+                    selectOptions={
+                        [{
+                            value: "active",
+                            text: "Active"
+                        },{
+                            value: "archived",
+                            text: "Archived"
+                        }]
+                    }
+                    id="leagueStatus"
+                />
+                <TextInput
+                    label="Google Sheets Url"
+                    placeholder="example: https://docs.google.com/spreadsheets/d/testurl"
+                    isRequired={true}
+                    validationPattern={validationPattern.googleSheetsUrl.string}
+                    id="googleSheetsUrl"
+                />
+                <Button text="Create League"
+                    kind="primary"
+                    type="submit"
+                    clickHandler={formSubmit} 
+                />
+            </form>
+        </>
     );
 }
