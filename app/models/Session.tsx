778db470b2432b1cb4b6a1d5fe69eb86a2a02a26
Jacob Meixner <jjm3333@gmail.com>
2025-09-20 17:02:58 -0700

    Add a new SessionInfo type w/ existing prop

--- app/models/Session.tsx
+++ app/models/Session.tsx
@@ -3,3 +3,7 @@ export default interface Session {
     // setIsLoggedIn needs to include void as a type because of useState doesn't return a value directly
     setIsLoggedIn: (_isLoggedIn:boolean)=> boolean | void // eslint-disable-line no-unused-vars
 }
+
+interface SessionInfo {
+    isLoggedIn: boolean
+}
