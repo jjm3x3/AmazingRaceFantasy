c6179c7a28e95a973950a9dc18e5b2076a5047e6
Jacob Meixner <jjm3333@gmail.com>
2025-09-21 16:47:21 -0700

    Update types in Session

--- app/models/Session.tsx
+++ app/models/Session.tsx
@@ -1,10 +1,11 @@
+import { Dispatch, SetStateAction } from "react";
+
 export default interface Session {
     sessionInfo: SessionInfo
-    // setIsLoggedIn needs to include void as a type because of useState doesn't return a value directly
-    setSessionInfo: (_sessionInfo:SessionInfo)=> boolean | void // eslint-disable-line no-unused-vars
+    setSessionInfo: Dispatch<SetStateAction<SessionInfo>>
 }
 
 interface SessionInfo {
     isLoggedIn: boolean,
-    userName: string
+    userName: string | null
 }
