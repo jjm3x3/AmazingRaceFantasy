187d964242092b63d279a028c6ca027a7a088449
Jacob Meixner <jjm3333@gmail.com>
2025-09-20 19:50:57 -0700

    Add a useEffect hoook in the ContextedLabel

--- app/components/sessionContextedLabel.tsx
+++ app/components/sessionContextedLabel.tsx
@@ -1,11 +1,18 @@
 "use client"
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { SessionContext } from "@/app/contexts/session";
 import styles from "./sessionContextedLabel.module.scss";
 
 export default function SessionContextedLabel() {
 
-    const { sessionInfo } = useContext(SessionContext);
+    const { sessionInfo, setSessionInfo } = useContext(SessionContext);
+
+    useEffect(() => {
+        if (sessionInfo.userName === null) {
+            const userName = localStorage.getItem("userName") ?? "";
+            setSessionInfo({isLoggedIn: sessionInfo.isLoggedIn, userName: userName});
+        }
+    });
 
     return (
         <>
