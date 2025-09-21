a0a28665b64a7605830f9f2f6447858386fa30ce
Jacob Meixner <jjm3333@gmail.com>
2025-09-20 17:47:31 -0700

    Add sessionContext component to template

--- app/template.tsx
+++ app/template.tsx
@@ -3,6 +3,7 @@ import { getShowPages } from "@/app/utils/pages";
 import Navigation from "./components/navigation/navigation";
 import { SessionProvider } from "./contexts/session";
 import { cookies } from "next/headers";
+import SessionContextedLabel from "@/app/components/sessionContextedLabel";
 
 export default async function Template({
     children
@@ -21,6 +22,7 @@ export default async function Template({
                     <p>
                         <a href="/" title="Link that takes user to homepage. " className="page-title">X Factor Fantasy</a>
                     </p>
+                    <SessionContextedLabel />
                     {pages.length > 0 && <Navigation pages={pages} />}
                 </header>
                 <main>
