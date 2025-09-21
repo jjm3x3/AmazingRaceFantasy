48aca1af8e353e8ebe86f37aef2085f2a9c653d1
Antoinette Janus <acj.gfx@gmail.com>
2025-09-20 18:38:07 -0700

    missed a character and simplified

--- app/dataSources/validationPatterns.tsx
+++ app/dataSources/validationPatterns.tsx
@@ -10,7 +10,7 @@ const validationPattern = {
             protocol: /^https$/,
             hostname: z.regexes.domain
         }),
-        string: "^(?:(?:(?:https):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$" // eslint-disable-line no-useless-escape
+        string: "[Hh][Tt][Tt][Pp][Ss]?:\/\/(?:(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)(?:\.(?:[a-zA-Z\u00a1-\uffff0-9]+-?)*[a-zA-Z\u00a1-\uffff0-9]+)*(?:\.(?:[a-zA-Z\u00a1-\uffff]{2,}))(?::\d{2,5})?(?:\/[^\s]*)?" // eslint-disable-line no-useless-escape
     },
     leagueStatus: {
         zod: z.enum(["active","archive"]),
