c18e9fa0f6ba88ed6d5b46c89f2f384893e327b3
Antoinette Janus <acj.gfx@gmail.com>
2025-09-23 15:10:50 -0700

    Wired up the form

--- app/league/configuration/form/form.tsx
+++ app/league/configuration/form/form.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useRef, useState, MouseEvent } from "react";
 import validationPattern from "@/app/dataSources/validationPatterns";
 import styles from "./styles.module.scss";
@@ -9,15 +10,29 @@ export default function LeagueConfigurationForm(){
     // This is needed to allow for query selector below
     const formRef = useRef(null as HTMLFormElement | null);
     const [errorMsg, setErrorMsg] = useState("");
+    const router = useRouter();
     const formSubmit = async (e: MouseEvent)=> { 
         e.preventDefault();
         if(formRef.current){
             const formData = new FormData(formRef.current);
-            const response = await fetch("/api/league", {
-                method: "POST",
-                body: formData
-            });
-            if (!response.ok) {
+            const formObject = Object.fromEntries(formData);
+            const formDataAsJson = JSON.stringify(formObject);
+            try{
+                const response = await fetch("/api/league", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: formDataAsJson
+                });
+                if(!response.ok){
+                    setErrorMsg("There was a problem. Please check the form and try to resubmit.");
+                }
+                const result = await response.json();
+                if(result.message === "posted"){
+                    router.push("/");
+                }
+            } catch(err){
                 setErrorMsg("There was a problem. Please check the form and try to resubmit.");
             }
         }
@@ -71,8 +86,8 @@ export default function LeagueConfigurationForm(){
                     label="Google Sheets Url"
                     placeholder="example: https://docs.google.com/spreadsheets/d/testurl"
                     isRequired={true}
-                    validationPattern={validationPattern.googleSheetsUrl.string}
-                    id="googleSheetsUrl"
+                    validationPattern={validationPattern.googleSheetUrl.string}
+                    id="googleSheetUrl"
                 />
                 <Button text="Create League"
                     kind="primary"
