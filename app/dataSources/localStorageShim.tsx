f3a0632bf5f0d3d2908272d6622ce2248987ffb9
Jacob Meixner <jjm3333@gmail.com>
2025-09-25 15:35:25 -0700

    Add getLocalUserData function to shim

--- app/dataSources/localStorageShim.tsx
+++ app/dataSources/localStorageShim.tsx
@@ -8,3 +8,13 @@ export function setLocalUserData(userData: LocalUserData) {
     localStorage.setItem("userName", userData.userName);
     localStorage.setItem("googleUserId", userData.googleUserId);
 }
+
+export function getLocalUserData(): LocalUserData {
+    const userData: LocalUserData = {
+        userName: "Default User Name From localStorageShim",
+        googleUserId: "xxx-xxx-xxx"
+    };
+    userData.userName = localStorage.getItem("userName") ?? userData.userName;
+    userData.googleUserId = localStorage.getItem("googleUserId") ?? userData.googleUserId;
+    return userData;
+}
