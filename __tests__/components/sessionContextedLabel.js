0e362ea59153199fe5f09f835b8a2452e70c4781
Jacob Meixner <jjm3333@gmail.com>
2025-09-26 12:53:55 -0700

    Add a couple of tests that mirror the existing ones about userId

--- __tests__/components/sessionContextedLabel.js
+++ __tests__/components/sessionContextedLabel.js
@@ -31,4 +31,28 @@ describe("SessionContextedLabel", () => {
         // Assert
         expect(setSessionInfoMock).not.toHaveBeenCalled();
     });
+
+    it("should call to setSessionInfo when sessionInfo.googleUserId is null", () => {
+        // Arrange
+        const setSessionInfoMock = jest.fn();
+        jest.spyOn(React, "useContext").mockReturnValue({ sessionInfo: { userName: "anything...", googleUserId: null }, setSessionInfo: setSessionInfoMock });
+
+        // Act
+        render(<SessionContextedLabel/>);
+
+        // Assert
+        expect(setSessionInfoMock).toHaveBeenCalled();
+    });
+
+    it("should not call to setSessionInfo when sessionInfo.googleUserId empty string", () => {
+        // Arrange
+        const setSessionInfoMock = jest.fn();
+        jest.spyOn(React, "useContext").mockReturnValue({ sessionInfo: { userName: "anything...", googleUserId: "" }, setSessionInfo: setSessionInfoMock });
+
+        // Act
+        render(<SessionContextedLabel/>);
+
+        // Assert
+        expect(setSessionInfoMock).not.toHaveBeenCalled();
+    });
 });
