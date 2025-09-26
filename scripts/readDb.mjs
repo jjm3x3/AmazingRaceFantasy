073d61154bc9874a1f3e2dbd5682131a10f17848
Jacob Meixner <jjm3333@gmail.com>
2025-09-26 13:48:55 -0700

    Add a simple impl inspired from the seed script

---
+++ scripts/readDb.mjs
@@ -0,0 +1,30 @@
+import { Redis } from "@upstash/redis"
+
+console.log("Seeding the db");
+
+console.log("Setting up db settings bassed on vars in process.env");
+let redisOptions = {
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN
+};
+
+console.log("Connecting to: '" + redisOptions.url + "'");
+
+const redis = new Redis(redisOptions);
+
+const keyspace = "league_configuration:*";
+
+readKeyspace(keyspace);
+
+async function readKeyspace(keyspace) {
+    let fullCursor = await redis.scan("0", {match: keyspace})
+    console.log(fullCursor)
+    
+    let nextId = fullCursor[0]
+    while (nextId != 0) {
+        console.log(`Fetching next scan batch with id: '${nextId}`)
+        fullCursor = await redis.scan(nextId, {match: keyspace})
+        console.log(fullCursor)
+        nextId = fullCursor[0]
+    }
+}
