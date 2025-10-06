a31e9a6bae5dda426760c15795512b6736f96bf3
Jacob Meixner <jjm3333@gmail.com>
2025-10-05 18:42:16 -0700

    Create a user data file for survivor 49

---
+++ app/leagueData/Survivor_49.js
@@ -0,0 +1,34 @@
+
+// Contestant Ranking
+const CONTESTANT_LEAGUE_DATA = [
+    {
+        name: "Antoinette",
+        userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+        ranking: [
+
+]
+    },
+    {
+        name: "Cindy",
+        userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
+        ranking: []
+    },
+    {
+        name: "Jacob",
+        userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
+        ranking: []
+    },
+    {
+        name: "Jenny",
+        userId: "894B180E-A635-41BA-B6BC-B0AC84AE7B23",
+        ranking: []
+    },
+    {
+        name: "Sam",
+        userId: "CD9F9A71-FF9C-416A-8D0D-C268A13B021F",
+        ranking: []
+    }
+];
+
+module.exports = { CONTESTANT_LEAGUE_DATA };
+
