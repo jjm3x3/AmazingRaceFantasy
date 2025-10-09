f85023f41bd4d8f712f1bf09821cead4443630a7
Jacob Meixner <jjm3333@gmail.com>
2025-10-08 21:48:00 -0700

    Add a file for amazing race rankings

---
+++ app/leagueData/AmazingRace_38.js
@@ -0,0 +1,46 @@
+
+// Contestant Ranking
+const CONTESTANT_LEAGUE_DATA = [
+    {
+        name: "Andrew Jaicks",
+        userId: "6252275B-C6AF-427B-82A6-1F4B4A2267C1",
+        ranking: []
+    //},
+    //{
+    //    name: "Antoinette",
+    //    userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+    //    ranking: []
+    //},
+    //{
+    //    name: "Cindy",
+    //    userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
+    //    ranking: []
+    //},
+    //{
+    //    name: "Jacob",
+    //    userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
+    //    ranking: []
+    //},
+    //{
+    //    name: "Jimminy",
+    //    userId: "CAFA7731-EC3C-4B9D-8BC6-A199B6FB86A4",
+    //    ranking: []
+    //},
+    //{
+    //    name: "Rach",
+    //    userId: "E3BA8CF1-0F66-4911-88D8-A9ECFEEB37A7",
+    //    ranking: []
+    //},
+    //{
+    //    name: "Sam",
+    //    userId: "CD9F9A71-FF9C-416A-8D0D-C268A13B021F",
+    //    ranking: []
+    //},
+    //{
+    //    name: "TriscuitHead" ,
+    //    userId: "112D95CA-CACB-4E9F-BEC5-97EF91D10994",
+    //    ranking: []
+    }
+];
+
+module.exports = { CONTESTANT_LEAGUE_DATA };
