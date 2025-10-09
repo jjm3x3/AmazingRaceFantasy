30d80aea3ebb8391703517d0fe4992bb8ee2cd46
Jacob Meixner <jjm3333@gmail.com>
2025-10-08 21:53:45 -0700

    ADd Cindy

--- app/leagueData/AmazingRace_38.js
+++ app/leagueData/AmazingRace_38.js
@@ -10,11 +10,11 @@ const CONTESTANT_LEAGUE_DATA = [
         name: "Antoinette",
         userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
         ranking: [ "Kyland Young & Taylor Hale", "Jack Baham & Chelsie Baham", "Jasmair \"Jas\" Bains & Jagateshwar \"Jag\" Bains", "Tucker Des Lauriers & Eric Des Lauriers", "Kristine Bernabe & Rubina Bernabe", "Joseph Abdin & Adam Abdin", "Hannah Chaddha & Simone Chaddha", "Natalie Negrotti & Stephanie Negrotti", "Isabel \"Izzy\" Gleicher & Paige Seber", "Megan Belmonte & Matt Turner", "Angela Murray & Lexi Murray", "Kathryn \"Kat\" Dunn & Alex Romo", "Giacomo \"Jack\" Palumbo & Vincenzo \"Enzo\" Palumbo"]
-    //},
-    //{
-    //    name: "Cindy",
-    //    userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
-    //    ranking: []
+    },
+    {
+        name: "Cindy",
+        userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
+        ranking: [ "Jasmair \"Jas\" Bains & Jagateshwar \"Jag\" Bains", "Tucker Des Lauriers & Eric Des Lauriers", "Kyland Young & Taylor Hale", "Kristine Bernabe & Rubina Bernabe", "Joseph Abdin & Adam Abdin", "Hannah Chaddha & Simone Chaddha", "Natalie Negrotti & Stephanie Negrotti", "Jack Baham & Chelsie Baham", "Isabel \"Izzy\" Gleicher & Paige Seber", "Kathryn \"Kat\" Dunn & Alex Romo", "Angela Murray & Lexi Murray", "Megan Belmonte & Matt Turner", "Giacomo \"Jack\" Palumbo & Vincenzo \"Enzo\" Palumbo" ]
     //},
     //{
     //    name: "Jacob",
