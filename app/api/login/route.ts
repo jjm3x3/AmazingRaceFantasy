1597e42fcdf813df1b6595e593f7421055d13328
Jacob Meixner <jjm3333@gmail.com>
2025-10-18 11:20:32 -0700

    Extend scope of authResponse to avoid failure

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -7,6 +7,7 @@ export async function POST(request: NextRequest) {
     const client = new OAuth2Client();
     const body = await request.json();
     const clientId = process.env.GOOGLE_LOGIN_CLIENT_ID;
+    let authResponse = null;
     try {
         authResponse = await client.verifyIdToken({
             idToken: body.token,
@@ -16,7 +17,7 @@ export async function POST(request: NextRequest) {
         console.warn(`Issue verifying google identity ${authError}`);
     }
     
-    const payload:TokenPayload | undefined = authResponse.getPayload();
+    const payload:TokenPayload | undefined = authResponse !== null ? authResponse.getPayload() : undefined;
 
     if(payload){
         const googleUserId = payload["sub"];
