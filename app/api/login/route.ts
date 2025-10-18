e4f04ebaa5712959cded69f530c0d3c78df02928
Jacob Meixner <jjm3333@gmail.com>
2025-10-18 11:17:52 -0700

    Wrap tokenVerify in a try/catch

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -7,10 +7,14 @@ export async function POST(request: NextRequest) {
     const client = new OAuth2Client();
     const body = await request.json();
     const clientId = process.env.GOOGLE_LOGIN_CLIENT_ID;
-    const authResponse = await client.verifyIdToken({
-        idToken: body.token,
-        audience: clientId
-    });
+    try {
+        authResponse = await client.verifyIdToken({
+            idToken: body.token,
+            audience: clientId
+        });
+    } catch (authError) {
+        console.warn(`Issue verifying google identity ${authError}`);
+    }
     
     const payload:TokenPayload | undefined = authResponse.getPayload();
 
