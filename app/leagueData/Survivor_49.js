6999898bf136c240420019968a77f785381ce3a9
Jacob Meixner <jjm3333@gmail.com>
2025-11-06 06:33:35 -0800

    Update Matt's name

--- app/leagueData/Survivor_49.js
+++ app/leagueData/Survivor_49.js
@@ -4,27 +4,27 @@ const CONTESTANT_LEAGUE_DATA = [
     {
         name: "Antoinette",
         userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
-        ranking: [ "Jeremiah Ing", "Savannah Louie", "Jason Treul", "Rizo Velovic", "Sage Ahrens-Nichols", "Sophia \"Sophi\" Balerdi", "Steven Ramm", "Michelle \"MC\" Chukwujekwu", "Shannon Fairweather", "Matthew \"Matt\" Williams", "Jawan Pitts", "Kristina Mills", "Sophia \"Sophie\" Segreti ", "Kimberly \"Annie\" Davis", "Jake Latimer", "Nate Moore", "Alex Moore", "Nicole Mazullo" ]
+        ranking: [ "Jeremiah Ing", "Savannah Louie", "Jason Treul", "Rizo Velovic", "Sage Ahrens-Nichols", "Sophia \"Sophi\" Balerdi", "Steven Ramm", "Michelle \"MC\" Chukwujekwu", "Shannon Fairweather", "Matt Williams", "Jawan Pitts", "Kristina Mills", "Sophia \"Sophie\" Segreti ", "Kimberly \"Annie\" Davis", "Jake Latimer", "Nate Moore", "Alex Moore", "Nicole Mazullo" ]
     },
     {
         name: "Cindy",
         userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
-        ranking: [ "Jake Latimer", "Savannah Louie", "Matthew \"Matt\" Williams ", "Jeremiah Ing", "Michelle \"MC\" Chukwujekwu", "Alex Moore", "Sophia \"Sophi\" Balerdi ", "Sage Ahrens-Nichols", "Shannon Fairweather", "Jawan Pitts", "Sophia \"Sophie\" Segreti ", "Steven Ramm", "Kristina Mills", "Rizo Velovic", "Nate Moore", "Jason Treul", "Kimberly \"Annie\" Davis", "Nicole Mazullo" ]
+        ranking: [ "Jake Latimer", "Savannah Louie", "Matt Williams", "Jeremiah Ing", "Michelle \"MC\" Chukwujekwu", "Alex Moore", "Sophia \"Sophi\" Balerdi ", "Sage Ahrens-Nichols", "Shannon Fairweather", "Jawan Pitts", "Sophia \"Sophie\" Segreti ", "Steven Ramm", "Kristina Mills", "Rizo Velovic", "Nate Moore", "Jason Treul", "Kimberly \"Annie\" Davis", "Nicole Mazullo" ]
     },
     {
         name: "Jacob",
         userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
-        ranking: [ "Jake Latimer", "Michelle \"MC\" Chukwujekwu", "Savannah Louie", "Shannon Fairweather", "Sage Ahrens-Nichols", "Sophia \"Sophie\" Segreti ", "Nate Moore", "Rizo Velovic", "Alex Moore", "Matthew \"Matt\" Williams ", "Jason Treul", "Jawan Pitts", "Sophia \"Sophi\" Balerdi ", "Steven Ramm", "Jeremiah Ing", "Kimberly \"Annie\" Davis", "Kristina Mills", "Nicole Mazullo" ]
+        ranking: [ "Jake Latimer", "Michelle \"MC\" Chukwujekwu", "Savannah Louie", "Shannon Fairweather", "Sage Ahrens-Nichols", "Sophia \"Sophie\" Segreti ", "Nate Moore", "Rizo Velovic", "Alex Moore", "Matt Williams", "Jason Treul", "Jawan Pitts", "Sophia \"Sophi\" Balerdi ", "Steven Ramm", "Jeremiah Ing", "Kimberly \"Annie\" Davis", "Kristina Mills", "Nicole Mazullo" ]
     },
     {
         name: "Jenny",
         userId: "894B180E-A635-41BA-B6BC-B0AC84AE7B23",
-        ranking: [ "Savannah Louie", "Jeremiah Ing", "Jason Treul", "Rizo Velovic", "Sophia \"Sophie\" Segreti ", "Kristina Mills", "Steven Ramm", "Sage Ahrens-Nichols", "Michelle \"MC\" Chukwujekwu", "Jake Latimer", "Matthew \"Matt\" Williams ", "Jawan Pitts", "Sophia \"Sophi\" Balerdi", "Alex Moore", "Shannon Fairweather", "Nate Moore", "Kimberly \"Annie\" Davis", "Nicole Mazullo" ]
+        ranking: [ "Savannah Louie", "Jeremiah Ing", "Jason Treul", "Rizo Velovic", "Sophia \"Sophie\" Segreti ", "Kristina Mills", "Steven Ramm", "Sage Ahrens-Nichols", "Michelle \"MC\" Chukwujekwu", "Jake Latimer", "Matt Williams", "Jawan Pitts", "Sophia \"Sophi\" Balerdi", "Alex Moore", "Shannon Fairweather", "Nate Moore", "Kimberly \"Annie\" Davis", "Nicole Mazullo" ]
     },
     {
         name: "Sam",
         userId: "CD9F9A71-FF9C-416A-8D0D-C268A13B021F",
-        ranking: [ "Savannah Louie", "Sage Ahrens-Nichols", "Nate Moore", "Jeremiah Ing", "Kristina Mills", "Sophia \"Sophi\" Balerdi ", "Alex Moore", "Shannon Fairweather", "Jake Latimer", "Jawan Pitts", "Jason Treul", "Michelle \"MC\" Chukwujekwu", "Steven Ramm", "Sophia \"Sophie\" Segreti ", "Matthew \"Matt\" Williams ", "Rizo Velovic", "Kimberly \"Annie\" Davis", "Nicole Mazullo" ]
+        ranking: [ "Savannah Louie", "Sage Ahrens-Nichols", "Nate Moore", "Jeremiah Ing", "Kristina Mills", "Sophia \"Sophi\" Balerdi ", "Alex Moore", "Shannon Fairweather", "Jake Latimer", "Jawan Pitts", "Jason Treul", "Michelle \"MC\" Chukwujekwu", "Steven Ramm", "Sophia \"Sophie\" Segreti ", "Matt Williams", "Rizo Velovic", "Kimberly \"Annie\" Davis", "Nicole Mazullo" ]
     }
 ];
 
