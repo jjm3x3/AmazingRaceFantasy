4f117ab1ec9de4614c6cd041410c99db12b5e47f
Jacob Meixner <jjm3333@gmail.com>
2025-11-08 21:29:39 -0800

    Update backup endpoint to be gatted by a password

--- app/api/backup/route.ts
+++ app/api/backup/route.ts
@@ -1,6 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+    console.log("cron endpoint hit");
+
+    const authHeader = request.headers.get("authorization");
+
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        console.log("cron caller unathorized");
+        return new Response("Unauthorized", {
+            status: 401,
+        });
+    }
+
     console.log("cron function triggered");
 
     return NextResponse.json({ success: true });
