381cfb1b4d0d62bcf3a4d4412867c889b3901281
Jacob Meixner <jjm3333@gmail.com>
2025-11-27 14:50:29 -0800

    Get something working but it breaks a test

--- app/api/backup/route.ts
+++ app/api/backup/route.ts
@@ -4,6 +4,7 @@ import { awsCredentialsProvider } from "@vercel/oidc-aws-credentials-provider";
 
 const AWS_REGION = process.env.AWS_REGION;
 const AWS_ROLE_ARN = process.env.AWS_ROLE_ARN;
+const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
 const s3Client = new S3.S3Client({
     region: AWS_REGION,
@@ -25,6 +26,16 @@ export async function GET(request: NextRequest) {
     }
 
     console.log("cron function triggered");
+
+    const result = await s3Client.send(
+        new S3.PutObjectCommand({
+            Bucket: S3_BUCKET_NAME,
+            Key: "test",
+            Body: "some text in a file..."
+        })
+    );
+
+    console.log(result);
 
     return NextResponse.json({ success: true });
 }
