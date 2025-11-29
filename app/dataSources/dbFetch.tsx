188ff363130c7edd201456641e3c06bfc1be7159
Jacob Meixner <jjm3333@gmail.com>
2025-11-28 19:26:20 -0800

    Use general purpose function in original use place

--- app/dataSources/dbFetch.tsx
+++ app/dataSources/dbFetch.tsx
@@ -57,18 +57,8 @@ export async function hasContestantData(keyPrefix:string): Promise<boolean>  {
 }
 
 export async function getLeagueConfigurationKeys(): Promise<string[]> {
-    const redis = new Redis({
-        url: process.env.KV_REST_API_URL,
-        token: process.env.KV_REST_API_TOKEN
-    });
-    let leagueConfigurationCursor = await redis.scan("0", {match: "league_configuration:*"});
-    let leagueConfigurationKeys = leagueConfigurationCursor[1];
-    let cursorStart = leagueConfigurationCursor[0];
-    while(cursorStart !== "0"){
-        leagueConfigurationCursor = await redis.scan(cursorStart, {match: "league_configuration:*"});
-        leagueConfigurationKeys = leagueConfigurationKeys.concat(leagueConfigurationCursor[1]);
-        cursorStart = leagueConfigurationCursor[0];
-    }
+    const leagueConfigurationKeys = await getAllKeys("league_configuration:*")
+
     if(leagueConfigurationKeys !== null){
         return leagueConfigurationKeys;
     } else {
