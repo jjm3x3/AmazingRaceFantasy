2005f3e55ca4a7486ac931ed555f9d8b2907faef
Jacob Meixner <jjm3333@gmail.com>
2025-11-28 17:26:15 -0800

    Add a test asserting we are calling S3 at all

--- __tests__/api/backup/route.js
+++ __tests__/api/backup/route.js
@@ -47,5 +47,19 @@ describe("backup GET", () => {
         expect(response).not.toBeNull();
         expect(response.status).toEqual(401);
     });
+
+    it("should call saveObject from an s3Provider layer", async () => {
+        // Arrange
+        const request = {
+            headers: { get: () => `Bearer ${aSecretValue}` }
+        };
+
+        // Act
+        await GET(request);
+
+        // Assert
+        expect(saveObject).toHaveBeenCalled();
+    });
+
 });
 
