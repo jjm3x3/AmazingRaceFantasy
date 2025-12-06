a182e1a373ccadcf21b88a4f3798625f1514608c
jjm3x3 <jjm3x3@users.noreply.github.com>
2025-12-06 11:30:32 -0800

    Feature/remove extraneous league configs (#405)
    * Remove older extraneous configs from seed script
    
    * Make changes to pass linter
    
    * Update lint rules to allow unused function
    
    * Remove extra data stored in source

--- scripts/seedDb.mjs
+++ scripts/seedDb.mjs
@@ -6,11 +6,7 @@ import bigBrother26Data from "../app/leagueData/BigBrother_26.js"
 import bigBrother27Data from "../app/leagueData/BigBrother_27.js"
 import survivor47Data from "../app/leagueData/Survivor_47.js"
 import survivor49Data from "../app/leagueData/Survivor_49.js"
-import amazingRace35LeagueConfiguration from "../app/leagueConfiguration/AmazingRace_35.js"
-import amazingRace36LeagueConfiguration from "../app/leagueConfiguration/AmazingRace_36.js"
-import amazingRace37LeagueConfiguration from "../app/leagueConfiguration/AmazingRace_37.js"
-import bigBrother26LeagueConfiguration from "../app/leagueConfiguration/BigBrother_26.js"
-import survivor47LeagueConfiguration from "../app/leagueConfiguration/Survivor_47.js"
+//import amazingRace37LeagueConfiguration from "../app/leagueConfiguration/AmazingRace_37.js"
 import { Redis } from "@upstash/redis"
 
 console.log("Seeding the db");
@@ -36,11 +32,10 @@ await recreateLeagueData("survivor:47:", survivor47Data)
 await recreateLeagueData("survivor:49:", survivor49Data)
 
 // Create league configuration data
-await recreateLeagueConfigurationData(`league_configuration:${amazingRace35LeagueConfiguration.leagueStatus}:amazing_race:35`, amazingRace35LeagueConfiguration)
-await recreateLeagueConfigurationData(`league_configuration:${amazingRace36LeagueConfiguration.leagueStatus}:amazing_race:36`, amazingRace36LeagueConfiguration)
-await recreateLeagueConfigurationData(`league_configuration:${amazingRace37LeagueConfiguration.leagueStatus}:amazing_race:37`, amazingRace37LeagueConfiguration)
-await recreateLeagueConfigurationData(`league_configuration:${bigBrother26LeagueConfiguration.leagueStatus}:big_brother:26`, bigBrother26LeagueConfiguration)
-await recreateLeagueConfigurationData(`league_configuration:${survivor47LeagueConfiguration.leagueStatus}:survivor:47`, survivor47LeagueConfiguration)
+// Note: leaving this commented out code, should we need to add another config
+//     this way, otherwise new league configs should be added using the
+//     league/configuration page instead.
+//await recreateLeagueConfigurationData(`league_configuration:${amazingRace37LeagueConfiguration.leagueStatus}:amazing_race:37`, amazingRace37LeagueConfiguration)
 
 let fullCursor = await redis.scan("0", {match: "*"})
 console.log(fullCursor)
@@ -76,7 +71,7 @@ async function recreateLeagueData(leagueKeyPrefix, dataRepo) {
 }
 
 
-async function recreateLeagueConfigurationData(leagueConfigurationKey, dataRepo) {
+async function _recreateLeagueConfigurationData(leagueConfigurationKey, dataRepo) {
 
     console.log(`Setting league configuration data for '${leagueConfigurationKey}'`);
     
