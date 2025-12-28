79ed66ea5557a3840cd4c2251e6a4ca964018eaa
Jacob Meixner <jjm3333@gmail.com>
2025-12-27 17:49:10 -0800

    Add a little message to prompt a user

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,13 @@ import GoogleLoginButton from "@/app/components/navigation/google-login-btn";
 export default async function Login() {
 
     return (
-        <div className="flex justify-center">
-            <GoogleLoginButton/>
+        <div>
+            <div className="flex justify-center">
+                <p>Login</p>
+            </div>
+            <div className="flex justify-center">
+                <GoogleLoginButton/>
+            </div>
         </div>
     );
 }
