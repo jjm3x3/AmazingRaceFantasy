0b4a5773d0a218d314db6c4bd8aa999fda6445f0
Jacob Meixner <jjm3333@gmail.com>
2026-01-17 20:49:40 -0800

    Update create page with new text

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,14 +1,14 @@
 import GoogleLoginButton from "@/app/components/navigation/google-login-btn";
 import styles from "./styles.module.scss"
 
-export default async function Login() {
+export default async function CreateAccount() {
 
     return (
         <div className="grid place-items-center h-screen">
             <div className="text-center">
-                <p className="text-xl">Login</p>
+                <p className="text-xl">Create Account</p>
                 <GoogleLoginButton classes={styles.google_login_btn} />
-                <p>Don't have an account? Create One.</p>
+                <p>Already have an account? Login</p>
             </div>
         </div>
     );
