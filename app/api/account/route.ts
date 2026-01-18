300799bb60516fcf847193164ddcefa047611801
Antoinette Janus <acj.gfx@gmail.com>
2026-01-17 21:19:44 -0800

    Removed unnecessary else

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -30,39 +30,38 @@ export async function POST(request: NextRequest) {
 
     if(!payload){
         return NextResponse.json({ "error": badGatewayErrorMessage }, { status: 502 });
+    }
+    const googleUserId = payload["sub"];
+    // Check if user already exists with googleUserId
+    const existingGoogleUser = await getUser(googleUserId);
+    if (existingGoogleUser){
+        return NextResponse.json({"error": "User already exists with the provided google user id"}, {status: 409});
     } else {
-        const googleUserId = payload["sub"];
-        // Check if user already exists with googleUserId
-        const existingGoogleUser = await getUser(googleUserId);
-        if (existingGoogleUser){
-            return NextResponse.json({"error": "User already exists with the provided google user id"}, {status: 409});
-        } else {
-            const uuid = randomUUID();
-            const userDbObj = {
-                googleUserId: googleUserId,
-                userId: uuid
-            }
-            writeGoogleUserDataWithId(userDbObj);
-    
-            // Data to send to the front end
-            const userObjForClient = {
-                email: payload?.email,
-                name: {
-                    firstName: payload?.given_name,
-                    lastName: payload?.family_name
-                },
-                googleUserId: googleUserId,
-                userId: uuid
-            }
-            const response = NextResponse.json(userObjForClient);
-            await createSession({
-                envelope: body.envelope, 
-                exp: payload.exp,
-                iat: payload.iat, 
-                sub: uuid,
-                response
-            });
-            return response;
+        const uuid = randomUUID();
+        const userDbObj = {
+            googleUserId: googleUserId,
+            userId: uuid
         }
+        writeGoogleUserDataWithId(userDbObj);
+
+        // Data to send to the front end
+        const userObjForClient = {
+            email: payload?.email,
+            name: {
+                firstName: payload?.given_name,
+                lastName: payload?.family_name
+            },
+            googleUserId: googleUserId,
+            userId: uuid
+        }
+        const response = NextResponse.json(userObjForClient);
+        await createSession({
+            envelope: body.envelope, 
+            exp: payload.exp,
+            iat: payload.iat, 
+            sub: uuid,
+            response
+        });
+        return response;
     }
 }
