04ca15dd5ac2cafcadd46e3a4142e5321ae5d1e9
Jacob Meixner <jjm3333@gmail.com>
2026-01-17 19:39:25 -0800

    Add page which is an exact copy of Login page

---
+++ app/create-account/page.tsx
@@ -0,0 +1,15 @@
+import GoogleLoginButton from "@/app/components/navigation/google-login-btn";
+import styles from "./styles.module.scss"
+
+export default async function Login() {
+
+    return (
+        <div className="grid place-items-center h-screen">
+            <div className="text-center">
+                <p className="text-xl">Login</p>
+                <GoogleLoginButton classes={styles.google_login_btn} />
+                <p>Don't have an account? Create One.</p>
+            </div>
+        </div>
+    );
+}
