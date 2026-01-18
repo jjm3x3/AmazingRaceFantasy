f8aa6dd1471df6a7f2362e5629c68b651c07b412
Jacob Meixner <jjm3333@gmail.com>
2026-01-17 21:20:27 -0800

    Update buttons div container id

--- app/components/baseComponents/googleButton/google-create-btn.tsx
+++ app/components/baseComponents/googleButton/google-create-btn.tsx
@@ -52,6 +52,6 @@ export default function GoogleCreateButton({classes}: {classes: string}){
     }
 
     return (<>
-        <div ref={googleCreateRef} id="google_login_btn" className={classes}/>
+        <div ref={googleCreateRef} id="google_create_btn" className={classes}/>
     </>);
 }
