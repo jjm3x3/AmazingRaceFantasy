cbba364733015e523c15b9bd5cacb512a5e121f3
Jacob Meixner <jjm3333@gmail.com>
2026-01-19 15:12:16 -0800

    Remove extra logging and add comment calling out an implicit assumption

--- app/components/baseComponents/googleButton/createButton.tsx
+++ app/components/baseComponents/googleButton/createButton.tsx
@@ -44,8 +44,7 @@ export default function GoogleCreateButton({classes}: {classes: string}){
 
     async function handleLogin(response: Response) {
         if (response.status === 409) {
-            console.error("That account already exists, try logging in");
-            setError(true);
+            setError(true); // since we are using a boolean we are assuming this is the only error possible at this time
         } else {
             const data = await response.json();
             setLocalUserData({userName: data.name.firstName, googleUserId: data.googleUserId});
