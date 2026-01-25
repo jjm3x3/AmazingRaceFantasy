0b08e3c6e8df14b95eed9e33e88dddcbccd6d9fa
Antoinette Janus <acj.gfx@gmail.com>
2026-01-24 18:20:59 -0800

    Refactored createButton into googleButton

--- app/components/baseComponents/googleButton/createButton.tsx
+++ app/components/baseComponents/googleButton/createButton.tsx
@@ -1,62 +1,9 @@
 "use client"
-import { useEffect, useRef, useContext, useState } from "react";
-import { SessionContext } from "@/app/contexts/session";
-import { setLocalUserData } from "@/app/dataSources/localStorageShim";
-import config from "@/app/config";
-import { useRouter } from "next/navigation";
-import { GoogleLogin } from "./models";
-import ErrorMessage from "@/app/components/baseComponents/components/errorMessage/errorMessage";
+import GoogleButton from "./googleButton";
 
 export default function GoogleCreateButton({classes}: {classes: string}){
-    const { setSessionInfo, googleSdkLoaded } = useContext(SessionContext);
-    const googleCreateRef = useRef(null);
-    const router = useRouter();
-    const [getError, setError] = useState(false);
-
-    useEffect(()=> {
-        if(googleSdkLoaded && window.google){
-            const google = window.google;
-            google.accounts.id.initialize({
-                client_id: config.googleAuthClientId,
-                callback: handleCredentialResponse,
-                use_fedcm_for_prompt: true
-            });
-            const parent = googleCreateRef.current;
-            if(googleCreateRef && parent){
-                google.accounts.id.renderButton(parent, {
-                    text: "signup_with",
-                    size: "medium",
-                    logo_alignment: "left",
-                    shape: "rectangular",
-                    theme: "outline",
-                    type: "standard"
-                });
-            }
-        }
-    }, [googleSdkLoaded]);
-    
-    function handleCredentialResponse(response:GoogleLogin) {
-        fetch("/api/account", {
-            method: "POST",
-            body: JSON.stringify({ token: response.credential }),
-        }).then(handleLogin);
-    }
-
-    async function handleLogin(response: Response) {
-        if (response.status === 409) {
-            setError(true); // since we are using a boolean we are assuming this is the only error possible at this time
-        } else {
-            const data = await response.json();
-            setLocalUserData({userName: data.name.firstName, googleUserId: data.googleUserId});
-            setSessionInfo({isLoggedIn: true, userName: data.name.firstName, googleUserId: data.googleUserId});
-            router.push("/");
-        }
-    }
-
-    return (<>
-        <div ref={googleCreateRef} id="google_create_btn" className={classes}/>
-        { getError ? <div>
-            <ErrorMessage message="There was an issue creating an account. Try logging in instead"/>
-        </div> : <div/> }
-    </>);
+    return <GoogleButton classes={classes} 
+        text="signup_with" 
+        endpoint="/api/account" 
+        errorMessage="There was an issue creating an account. Try logging in instead"/>;
 }
