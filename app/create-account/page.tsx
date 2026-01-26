36b36c6bc4598e691dc4d72ee84bb541d46e0b87
Jacob Meixner <jjm3333@gmail.com>
2026-01-25 17:36:06 -0800

    Add link back to login

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import GoogleCreateButton from "@/app/components/baseComponents/googleButton/createButton";
 import styles from "./styles.module.scss"
 
@@ -8,7 +9,7 @@ export default async function CreateAccount() {
             <div className="text-center">
                 <p className="text-xl">Create Account</p>
                 <GoogleCreateButton classes={styles.google_create_btn} />
-                <p>Already have an account? Login</p>
+                <p>Already have an account? <Link className="standard-link" href={"/login"}>Login</Link></p>
             </div>
         </div>
     );
