344e50a5bb905b42f6a5cd72af88b35a97321fc4
Jacob Meixner <jjm3333@gmail.com>
2026-01-29 22:23:08 -0800

    Changing the login and create account headers to h3

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -7,7 +7,7 @@ export default async function CreateAccount() {
     return (
         <div className="grid place-items-center h-screen">
             <div className="text-center">
-                <p className="text-xl">Create Account</p>
+                <h3 className="text-xl">Create Account</h3>
                 <GoogleCreateButton classes={styles.google_create_btn} />
                 <p>Already have an account? <Link className="standard-link" href={"/login"}>Login</Link></p>
             </div>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,7 @@ export default async function Login() {
     return (
         <div className="grid place-items-center h-screen">
             <div className="text-center">
-                <p className="text-xl">Login</p>
+                <h3 className="text-xl">Login</h3>
                 <GoogleLoginButton classes={styles.google_login_btn} />
                 <p>Don't have an account? <Link className="standard-link" href={"/create-account"}>Create One</Link></p>
             </div>
