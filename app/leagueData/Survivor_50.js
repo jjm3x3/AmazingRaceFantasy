459cd1e8d980e79136acdf3388985cf09def0171
Jacob Meixner <jjm3333@gmail.com>
2026-03-13 18:04:07 -0700

    Correct Antoinettes ranking
    Q
    Coach
    Nicole
    Ozzy

--- app/leagueData/Survivor_50.js
+++ app/leagueData/Survivor_50.js
@@ -4,7 +4,7 @@ const CONTESTANT_LEAGUE_DATA = [
     {
         name: "Antoinette",
         userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
-        ranking: [ "Christian Hubicki", "Joe Hunter", "Cirie Fields", "Aubry Bracco", "Kamilla Karthigesu", "Q Burdette", "Charlie Davis", "Jonathan Young", "Colby Donaldson", "Coach Wade", "Dee Valladares", "Genevieve Mushaluk", "Chrissy Hofbeck", "Angelina Keeley", "Emily Flippen", "Mike White", "Rick Devens", "Tiffany Nicole Ervin", "Stephenie LaGrossa Kendrick", "Savannah Louie", "Ozzy Lusth", "Rizo Velovic", "Jenna Lewis-Dougherty", "Kyle Fraser" ]
+        ranking: [ "Christian Hubicki", "Joe Hunter", "Cirie Fields", "Aubry Bracco", "Kamilla Karthigesu", "Quintavius \"Q\" Burdette", "Charlie Davis", "Jonathan Young", "Colby Donaldson", "Benjamin \"Coach\" Wade", "Dee Valladares", "Genevieve Mushaluk", "Chrissy Hofbeck", "Angelina Keeley", "Emily Flippen", "Mike White", "Rick Devens", "Tiffany Ervin", "Stephenie LaGrossa Kendrick", "Savannah Louie", "Oscar \"Ozzy\" Lusth", "Rizo Velovic", "Jenna Lewis-Dougherty", "Kyle Fraser" ]
     }
 ];
 
