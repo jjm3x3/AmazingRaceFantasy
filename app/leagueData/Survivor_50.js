1212fc7e575f9e6d66f829ff457c06ad6d8672b6
Jacob Meixner <jjm3333@gmail.com>
2026-03-13 20:15:17 -0700

    Add Cindy's ranking

--- app/leagueData/Survivor_50.js
+++ app/leagueData/Survivor_50.js
@@ -5,6 +5,11 @@ const CONTESTANT_LEAGUE_DATA = [
         name: "Antoinette",
         userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
         ranking: [ "Christian Hubicki", "Joe Hunter", "Cirie Fields", "Aubry Bracco", "Kamilla Karthigesu", "Quintavius \"Q\" Burdette", "Charlie Davis", "Jonathan Young", "Colby Donaldson", "Benjamin \"Coach\" Wade", "Dee Valladares", "Genevieve Mushaluk", "Chrissy Hofbeck", "Angelina Keeley", "Emily Flippen", "Mike White", "Rick Devens", "Tiffany Ervin", "Stephenie LaGrossa Kendrick", "Savannah Louie", "Oscar \"Ozzy\" Lusth", "Rizo Velovic", "Jenna Lewis-Dougherty", "Kyle Fraser" ]
+    },
+    {
+        name: "Cindy",
+        userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
+        ranking: [ "Benjamin \"Coach\" Wade", "Cirie Fields", "Joe Hunter", "Savannah Louie", "Rick Devens", "Mike White", "Oscar \"Ozzy\" Lusth", "Genevieve Mushaluk", "Colby Donaldson", "Angelina Keeley", "Aubry Bracco", "Charlie Davis", "Chrissy Hofbeck", "Christian Hubicki", "Dee Valladares", "Emily Flippen", "Jonathan Young", "Kamilla Karthigesu", "Quintavius \"Q\" Burdette", "Stephenie LaGrossa Kendrick", "Tiffany Ervin", "Rizo Velovic", "Kyle Fraser", "Jenna Lewis-Dougherty"]
     }
 ];
 
