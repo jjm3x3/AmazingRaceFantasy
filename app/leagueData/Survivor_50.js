ee26483f6788562271b46cff9fbf97d30a20e83e
Jacob Meixner <jjm3333@gmail.com>
2026-03-13 20:25:21 -0700

    Add Jacob's ranking

--- app/leagueData/Survivor_50.js
+++ app/leagueData/Survivor_50.js
@@ -10,6 +10,11 @@ const CONTESTANT_LEAGUE_DATA = [
         name: "Cindy",
         userId: "685AAAF4-97DB-4266-A0B7-E61E2C6CA22E",
         ranking: [ "Benjamin \"Coach\" Wade", "Cirie Fields", "Joe Hunter", "Savannah Louie", "Rick Devens", "Mike White", "Oscar \"Ozzy\" Lusth", "Genevieve Mushaluk", "Colby Donaldson", "Angelina Keeley", "Aubry Bracco", "Charlie Davis", "Chrissy Hofbeck", "Christian Hubicki", "Dee Valladares", "Emily Flippen", "Jonathan Young", "Kamilla Karthigesu", "Quintavius \"Q\" Burdette", "Stephenie LaGrossa Kendrick", "Tiffany Ervin", "Rizo Velovic", "Kyle Fraser", "Jenna Lewis-Dougherty"]
+    },
+    {
+        name: "Jacob",
+        userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
+        ranking: [ "Emily Flippen", "Quintavius \"Q\" Burdette", "Joe Hunter", "Savannah Louie", "Jonathan Young", "Benjamin \"Coach\" Wade", "Christian Hubicki", "Oscar \"Ozzy\" Lusth", "Aubry Bracco", "Kamilla Karthigesu", "Genevieve Mushaluk", "Tiffany Ervin", "Rick Devens", "Stephenie LaGrossa Kendrick", "Cirie Fields", "Dee Valladares", "Colby Donaldson", "Chrissy Hofbeck", "Charlie Davis", "Rizo Velovic", "Angelina Keeley", "Mike White", "Kyle Fraser", "Jenna Lewis-Dougherty" ]
     }
 ];
 
