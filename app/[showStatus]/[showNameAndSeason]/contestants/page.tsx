c058dd04e14f28d93a7fc42c41921b13cd470b45
Antoinette Janus <acj.gfx@gmail.com>
2026-03-19 19:44:22 -0700

    Added select to teamlist with toggle

--- app/[showStatus]/[showNameAndSeason]/contestants/page.tsx
+++ app/[showStatus]/[showNameAndSeason]/contestants/page.tsx
@@ -1,7 +1,7 @@
 import styles from "./contestantsPageContent.module.scss";
 import { parseEntities } from "@/app/utils/entityParserSwitch"
 import { getWikipediaContestantData } from "../../../dataSources/wikiFetch";
-import { getLeagueConfigurationData, getLeagueConfigurationKeys } from "@/app/dataSources/dbFetch";
+import { getLeagueConfigurationData, getLeagueConfigurationKeys, getContestantData } from "@/app/dataSources/dbFetch";
 import { getUrlParams } from "@/app/utils/pages";
 import TeamListWithToggle from "./teamListWithToggle";
 // This forces Next to only generate routes that exist in generateStaticParams, otherwise return a 404
@@ -28,22 +28,22 @@ export default async function Contestants({ params }: {
     const friendlyShowName = showAndSeasonArr.join(" ");
     // "Dynamically" (still static site generated) retrieving modules
     const leagueConfigurationData = await getLeagueConfigurationData(`league_configuration:${showStatus}:${showName}:${showSeason}`);
-    const { wikiApiUrl, wikiPageUrl, castPhrase, competitingEntityName } = leagueConfigurationData;
+    const { wikiApiUrl, wikiPageUrl, castPhrase, competitingEntityName, contestantLeagueDataKeyPrefix } = leagueConfigurationData;
 
     const wikiContestants = await getWikipediaContestantData(wikiApiUrl, castPhrase);
 
     const final = parseEntities(wikiContestants, showName);
     const randomizedContestants = [...final].sort(() => Math.random() - 0.5);
-
     // This is a workaround to ensure the data is serializable and can be passed to a client-side component.
     // To understand better, see: https://stackoverflow.com/questions/77091418/warning-only-plain-objects-can-be-passed-to-client-components-from-server-compo
     const parsedFinal = JSON.parse(JSON.stringify(randomizedContestants));
+    const contestantRoundData = await getContestantData(contestantLeagueDataKeyPrefix);
 
     return (
         <div>
             <h1 className="text-2xl text-center">Contestants</h1>
             <p className={`text-lg text-center ${styles.contestantsCount}`}>{parsedFinal.length} {competitingEntityName}</p>
-            <TeamListWithToggle contestantsData={parsedFinal}/>
+            <TeamListWithToggle playerData={contestantRoundData} contestantsData={parsedFinal}/>
             <div>
                 <p>
                     Data provided by <a className="standard-link" href={wikiPageUrl}>Wikipedia</a> for this season of {friendlyShowName}
--- app/[showStatus]/[showNameAndSeason]/contestants/teamListWithToggle.tsx
+++ app/[showStatus]/[showNameAndSeason]/contestants/teamListWithToggle.tsx
@@ -1,17 +1,67 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CompetingEntity from "@/app/models/CompetingEntity"
-import { CheckboxToggle } from "@/app/components/baseComponents";
+import League from "@/app/models/League";
+import { CheckboxToggle, Select } from "@/app/components/baseComponents";
 import TeamList from "@/app/components/teamList";
 import styles from "./contestantsPageContent.module.scss";
 
-export default function TeamListWithToggle({ contestantsData }: { 
+type PlayerData = {
+    userId: string,
+    name: string,
+    ranking: string[]
+}
+
+export default function TeamListWithToggle({ playerData, contestantsData }: { 
+    playerData: PlayerData[],
     contestantsData: CompetingEntity[]
 }) {
     const [showEliminationStatus, setShowEliminationStatus] = useState(false);
+    const defaultDataSelectOption = {
+        value: "default",
+        text: "Select a contestant",
+        id: "default",
+        key: "default",
+        teamList: contestantsData
+    }
+    const [selectedContestant, setSelectedContestant] = useState(defaultDataSelectOption);
+    const [selectOptions, setSelectedOptions] = useState([defaultDataSelectOption]);
+    const league = new League(contestantsData);
+    useEffect(() => {
+        const fetchData = async () => {
+            const playerDataSelectOptions = playerData.map(contestant => {
+                return {
+                    key: contestant.name.toLowerCase().replace(/\s/g, "-"),
+                    text: contestant.name,
+                    value: contestant.name.toLowerCase().replace(/\s/g, "-"),
+                    id: contestant.name.toLowerCase().replace(/\s/g, "-"),
+                    teamList: league.getTeamList(contestant.ranking)
+                }
+            });
+            const allSelectOptions = [defaultDataSelectOption, ...playerDataSelectOptions];
+            setSelectedOptions(allSelectOptions);
+        }
+        if(selectOptions.length === 1){
+            fetchData();
+        }
+    }, []);
+
+    const onSelectHandler = (e)=> {
+        const selectedValue = e.target.value;
+        const selectedOption = selectOptions.find(option => option.value === selectedValue);
+        if (selectedOption) {
+            setSelectedContestant(selectedOption);
+        }
+    }
+
+
     return (
         <>
+            <Select labelText={defaultDataSelectOption.text} 
+                selectOptions={selectOptions} 
+                id="player-selector"
+                changeHandler={onSelectHandler}/>
             <div className={`text-center ${styles.filterBar}`}>
                 <CheckboxToggle
                     id="contestant-elimination-status-toggle"
@@ -22,7 +72,7 @@ export default function TeamListWithToggle({ contestantsData }: {
             </div>
             <div className="text-center">
                 <TeamList
-                    teamList={contestantsData}
+                    teamList={selectedContestant.teamList}
                     showEliminationStatus={showEliminationStatus}
                 />
             </div>
