68038b3f5841fa35461f0454f55e4d09c9173d8b
Antoinette Janus <acj.gfx@gmail.com>
2026-03-31 19:31:24 -0700

    Added button story

---
+++ app/components/baseComponents/components/button/button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+ 
+import Button from "./button";
+ 
+const meta = {
+    // 👇 The component you're working on
+    component: Button,
+} satisfies Meta<typeof Button>;
+ 
+export default meta;
+// 👇 Type helper to reduce boilerplate 
+type Story = StoryObj<typeof meta>;
+
+// 👇 A story named Primary that renders `<Button primary label="Button" />`
+export const Primary: Story = {
+    args: {
+        text: "Test Button",
+        kind: "primary",
+        id:"test-btn",
+        type: "button",
+        disabled: false,
+        clickHandler: ()=> {}
+    },
+};
