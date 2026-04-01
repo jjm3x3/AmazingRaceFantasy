c597c266574c099509b4103e91806aabf60d3167
Antoinette Janus <acj.gfx@gmail.com>
2026-03-31 19:15:40 -0700

    486 set up storybook (#490)
    * Added additional necessary updates and packages
    
    * Set up
    
    * Changed where to search for stories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,52 +1,50 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 
-export default [
-    {
-        ignores: [".next", "node_modules"],
-        settings: {
-            react: {
-                version: "detect"
-            }
+export default [{
+    ignores: [".next", "node_modules"],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+}, pluginJs.configs.recommended, ...tseslint.configs.recommended, pluginReactConfig, {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+            ...globals.node,
+            browser: true,
+            module: true
         }
     },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReactConfig,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                ...globals.jest,
-                ...globals.node,
-                browser: true,
-                module: true
-            }
-        },
-        rules: {
-            "react/react-in-jsx-scope": "off",
-            "react/no-unescaped-entities": "off",
-            "no-unused-vars": ["error", {
-                argsIgnorePattern: "^_[a-zA-Z]*",
-                varsIgnorePattern: "^_[a-zA-Z]*"
-            }],
-            "@typescript-eslint/no-unused-vars": ["error", {
-                argsIgnorePattern: "^_[a-zA-Z]*",
-                varsIgnorePattern: "^_[a-zA-Z]*"
-            }],
-            indent: [
-                "error",
-                4
-            ],
-            "linebreak-style": [
-                "error",
-                "unix"
-            ],
-            quotes: [
-                "error",
-                "double"
-            ],
-        }
-    }];
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/no-unescaped-entities": "off",
+        "no-unused-vars": ["error", {
+            argsIgnorePattern: "^_[a-zA-Z]*",
+            varsIgnorePattern: "^_[a-zA-Z]*"
+        }],
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_[a-zA-Z]*",
+            varsIgnorePattern: "^_[a-zA-Z]*"
+        }],
+        indent: [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        quotes: [
+            "error",
+            "double"
+        ],
+    }
+}, ...storybook.configs["flat/recommended"]];
