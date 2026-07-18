0502f0051d174dfa56aaef3ee0938992e67f0111
jjm3x3 <jjm3x3@users.noreply.github.com>
2026-07-17 23:41:27 -0700

    Feature/add bb 22 rankings (#507)
    * Add absolute minimum file
    
    * Add that file to seeding script
    
    * Add Antoinette's data
    
    * Add Jacob and Sam

---
+++ app/leagueData/BigBrother_28.js
@@ -0,0 +1,21 @@
+
+// Contestant Ranking
+const CONTESTANT_LEAGUE_DATA = [
+    {
+        name: "Antoinette",
+        userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+        ranking: [ "Dee Valladares", "Rick Devens", "Kamuela \"Kamu\" Kirk", "Angela Murray", "Chuk Anyanwu", "Drew Campbell", "Lyric Medeiros", "Jason De Puy", "Haley Thogmartin", "Jack \"Rome\" Seymour", "LaTrice Verrett", "Barrett Pfeiffer", "Taylor Brown", "Melody Morris", "Yash Patel", "Mallory Aurichio", "Ashley Trail" ]
+    },
+    {
+        name: "Jacob",
+        userId: "C7D10281-8879-4F41-929B-723EFBE4A1C4",
+        ranking: [ "Dee Valladares", "Kamuela \"Kamu\" Kirk", "LaTrice Verrett", "Rick Devens", "Haley Thogmartin", "Chuk Anyanwu", "Lyric Medeiros", "Jack \"Rome\" Seymour", "Jason De Puy", "Angela Murray", "Drew Campbell", "Barrett Pfeiffer", "Yash Patel", "Taylor Brown", "Mallory Aurichio", "Melody Morris", "Ashley Trail" ]
+    },
+    {
+        name: "Sam",
+        userId: "CD9F9A71-FF9C-416A-8D0D-C268A13B021F",
+        ranking: [ "Barrett Pfeiffer", "Drew Campbell", "Rick Devens", "Angela Murray", "Dee Valladares", "Kamuela \"Kamu\" Kirk", "Chuk Anyanwu", "LaTrice Verrett", "Haley Thogmartin", "Lyric Medeiros", "Melody Morris", "Jack \"Rome\" Seymour", "Jason De Puy", "Taylor Brown", "Mallory Aurichio", "Yash Patel", "Ashley Trail" ]
+    }
+];
+
+module.exports = { CONTESTANT_LEAGUE_DATA };
