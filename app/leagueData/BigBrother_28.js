e856681b558b46157f307e934919c71e9df0b283
Jacob Meixner <jjm3333@gmail.com>
2026-07-17 23:21:05 -0700

    Add Antoinette's data

--- app/leagueData/BigBrother_28.js
+++ app/leagueData/BigBrother_28.js
@@ -2,6 +2,9 @@
 // Contestant Ranking
 const CONTESTANT_LEAGUE_DATA = [
     {
+        name: "Antoinette",
+        userId: "DCC9DCDC-AE5C-4A53-AF09-23F3C957D60B",
+        ranking: [ "Dee Valladares", "Rick Devens", "Kamuela \"Kamu\" Kirk", "Angela Murray", "Chuk Anyanwu", "Drew Campbell", "Lyric Medeiros", "Jason De Puy", "Haley Thogmartin", "Jack \"Rome\" Seymour", "LaTrice Verrett", "Barrett Pfeiffer", "Taylor Brown", "Melody Morris", "Yash Patel", "Mallory Aurichio", "Ashley Trail" ]
     }
 ];
 
