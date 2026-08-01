c944fab54e25f9e8e3c885b41a71737646fe6f50
Jacob Meixner <jjm3333@gmail.com>
2026-08-01 12:11:45 -0700

    Revert "Address some typing issues"
    This reverts commit ff4897a04d754ca09234a3af1902b8f4827ad831.

--- app/dataSources/dbFetch.tsx
+++ app/dataSources/dbFetch.tsx
@@ -180,7 +180,7 @@ export async function getUser(userId: string): Promise<UserData> {
     return getJson<UserData>(`internal_user:${userId}`);
 }
 
-export async function getJson<T>(key: string): Promise<T|null> {
+export async function getJson<T>(key: string): Promise<T> {
 
     if (key === undefined) {
         throw new Error("Unable to getJson. Provided param 'key' is undefined but must have a value");
