e8a19ccdd8698f91fc02f31bd830d54b36562872
Jacob Meixner <jjm3333@gmail.com>
2026-08-01 12:12:06 -0700

    Revert "Attempt to wrap the db call in a try/catch"
    This reverts commit 3d1cb77df5829f5a82835d423f35bc34df9804ef.

--- app/dataSources/dbFetch.tsx
+++ app/dataSources/dbFetch.tsx
@@ -190,16 +190,7 @@ export async function getJson<T>(key: string): Promise<T> {
         url: process.env.KV_REST_API_URL,
         token: process.env.KV_REST_API_TOKEN
     })
-
-    let jsonResult: T | null = null;
-    try {
-        jsonResult = await redis.json.get(key);
-    } catch (error) {
-        console.error(`Could not read key '${key}' because of error: ${error}`);
-        // Not sure if I can... but here's to trying
-        return null;
-    }
-
+    const jsonResult: T | null = await redis.json.get(key);
     if (jsonResult !== null){
         return jsonResult;
     } else {
